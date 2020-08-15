0e616c4ec18476b492fb2d64b7eb21161f48c52d
ankurnarkhede <ankurnarkhede@gmail.com>
2020-08-15 11:20:19 +0530

    Integrated a basic logger

--- config/test.js
+++ config/test.js
@@ -1,3 +1,6 @@
+const setBooleanConfig = require('../utils/config').setBooleanConfig
+
 module.exports = {
-  port: process.env.PORT || 3000
+  port: process.env.PORT || 3000,
+  enableLogTrasports: setBooleanConfig(process.env.ENABLE_LOGS, true)
 }
