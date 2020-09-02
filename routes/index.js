0a181ca553e28a7305c70d53500b97e1ebaf4e9a
Nikhil Bhandarkar <nikhil.bhandarkar@waycool.in>
2020-09-02 19:07:54 +0530

    Changed user to users

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,6 @@ const app = express()
 
 app.use('/healthcheck', require('./healthCheck.js'))
 app.use('/auth', require('./auth.js'))
-app.use('/user', require('./user.js'))
+app.use('/users', require('./user.js'))
 
 module.exports = app
