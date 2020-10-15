255102328fe58bac7bbd5bb3339b5b6c568e4dac
Ankur Narkhede <ankurnarkhede@gmail.com>
2020-10-16 02:58:51 +0530

    Revert "Throw err from model functions instead of returning them"

--- models/users.js
+++ models/users.js
@@ -45,7 +45,7 @@ const addOrUpdate = async (userData, userId = null) => {
     return { isNewUser: true, userId: userInfo.id }
   } catch (err) {
     logger.error('Error in adding or updating user', err)
-    throw err
+    return err
   }
 }
 
@@ -74,7 +74,7 @@ const fetchUsers = async (query) => {
     return allUsers
   } catch (err) {
     logger.error('Error retrieving user data', err)
-    throw err
+    return err
   }
 }
 
@@ -98,7 +98,7 @@ const fetchUser = async (userId) => {
     }
   } catch (err) {
     logger.error('Error retrieving user data', err)
-    throw err
+    return err
   }
 }
 
