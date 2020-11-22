14f23a13bc01fd008fdce4553660c218a3fc372b
takshch <tkcphp@gmail.com>
2020-11-22 09:26:44 +0530

    Few code changes based on review

--- local-test.js
+++ local-test.js
@@ -23,21 +23,15 @@ if (!firestoreData) {
   throw new Error('Please make sure firestore-private-key.json file is not empty')
 }
 
-readWriteCheck()
-
 // check local development have permission to read and write in firestore or not
-async function readWriteCheck () {
-  try {
-    const docRef = await Firestore.collection('dummy').doc('users')
-    await docRef.set({
-      user: 'dummy'
-    })
-    const resp = await docRef.get('user')
-    if (resp.data().user !== 'dummy') {
-      throw new Error('Problem with permission of read and write.\nCheck your firestore permissions')
-    }
-    await docRef.delete()
-  } catch (err) {
-    throw new Error(err)
+(async () => {
+  const docRef = await Firestore.collection('dummy').doc('users')
+  await docRef.set({
+    user: 'dummy'
+  })
+  const resp = await docRef.get('user')
+  if (resp.data().user !== 'dummy') {
+    throw new Error('Problem with permission of read and write.\nCheck your firestore permissions')
   }
-}
+  await docRef.delete()
+})()
