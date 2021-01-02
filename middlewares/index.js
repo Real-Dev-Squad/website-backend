fe3033f5d3d2458cc824333699ea23e0a082dbfa
ankurnarkhede <ankurnarkhede@gmail.com>
2021-01-02 15:53:11 +0530

    Remove unneeded response headers

--- middlewares/index.js
+++ middlewares/index.js
@@ -20,19 +20,33 @@ const middleware = (app) => {
   // Middleware for sending error responses with express response object. To be required above all middlewares
   app.use(boom())
 
+  // Initialise logging middleware
   app.use(morgan('combined', { stream: logger.stream }))
 
   // Request parsing middlewares
   app.use(express.json())
   app.use(express.urlencoded({ extended: false }))
   app.use(cookieParser())
-  app.use(helmet())
+
+  // Middleware to add security headers. Few headers have been disabled as it does not serve any purpose for the API.
+  app.use(
+    helmet({
+      contentSecurityPolicy: false,
+      dnsPrefetchControl: false,
+      ieNoOpen: false,
+      referrerPolicy: false,
+      xssFilter: false
+    })
+  )
+
   app.use(cors({
     origin: config.get('cors.allowedOrigins'),
     credentials: true,
     optionsSuccessStatus: 200
   }))
   app.use(contentTypeCheck)
+
+  // Initialise authentication middleware
   app.use(passport.initialize())
 
   // Enable Swagger API docs in non-production environments
