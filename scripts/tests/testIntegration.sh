6984910b817936041cf1c9c797171c2aadf578aa
Cartmanishere <apbetahouse45@gmail.com>
2021-01-16 13:01:43 +0530

    Fix firestore emulator issue
    Emulator would keep running in bg even after test are done.
    This change fixes that so emulator is stopped after tests.

--- scripts/tests/testIntegration.sh
+++ scripts/tests/testIntegration.sh
@@ -3,18 +3,5 @@
 # set 'test' environment
 export NODE_ENV='test'
 
-# Set environment variable for firestore emulator host
-# @todo: Get port value from '../../firebase.json'
-export FIRESTORE_EMULATOR_HOST='localhost:8080'
-
-echo 'Start Firestore Emulator:'
-firebase emulators:start &
-
-echo 'Running integration tests:'
-nyc mocha test/integration/**
-
-# Erase the database
-# @todo: Get port value from '../../firebase.json' and projectId from config
-# Route; DELETE http://localhost:<PORT>/emulator/v1/projects/<PROJECT_ID>/databases/(default)/documents
-echo 'Erasing Firestore data:'
-curl -v -X DELETE 'http://localhost:8080/emulator/v1/projects/rds-dev/databases/(default)/documents'
+echo 'Start firestore emulator and run integration tests:'
+firebase emulators:exec 'nyc mocha test/integration/**'
