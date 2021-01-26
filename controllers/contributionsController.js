2d4cb3f482e71a5e9656b897a80e8d0219cb414d
Prakash <om.pcprakash@gmail.com>
2021-01-26 08:54:22 +0530

    added user not found message

--- controllers/contributionsController.js
+++ controllers/contributionsController.js
@@ -1,4 +1,5 @@
 const contributionsService = require('../services/contributions')
+const { fetchUser } = require('../models/users');
 
 const ERROR_MESSAGE = 'Something went wrong. Please try again or contact admin'
 
@@ -11,8 +12,12 @@ const ERROR_MESSAGE = 'Something went wrong. Please try again or contact admin'
 async function getUserContributions (req, res) {
   try {
     const username = req.params.username
-    const contributions = await contributionsService.getUserContributions(username)
-    return res.status.json(contributions)
+    const result = await fetchUser({ username: req.params.username })
+    if (result.userExists) {
+      const contributions = await contributionsService.getUserContributions(username)
+      return res.json(contributions)
+    }
+    return res.boom.notFound('User doesn\'t exist')
   } catch (err) {
     logger.error(`Error while retriving contributions ${err}`)
     return res.boom.serverUnavailable(ERROR_MESSAGE)
