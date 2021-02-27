f5c884e91f1e548b45725a2f3ff89af642e1468b
kachwalUttamsharma <kachwaluttamsharma@Kachwals-MacBook-Pro.local>
2021-02-27 21:05:26 +0530

    code refector created new common function for transaction calculation

--- controllers/cryptoController.js
+++ controllers/cryptoController.js
@@ -1,18 +1,10 @@
 const usersdata = require('../models/crypto')
+const calculation = require('../middlewares/cryptoTransactionClac')
 /**
  * Send Money from one User to Other
  * @param {Object} req - Express request object
  * @param {Object} res - Express response object
  */
-// http://localhost:3000/cryptoTransaction/send
-// content-type: application.json -> hearder
-// JSON body
-// {
-//   "to": "uttam",
-//   "from": "kratika",
-//   "amount": 30,
-//   "currency": "silver"
-// }
 const send = async (req, res) => {
   try {
     const request = req.body
@@ -20,22 +12,11 @@ const send = async (req, res) => {
     const message1 = `${amount} ${currency} Coins Transfered From ${userFrom} to ${userTo}`
     const verifyUserTO = await usersdata.fetchUser(userTo)
     const verifyUserFrom = await usersdata.fetchUser(userFrom)
-    if (verifyUserTO && verifyUserFrom) {
-      if (currency === 'brass' && verifyUserFrom.user.coins.brass >= amount) {
-        verifyUserFrom.user.coins.brass = verifyUserFrom.user.coins.brass - amount
-        verifyUserTO.user.coins.brass = verifyUserTO.user.coins.brass + amount
-      } else if (currency === 'gold' && verifyUserFrom.user.coins.gold >= amount) {
-        verifyUserFrom.user.coins.gold = verifyUserFrom.user.coins.gold - amount
-        verifyUserTO.user.coins.gold = verifyUserTO.user.coins.gold + amount
-      } else if (currency === 'silver' && verifyUserFrom.user.coins.silver >= amount) {
-        verifyUserFrom.user.coins.silver = verifyUserFrom.user.coins.silver - amount
-        verifyUserTO.user.coins.silver = verifyUserTO.user.coins.silver + amount
-      }
-    }
+    const resultObj = calculation.cryptoCalc(verifyUserTO, verifyUserFrom, currency, amount)
     return res.json({
       message: message1,
-      to: verifyUserTO,
-      from: verifyUserFrom
+      to: resultObj.verifyUserTO,
+      from: resultObj.verifyUserFrom
     })
   } catch (error) {
     logger.error(`Error while processing pull requests: ${error}`)
@@ -48,15 +29,6 @@ const send = async (req, res) => {
  * @param {Object} req - Express request object
  * @param {Object} res - Express response object
  */
-// http://localhost:3000/cryptoTransaction/receive
-// content-type: application.json -> hearder
-// JSON body
-// {
-//   "to": "uttam",
-//   "from": "kratika",
-//   "amount": 30,
-//   "currency": "silver"
-// }
 const request = async (req, res) => {
   try {
     const request = req.body
@@ -85,9 +57,6 @@ const request = async (req, res) => {
  * @param {Object} req - Express request object
  * @param {Object} res - Express response object
  */
-
-// http://localhost:3000/cryptoTransaction/approvedRequest
-// content-type: appliction/json -> header
 // Json Body
 //  {
 //   "notification":  "30 silver Coins Requested By kratika",
@@ -102,22 +71,11 @@ const approved = async (req, res) => {
     const [amount, currency, userTO] = [message[0], message[1], message[(message.length) - 1]]
     const verifyUserTo = await usersdata.fetchUser(userTO)
     // const message1 = `${amount} ${currency} Coins Transfered From ${userFrom} to ${userTo}`;
-    if (verifyUserTo && verifyUserFrom) {
-      if (currency === 'brass' && verifyUserFrom.user.coins.brass >= amount) {
-        verifyUserFrom.user.coins.brass = verifyUserFrom.user.coins.brass - amount
-        verifyUserTo.user.coins.brass = verifyUserTo.user.coins.brass + parseInt(amount)
-      } else if (currency === 'gold' && verifyUserFrom.user.coins.gold >= amount) {
-        verifyUserFrom.user.coins.gold = verifyUserFrom.user.coins.gold - amount
-        verifyUserTo.user.coins.gold = verifyUserTo.user.coins.gold + parseInt(amount)
-      } else if (currency === 'silver' && verifyUserFrom.user.coins.silver >= amount) {
-        verifyUserFrom.user.coins.silver = verifyUserFrom.user.coins.silver - amount
-        verifyUserTo.user.coins.silver = verifyUserTo.user.coins.silver + parseInt(amount)
-      }
-    }
+    const resultObj = calculation.cryptoCalc(verifyUserTo, verifyUserFrom, currency, amount)
     return res.json({
       message: notification,
-      from: verifyUserFrom,
-      to: verifyUserTo
+      to: resultObj.verifyUserTO,
+      from: resultObj.verifyUserFrom
     })
   } catch (error) {
     logger.error(`Error while processing pull requests: ${error}`)
---
+++ middlewares/cryptoTransactionClac.js
@@ -0,0 +1,26 @@
+/**
+ * Does Calculation for Transaction from one User to Other
+ * @param {Object} verifyUserTO - To whom currency should be transferred
+ * @param {Object} verifyUserFrom - From whom currency should be deducted
+ * @param {Object} currency - Type of Currency
+ * @param {Object} amount - amount of Currency
+ */
+const cryptoCalc = (verifyUserTO, verifyUserFrom, currency, amount) => {
+  if (verifyUserTO && verifyUserFrom) {
+    if (currency === 'brass' && verifyUserFrom.user.coins.brass >= amount) {
+      verifyUserFrom.user.coins.brass = verifyUserFrom.user.coins.brass - parseInt(amount)
+      verifyUserTO.user.coins.brass = verifyUserTO.user.coins.brass + parseInt(amount)
+    } else if (currency === 'gold' && verifyUserFrom.user.coins.gold >= amount) {
+      verifyUserFrom.user.coins.gold = verifyUserFrom.user.coins.gold - parseInt(amount)
+      verifyUserTO.user.coins.gold = verifyUserTO.user.coins.gold + parseInt(amount)
+    } else if (currency === 'silver' && verifyUserFrom.user.coins.silver >= amount) {
+      verifyUserFrom.user.coins.silver = verifyUserFrom.user.coins.silver - parseInt(amount)
+      verifyUserTO.user.coins.silver = verifyUserTO.user.coins.silver + parseInt(amount)
+    }
+  }
+  return { verifyUserFrom, verifyUserTO }
+}
+
+module.exports = {
+  cryptoCalc
+}
