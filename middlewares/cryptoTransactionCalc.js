7fef77639b949f7f8735ef8530f85b86cc8f5eba
kachwalUttamsharma <kachwaluttamsharma@Kachwals-MacBook-Pro.local>
2021-03-06 12:55:17 +0530

    api fix

--- middlewares/cryptoTransactionCalc.js
+++ middlewares/cryptoTransactionCalc.js
@@ -6,21 +6,21 @@
  * @param {Object} amount - amount of Currency
  */
 const cryptoCalc = (verifyUserTO, verifyUserFrom, currency, amount) => {
-    if (verifyUserTO && verifyUserFrom) {
-      if (currency === 'brass' && verifyUserFrom.user.coins.brass >= amount) {
-        verifyUserFrom.user.coins.brass = verifyUserFrom.user.coins.brass - parseInt(amount)
-        verifyUserTO.user.coins.brass = verifyUserTO.user.coins.brass + parseInt(amount)
-      } else if (currency === 'gold' && verifyUserFrom.user.coins.gold >= amount) {
-        verifyUserFrom.user.coins.gold = verifyUserFrom.user.coins.gold - parseInt(amount)
-        verifyUserTO.user.coins.gold = verifyUserTO.user.coins.gold + parseInt(amount)
-      } else if (currency === 'silver' && verifyUserFrom.user.coins.silver >= amount) {
-        verifyUserFrom.user.coins.silver = verifyUserFrom.user.coins.silver - parseInt(amount)
-        verifyUserTO.user.coins.silver = verifyUserTO.user.coins.silver + parseInt(amount)
-      }
+  if (verifyUserTO && verifyUserFrom) {
+    if (currency === 'brass' && verifyUserFrom.user.coins.brass >= amount) {
+      verifyUserFrom.user.coins.brass = verifyUserFrom.user.coins.brass - parseInt(amount)
+      verifyUserTO.user.coins.brass = verifyUserTO.user.coins.brass + parseInt(amount)
+    } else if (currency === 'gold' && verifyUserFrom.user.coins.gold >= amount) {
+      verifyUserFrom.user.coins.gold = verifyUserFrom.user.coins.gold - parseInt(amount)
+      verifyUserTO.user.coins.gold = verifyUserTO.user.coins.gold + parseInt(amount)
+    } else if (currency === 'silver' && verifyUserFrom.user.coins.silver >= amount) {
+      verifyUserFrom.user.coins.silver = verifyUserFrom.user.coins.silver - parseInt(amount)
+      verifyUserTO.user.coins.silver = verifyUserTO.user.coins.silver + parseInt(amount)
     }
-    return { verifyUserFrom, verifyUserTO }
   }
-  
-  module.exports = {
-    cryptoCalc
-  }
+  return { verifyUserFrom, verifyUserTO }
+}
+
+module.exports = {
+  cryptoCalc
+}
