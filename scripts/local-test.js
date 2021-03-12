72f72abd9a2b00e5bae08e71289c8943f09c118a
Tanya Dixit <shanya.2018td@gmail.com>
2021-03-12 23:52:01 +0530

    added logs

--- scripts/local-test.js
+++ scripts/local-test.js
@@ -9,12 +9,17 @@ if (
   !config.githubOauth.clientSecret
 ) {
   throw new Error('Github credentials are not properly set')
+} else {
+  // eslint-disable-next-line no-console
+  console.log('Github cridentials are properly set.')
 }
 
 let firestoreData
 // throw an error if unable to read file
 try {
   firestoreData = fs.readFileSync('./firestore-private-key.json', 'utf8')
+  // eslint-disable-next-line no-console
+  console.log('Firestore-private-key.json file is correct.')
 } catch (error) {
   throw new Error('Please make sure firestore-private-key.json file is correct')
 }
@@ -22,6 +27,9 @@ try {
 // check whether firestoreData is empty, null, and  undefined
 if (!firestoreData) {
   throw new Error('Please make sure firestore-private-key.json file is not empty')
+} else {
+  // eslint-disable-next-line no-console
+  console.log('Firestore-private-key.json file is not empty.')
 }
 
 // check local development have permission to read and write in firestore or not
@@ -33,6 +41,9 @@ if (!firestoreData) {
   const resp = await docRef.get('user')
   if (resp.data().user !== 'dummy') {
     throw new Error('Problem with permission of read and write.\nCheck your firestore permissions')
+  } else {
+    // eslint-disable-next-line no-console
+    console.log('Local development has permission to read and write in firestore.')
   }
   await docRef.delete()
 
