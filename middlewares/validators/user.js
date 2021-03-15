b906d0957355b621a09878aa575083a7728a926c
Tanya Dixit <shanya.2018td@gmail.com>
2021-03-15 14:51:19 +0530

    Removed extra validation fields from user validator

--- middlewares/validators/user.js
+++ middlewares/validators/user.js
@@ -4,7 +4,6 @@ const updateUser = async (req, res, next) => {
   const schema = joi.object().keys({
     phone: joi.string().optional(),
     email: joi.string().optional(),
-    company_name: joi.string().optional(),
     username: joi.string().optional(),
     first_name: joi.string().optional(),
     last_name: joi.string().optional(),
@@ -20,7 +19,6 @@ const updateUser = async (req, res, next) => {
     site: joi.string().optional(),
     isMember: joi.boolean().optional(),
     tokens: joi.object().optional(),
-    website: joi.string().optional()
   })
 
   try {
