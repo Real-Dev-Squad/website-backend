577e67822228c385b8e434a78ef20adc2194874a
ankushdharkar <ankushdharkar@gmail.com>
2021-03-20 17:12:03 -0700

    Added get wallet by username API #281

--- controllers/walletsController.js
+++ controllers/walletsController.js
@@ -1,9 +1,10 @@
 const { fetchWallet } = require('../models/wallets')
+const userUtils = require('../utils/users')
 
 const ERROR_MESSAGE = 'Something went wrong. Please try again or contact admin'
 
 /**
- * Get the  wallet details of user
+ * Get the wallet details of user
  * @param {Object} req - Express request object
  * @param {Object} res - Express response object
  */
@@ -22,6 +23,28 @@ const getUserWallet = async (req, res) => {
   }
 }
 
+/**
+ * Get the wallet details of user, if a username is provided
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ */
+const getWallet = async (req, res) => {
+  try {
+    const { params: { username } = {} } = req
+    const userId = await userUtils.getUserId(username)
+
+    const walletData = await fetchWallet(userId)
+    return res.json({
+      message: 'Wallet returned successfully',
+      wallet: walletData
+    })
+  } catch (err) {
+    logger.error(`Error while retriving wallet data ${err}`)
+    return res.boom.badImplementation(ERROR_MESSAGE)
+  }
+}
+
 module.exports = {
-  getUserWallet
+  getUserWallet,
+  getWallet
 }
