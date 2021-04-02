5dbaab3670463cc3c170ff5d4c5b0793d2e89f53
Rishab Kumar <rkmr039@gmail.com>
2021-04-02 10:22:18 +0530

    no user and no records case handeled

--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -2,8 +2,8 @@ const transactionsModel = require('../models/transaction')
 const { getUserId } = require('../utils/users')
 
 /**
- * Collects all transactions and sends only required data for each userId
- *
+ * Collects all transactions and sends only required data for username spicified in url
+ * 
  * @param req {Object} - Express request object
  * @param res {Object} - Express response object
  */
@@ -11,13 +11,25 @@ const { getUserId } = require('../utils/users')
 const fetchLatest = async (req, res) => {
   try {
     const userId = await getUserId(req.params.username)
-    const noOfRecords = req.params.noOfRecords
-    const data = await transactionsModel.fetchLatest(userId, noOfRecords)
-    return res.json({
-      message: 'Transactions returned successfully!',
-      responseCode: 200,
-      data
-    })
+    if (userId != undefined) {
+      const noOfRecords = req.params.noOfRecords
+      const data = await transactionsModel.fetchLatest(userId, noOfRecords)
+      if (data.length > 0) {
+        return res.json({
+          message: 'Transactions returned successfully!',
+          responseCode: 200,
+          data
+        })
+      } else {
+        return res.json({
+          message: 'No record exist!'
+        })
+      }
+    } else {
+      return res.json({
+        message: 'User does not exist!'
+      })
+    }
   } catch (err) {
     logger.error(`Error while processing transactions fetch request: ${err}`)
     return res.boom.badImplementation('Something went wrong please contact admin')
