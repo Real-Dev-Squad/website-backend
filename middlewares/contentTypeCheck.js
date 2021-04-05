865b74d08ebfa3f89ef3d1e8a2f1595d053d4e6d
Prakash <om.pcprakash@gmail.com>
2021-04-05 13:58:23 +0530

    Corrected content type check

--- middlewares/contentTypeCheck.js
+++ middlewares/contentTypeCheck.js
@@ -7,8 +7,12 @@
  * @param {function} next - Express middleware function
  */
 module.exports = (req, res, next) => {
-  if (req.headers['content-type'] && (req.headers['content-type'] === 'application/json' || req.headers['content-type'] === 'multipart/form-data')) {
-    return res.boom.unsupportedMediaType(`Invalid content-type header: ${req.headers['content-type']}, expected: application/json`)
+  if (req.headers['content-type'] && (req.headers['content-type'] !== 'application/json')) {
+    const contentType = req.headers['content-type']
+    const notMultiPart = !(contentType.includes('multipart/form-data'))
+    if (notMultiPart) {
+      return res.boom.unsupportedMediaType(`Invalid content-type header: ${req.headers['content-type']}, expected: application/json or multipart/form-data`)
+    }
   }
 
   return next()
