30a89082dcf8edded08c1c252405475b3be5ef41
Prakash <om.pcprakash@gmail.com>
2021-04-26 22:27:45 +0530

    updated cloudinary rename

--- config/custom-environment-variables.js
+++ config/custom-environment-variables.js
@@ -59,8 +59,8 @@ module.exports = {
   },
 
   cloudinary: {
-    cloud_name: 'CLOUD_NAME',
-    api_key: 'API_KEY',
-    api_secret: 'API_SECRET_KEY'
+    cloud_name: 'CLOUDINARY_CLOUD_NAME',
+    api_key: 'CLOUDINARY_API_KEY',
+    api_secret: 'CLOUDINARY_API_SECRET_KEY'
   }
 }
