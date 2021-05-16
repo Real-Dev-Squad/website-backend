001459fd559ae2caf95f917fad0584429890e5ac
Anurag <anurag.khandelwal@primussoft.com>
2021-05-16 22:44:52 +0530

    Added jsDoc for getUnixEpochTime function

--- utils/time.js
+++ utils/time.js
@@ -1,3 +1,9 @@
+/**
+ * Converts Time in milliseconds to Epoch Time
+ * @param {number} time 
+ * @returns {number}
+ */
+
 const getUnixEpochTime = (time = Date.now()) => {
   return Math.floor(time / 1000)
 }
