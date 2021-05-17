27d0cf82fc6e39c7d3e76cde156eb24b20959578
anshulg26 <anshul@freightdeck.com>
2021-05-17 16:22:27 +0530

    added setup for testing

--- controllers/notifications.js
+++ controllers/notifications.js
@@ -31,7 +31,7 @@ module.exports.getNotificationsForUser = async (req, res) => {
       userId: ''
     })
 
-    return res.json({
+    return res.status(200).json({
       message: 'Notifications returned successfully!',
       data: paginatedNotifications || []
     })
