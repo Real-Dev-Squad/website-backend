8225b5d2f3156b8a5e925796ebdf2ea90dcce678
Harshith <harshithece7016@gmail.com>
2021-06-19 06:13:23 +0530

    removed valid-url package

--- models/urls.js
+++ models/urls.js
@@ -12,8 +12,9 @@ const ERROR_MESSAGE = 'Error getting urls'
  */
 const addUrl = async (urlData) => {
   try {
+    const { longUrl, shortUrl } = urlData
     const { id } = await urlsModel.add(urlData)
-    return { id, longUrl: urlData.longUrl, shortUrl: urlData.shortUrl }
+    return { id, longUrl, shortUrl }
   } catch (err) {
     logger.error(ERROR_MESSAGE, err)
     throw err
@@ -45,7 +46,8 @@ const fetchUrl = async (shortUrl) => {
     const snapshot = await urlsModel.where('shortUrl', '==', shortUrl).get()
     const [urlData] = snapshot.docs
     if (urlData) {
-      return { id: urlData.id, longUrl: urlData.longUrl, shortUrl: urlData.shortUrl }
+      const { longUrl, shortUrl } = urlData
+      return { id: urlData.id, longUrl, shortUrl }
     }
     return false
   } catch (err) {
