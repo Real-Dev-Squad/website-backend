c08c4b84265ffd219e16edd81fe4a1fea3514443
Cartmanishere <apbetahouse45@gmail.com>
2021-06-30 14:51:58 +0530

    Refactor authenticate middleware

--- routes/challenges.js
+++ routes/challenges.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const authenticate = require('../middlewares/authenticate')
+const { authenticate } = require('../middlewares/authenticate')
 const challenges = require('../controllers/challenge')
 const { createChallenge } = require('../middlewares/validators/challenges')
 
