0f7493c81c98fc1c0440e74f30c5288805ad2160
Mehul Kiran Chaudhari <chaudharimehul2001@gmail.com>
2021-11-05 22:20:42 +0530

    added: requested changes

--- models/members.js
+++ models/members.js
@@ -158,7 +158,7 @@ const addArchiveRoleToMembers = async (userId) => {
   try {
     const userDoc = await userModel.doc(userId).get()
     const user = userDoc.data()
-    if (user?.roles && user?.roles?.archivedMember) return { isArchived: true, addArchiveRole: false }
+    if (user?.roles?.archivedMember) return { isArchived: true, addArchiveRole: false }
     const roles = user.roles ? { ...user.roles, archivedMember: true } : { archivedMember: true }
     await userModel.doc(userId).update({
       roles
