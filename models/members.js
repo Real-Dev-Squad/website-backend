712929947a5c510ba96108b8830d35d83f86a036
Mehul Kiran Chaudhari <55375534+MehulKChaudhari@users.noreply.github.com>
2021-11-30 22:55:38 +0530

    feat: archive users/members from the members page (#427)
    * feat: archive members
    
    * generated api schema
    
    * added: check if user is already archived or not
    
    * bug fix
    
    * added: optional chaining
    
    * added: move to member model.
    
    * added: integration test for testing archive member feature
    
    * key changed to camelCase
    
    * Added: requested changes
    
    * added: requested changes
    
    * added: requested changes
    
    * bug fix
    
    * fix: typo
    
    * removed unwanted code.
    
    * removed unwanted code
    
    * Fixed schema
    
    * added: requested changes
    
    * added: requested changes

--- models/members.js
+++ models/members.js
@@ -148,8 +148,31 @@ const fetchUsersWithRole = async (role) => {
   }
 }
 
+/**
+ * changes the role of a new user to member
+ * @param userId { String }: User id of user to be modified
+ * @return { Object }: whether moveToMember was successful or not and whether user is already a member or not
+ */
+
+const addArchiveRoleToMembers = async (userId) => {
+  try {
+    const userDoc = await userModel.doc(userId).get()
+    const user = userDoc.data()
+    if (user?.roles?.archivedMember) return { isArchived: true }
+    const roles = { ...user.roles, archivedMember: true }
+    await userModel.doc(userId).update({
+      roles
+    })
+    return { isArchived: false }
+  } catch (err) {
+    logger.error('Error updating user', err)
+    throw err
+  }
+}
+
 module.exports = {
   moveToMembers,
+  addArchiveRoleToMembers,
   fetchUsers,
   migrateUsers,
   deleteIsMemberProperty,
