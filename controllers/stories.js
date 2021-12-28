f0d232d39aeee684a92dbaf3039ecbb0d5564491
Shubham <yadav105shubham@gmail.com>
2021-12-28 20:51:58 +0530

    Add create, update and get story API with integration test

---
+++ controllers/stories.js
@@ -0,0 +1,80 @@
+const stories = require('../models/stories')
+/**
+ * Creates new story
+ *
+ * @param req {Object} - Express request object
+ * @param req.body {Object} - Story object
+ * @param res {Object} - Express response object
+ */
+const addNewStory = async (req, res) => {
+  try {
+    const body = {
+      ...req.body
+    }
+    const story = await stories.updateStory(body)
+
+    if (story) {
+      return res.json({
+        message: 'Story created successfully!',
+        story: story.storyDetails,
+        id: story.storyId
+      })
+    } else {
+      logger.error('Error while creating new story: Incorrect username passed')
+      return res.boom.badRequest('Unable to add story')
+    }
+  } catch (err) {
+    logger.error(`Error while creating new story: ${err}`)
+    return res.boom.badImplementation('An internal server error occurred')
+  }
+}
+/**
+ * Fetches all the stories
+ *
+ * @param req {Object} - Express request object
+ * @param res {Object} - Express response object
+ */
+const fetchStories = async (req, res) => {
+  try {
+    const allStories = await stories.fetchStories()
+    return res.json({
+      message: 'Stories returned successfully!',
+      stories: allStories.length > 0 ? allStories : []
+    })
+  } catch (err) {
+    logger.error(`Error while fetching stories ${err}`)
+    return res.boom.badImplementation('An internal server error occurred')
+  }
+}
+
+/**
+ * Updates the story
+ *
+ * @param req {Object} - Express request object
+ * @param res {Object} - Express response object
+ */
+const updateStory = async (req, res) => {
+  try {
+    const story = await stories.fetchStory(req.params.id)
+    if (!story.storyData) {
+      return res.boom.notFound('Story not found')
+    }
+
+    const updateStoryResult = await stories.updateStory(req.body, req.params.id)
+    if (updateStoryResult) {
+      return res.status(204).send()
+    } else {
+      logger.error(`Error while updating storyId ${req.params.id}: Incorrect username passed`)
+      return res.boom.badRequest('Unable to update story')
+    }
+  } catch (err) {
+    logger.error(`Error while updating story: ${err}`)
+    return res.boom.badImplementation('An internal server error occurred')
+  }
+}
+
+module.exports = {
+  addNewStory,
+  fetchStories,
+  updateStory
+}
---
+++ test/integration/stories.test.js
@@ -0,0 +1,151 @@
+const chai = require('chai')
+const { expect } = chai
+const chaiHttp = require('chai-http')
+
+const app = require('../../server')
+const stories = require('../../models/stories')
+const authService = require('../../services/authService')
+const addUser = require('../utils/addUser')
+const config = require('config')
+const cookieName = config.get('userToken.cookieName')
+const userData = require('../fixtures/user/user')()
+const cleanDb = require('../utils/cleanDb')
+
+chai.use(chaiHttp)
+
+const appOwner = userData[3]
+const userToBeFeatureOwner = userData[0]
+const userToBeBackendEngineer = userData[1]
+const userToBeFrontendEngineer = userData[1]
+
+let jwt
+
+describe('Stories', function () {
+  let storyId1, storyId
+
+  before(async function () {
+    const userId = await addUser(appOwner)
+    jwt = authService.generateAuthToken({ userId })
+
+    const storyData = [{
+      title: 'Test story',
+      description: 'Test active story',
+      status: 'active',
+      tasks: ['12ab'],
+      featureOwner: userToBeFeatureOwner.username,
+      backendEngineer: userToBeBackendEngineer.username,
+      frontendEngineer: userToBeFrontendEngineer.username,
+      startedOn: 4567,
+      endsOn: 1234
+    }, {
+      title: 'Test story',
+      description: 'Test completed story',
+      status: 'completed',
+      tasks: ['34ab'],
+      featureOwner: userToBeFeatureOwner.username,
+      backendEngineer: userToBeBackendEngineer.username,
+      frontendEngineer: userToBeFrontendEngineer.username,
+      startedOn: 4567,
+      endsOn: 1234
+    }]
+    await addUser(userToBeFeatureOwner)
+    await addUser(userToBeBackendEngineer)
+    await addUser(userToBeFrontendEngineer)
+    // Add the active story
+    storyId = (await stories.updateStory(storyData[0])).storyId
+    storyId1 = storyId
+
+    // Add the completed story
+    storyId = (await stories.updateStory(storyData[1])).storyId
+  })
+
+  after(async function () {
+    await cleanDb()
+  })
+
+  describe('POST /story - creates a new story', function () {
+    it('Should return success response after adding the story', function (done) {
+      chai
+        .request(app)
+        .post('/story')
+        .set('cookie', `${cookieName}=${jwt}`)
+        .send({
+          title: 'Test story',
+          description: 'Test active story',
+          status: 'active',
+          tasks: ['12ab'],
+          featureOwner: userToBeFeatureOwner.username,
+          backendEngineer: userToBeBackendEngineer.username,
+          frontendEngineer: userToBeFrontendEngineer.username,
+          startedOn: 4567,
+          endsOn: 1234
+        })
+        .end((err, res) => {
+          if (err) { return done(err) }
+          expect(res).to.have.status(200)
+          expect(res.body).to.be.a('object')
+          expect(res.body.message).to.equal('Story created successfully!')
+          expect(res.body.id).to.be.a('string')
+          expect(res.body.story).to.be.a('object')
+          expect(res.body.story.featureOwner).to.equal(userToBeFeatureOwner.username)
+          expect(res.body.story.backendEngineer).to.equal(userToBeBackendEngineer.username)
+          expect(res.body.story.frontendEngineer).to.equal(userToBeFrontendEngineer.username)
+          expect(res.body.story.tasks).to.be.a('array')
+          return done()
+        })
+    })
+  })
+
+  describe('GET /story', function () {
+    it('Should get all the list of stories', function (done) {
+      chai
+        .request(app)
+        .get('/story')
+        .end((err, res) => {
+          if (err) { return done(err) }
+          expect(res).to.have.status(200)
+          expect(res.body).to.be.a('object')
+          expect(res.body.message).to.equal('Stories returned successfully!')
+          expect(res.body.stories).to.be.a('array')
+
+          return done()
+        })
+    })
+  })
+
+  describe('PATCH /story', function () {
+    it('Should update the story for the given storyId', function (done) {
+      chai
+        .request(app)
+        .patch('/story/' + storyId1)
+        .set('cookie', `${cookieName}=${jwt}`)
+        .send({
+          title: 'Test update story title'
+        })
+        .end((err, res) => {
+          if (err) { return done(err) }
+          expect(res).to.have.status(204)
+
+          return done()
+        })
+    })
+
+    it('Should return 404 if story does not exist', function (done) {
+      chai
+        .request(app)
+        .patch('/story/storyId')
+        .set('cookie', `${cookieName}=${jwt}`)
+        .send({
+          title: 'Test update story title'
+        })
+        .end((err, res) => {
+          if (err) { return done(err) }
+          expect(res).to.have.status(404)
+          expect(res.body).to.be.a('object')
+          expect(res.body.message).to.equal('Story not found')
+
+          return done()
+        })
+    })
+  })
+})
