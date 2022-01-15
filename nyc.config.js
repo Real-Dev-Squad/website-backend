a139dd851062ffe9937cf674853a090b55aeb924
akshay shinde <akshayshinde7289@gmail.com>
2022-01-15 20:05:03 +0530

    Revert "Merge branch 'feature/tdd-setup-#492' of https://github.com/Real-Dev-Squad/website-backend into develop"
    This reverts commit 71a13346efbde5bfa8d9a7833b42ed239df22651, reversing
    changes made to 247475855e3c84752f1e73b8381a6211d81ab257.

--- nyc.config.js
+++ nyc.config.js
@@ -4,21 +4,10 @@
  */
 module.exports = {
   all: true,
-  'check-coverage': false, // Will be enabled after reaching 50% coverage: https://github.com/Real-Dev-Squad/website-backend/issues/493
   exclude: [
-    'test/**'
+    '**/test.js'
   ],
   reporter: ['text', 'lcov', 'text-summary'],
   reportDir: '.coverage',
-  tempDir: '.coverage',
-  branches: 50,
-  lines: 50,
-  functions: 50,
-  statements: 50,
-  watermarks: {
-    lines: [75, 90],
-    functions: [75, 90],
-    branches: [75, 90],
-    statements: [75, 90]
-  }
+  tempDir: '.coverage'
 }
