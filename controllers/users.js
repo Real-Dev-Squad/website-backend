ecc838d03c0e8247b3a9b289414710515da7adb7
Deepansharora27 <deepansharora27@gmail.com>
2022-01-24 00:00:52 +0530

    Working on /users/verify endpoint

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,6 @@
-
-const userQuery = require('../models/users')
-const imageService = require('../services/imageService')
+const userQuery = require("../models/users");
+const imageService = require("../services/imageService");
+const axios = require("axios");
 /**
  * Fetches the data about our users
  *
@@ -8,19 +8,35 @@ const imageService = require('../services/imageService')
  * @param res {Object} - Express response object
  */
 
+const verifyUser = async (req, res) => {
+  try {
+    const userId = req.userData.id;
+    await userQuery.addOrUpdate(req.userData.identityStatus, userId);
+    let fetchData = {
+      body: JSON.stringify({
+        username: req.userData.username,
+        identityURL: req.userData.identityURL,
+      }),
+    };
+    fetch("https://someurl/verify", fetchData);
+  } catch (error) {
+    logger.error(`Error while Processing the Request : ${error}`);
+  }
+};
+
 const getUsers = async (req, res) => {
   try {
-    const allUsers = await userQuery.fetchUsers(req.query)
+    const allUsers = await userQuery.fetchUsers(req.query);
 
     return res.json({
-      message: 'Users returned successfully!',
-      users: allUsers
-    })
+      message: "Users returned successfully!",
+      users: allUsers,
+    });
   } catch (error) {
-    logger.error(`Error while fetching all users: ${error}`)
-    return res.boom.serverUnavailable('Something went wrong please contact admin')
+    logger.error(`Error while fetching all users: ${error}`);
+    return res.boom.serverUnavailable("Something went wrong please contact admin");
   }
-}
+};
 
 /**
  * Fetches the data about user with given id
@@ -31,22 +47,22 @@ const getUsers = async (req, res) => {
 
 const getUser = async (req, res) => {
   try {
-    const result = await userQuery.fetchUser({ username: req.params.username })
-    const { phone, email, ...user } = result.user
+    const result = await userQuery.fetchUser({ username: req.params.username });
+    const { phone, email, ...user } = result.user;
 
     if (result.userExists) {
       return res.json({
-        message: 'User returned successfully!',
-        user
-      })
+        message: "User returned successfully!",
+        user,
+      });
     }
 
-    return res.boom.notFound('User doesn\'t exist')
+    return res.boom.notFound("User doesn't exist");
   } catch (error) {
-    logger.error(`Error while fetching user: ${error}`)
-    return res.boom.serverUnavailable('Something went wrong please contact admin')
+    logger.error(`Error while fetching user: ${error}`);
+    return res.boom.serverUnavailable("Something went wrong please contact admin");
   }
-}
+};
 
 /**
  * checks whether a given username is available
@@ -57,15 +73,15 @@ const getUser = async (req, res) => {
 
 const getUsernameAvailabilty = async (req, res) => {
   try {
-    const result = await userQuery.fetchUser({ username: req.params.username })
+    const result = await userQuery.fetchUser({ username: req.params.username });
     return res.json({
-      isUsernameAvailable: !result.userExists
-    })
+      isUsernameAvailable: !result.userExists,
+    });
   } catch (error) {
-    logger.error(`Error while checking user: ${error}`)
-    return res.boom.serverUnavailable('Something went wrong please contact admin')
+    logger.error(`Error while checking user: ${error}`);
+    return res.boom.serverUnavailable("Something went wrong please contact admin");
   }
-}
+};
 
 /**
  * Fetches the data about logged in user
@@ -78,17 +94,17 @@ const getSelfDetails = (req, res) => {
   try {
     if (req.userData) {
       if (req.query.private) {
-        return res.send(req.userData)
+        return res.send(req.userData);
       }
-      const { phone, email, ...userData } = req.userData
-      return res.send(userData)
+      const { phone, email, ...userData } = req.userData;
+      return res.send(userData);
     }
-    return res.boom.notFound('User doesn\'t exist')
+    return res.boom.notFound("User doesn't exist");
   } catch (error) {
-    logger.error(`Error while fetching user: ${error}`)
-    return res.boom.badImplementation('An internal server error occurred')
+    logger.error(`Error while fetching user: ${error}`);
+    return res.boom.badImplementation("An internal server error occurred");
   }
-}
+};
 
 /**
  * Update the user
@@ -99,28 +115,29 @@ const getSelfDetails = (req, res) => {
  */
 const updateSelf = async (req, res) => {
   try {
-    const { id: userId } = req.userData
+    const { id: userId } = req.userData;
     if (req.body.username) {
-      const { user } = await userQuery.fetchUser({ userId })
+      const { user } = await userQuery.fetchUser({ userId });
       if (!user.incompleteUserDetails) {
-        return res.boom.forbidden('Cannot update username again')
+        return res.boom.forbidden("Cannot update username again");
       }
-      await userQuery.setIncompleteUserDetails(userId)
+      await userQuery.setIncompleteUserDetails(userId);
     }
 
-    const user = await userQuery.addOrUpdate(req.body, userId)
+    const user = await userQuery.addOrUpdate(req.body, userId);
 
-    if (!user.isNewUser) { // Success criteria, user finished the sign up process.
-      userQuery.initializeUser(userId)
-      return res.status(204).send()
+    if (!user.isNewUser) {
+      // Success criteria, user finished the sign up process.
+      userQuery.initializeUser(userId);
+      return res.status(204).send();
     }
 
-    return res.boom.notFound('User not found')
+    return res.boom.notFound("User not found");
   } catch (error) {
-    logger.error(`Error while updating user: ${error}`)
-    return res.boom.serverUnavailable('Something went wrong please contact admin')
+    logger.error(`Error while updating user: ${error}`);
+    return res.boom.serverUnavailable("Something went wrong please contact admin");
   }
-}
+};
 
 /**
  * Post user profile picture
@@ -130,24 +147,25 @@ const updateSelf = async (req, res) => {
  */
 const postUserPicture = async (req, res) => {
   try {
-    const { file } = req
-    const { id: userId } = req.userData
-    const imageData = await imageService.uploadProfilePicture(file, userId)
+    const { file } = req;
+    const { id: userId } = req.userData;
+    const imageData = await imageService.uploadProfilePicture(file, userId);
     return res.json({
-      message: 'Profile picture uploaded successfully!',
-      image: imageData
-    })
+      message: "Profile picture uploaded successfully!",
+      image: imageData,
+    });
   } catch (error) {
-    logger.error(`Error while adding profile picture of user: ${error}`)
-    return res.boom.badImplementation('An internal server error occurred')
+    logger.error(`Error while adding profile picture of user: ${error}`);
+    return res.boom.badImplementation("An internal server error occurred");
   }
-}
+};
 
 module.exports = {
+  verifyUser,
   updateSelf,
   getUsers,
   getSelfDetails,
   getUser,
   getUsernameAvailabilty,
-  postUserPicture
-}
+  postUserPicture,
+};
