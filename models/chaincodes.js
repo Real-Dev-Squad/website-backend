9146dd2a9e7e4214c82f4bd714e728473f616601
Ojas <ojaselawadhi1@gmail.com>
2022-01-26 00:58:22 +0530

    Created route /user/chaincode

--- models/chaincodes.js
+++ models/chaincodes.js
@@ -2,16 +2,14 @@ const firestore = require('../utils/firestore')
 const admin = require('firebase-admin')
 
 const chaincodeModel = firestore.collection('chaincodes')
-const storeChaincode = async (username) =>{
-    
-    const userchaincode = await chaincodeModel.add({
-        username, 
-        timestamp : admin.firestore.Timestamp.fromDate(new Date())  
-    })   
- 
+const storeChaincode = async (username) =>  {
+  const userchaincode = await chaincodeModel.add({
+  username,
+    timestamp:admin.firestore.Timestamp.fromDate(new Date())
+  })
     return userchaincode.id
 }
 
-module.exports= {
-    storeChaincode
+module.exports = {
+  storeChaincode
 }
