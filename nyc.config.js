2cc7f0ff58b62ed495f6a92fc456a3d5fbe5058c
Pallab Sonowal <pallabsonowal999@gmail.com>
2022-02-23 19:40:55 +0530

    change ignored folders

--- nyc.config.js
+++ nyc.config.js
@@ -5,7 +5,7 @@
 module.exports = {
   all: true,
   "check-coverage": false, // Will be enabled after reaching 50% coverage: https://github.com/Real-Dev-Squad/website-backend/issues/493
-  exclude: ["test/**"],
+  exclude: ["test/**", "mockdata/**", "docs/**", "public/**"],
   reporter: ["text", "lcov", "text-summary"],
   reportDir: ".coverage",
   tempDir: ".coverage",
--- scripts/tests/testUnit.sh
+++ scripts/tests/testUnit.sh
@@ -4,4 +4,4 @@
 export NODE_ENV='test'
 
 echo 'Start firestore emulator and run unit tests:'
-firebase emulators:exec 'nyc --n=models --n=middlewares/**.js --n=services --n=utils mocha test/unit/**'
+firebase emulators:exec 'nyc --x=controllers --x=test --x=docs --x=mockdata mocha test/unit/**'
