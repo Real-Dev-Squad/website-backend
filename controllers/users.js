becb9fe3254740f30bdc707222f45a6196ee3dc2
Lakshay Manchanda <lakshaymanchanda73@gmail.com>
2022-04-17 20:21:30 +0530

    Renamed in verify function

--- controllers/users.js
+++ controllers/users.js
@@ -10,10 +10,10 @@ const { fetch } = require("../utils/fetch");
 const verifyUser = async (req, res) => {
   const userId = req.userData.id;
   try {
-    if (!req.userData?.identityURL) {
-      return res.boom.serverUnavailable("IdentityURL is Missing");
+    if (!req.userData?.profileURL) {
+      return res.boom.serverUnavailable("ProfileURL is Missing");
     }
-    await userQuery.addOrUpdate({ identityStatus: "PENDING" }, userId);
+    await userQuery.addOrUpdate({ profileStatus: "PENDING" }, userId);
   } catch (error) {
     logger.error(`Error while verifying user: ${error}`);
     return res.boom.serverUnavailable("Something went wrong please contact admin");
