2602ffb974b16207bf9234a77a3ecd756223103c
rajat-mehra05 <rajat.m.5278@gmail.com>
2022-05-12 21:03:00 +0530

    fix: :hammer: remove swagger docs from files inside routes folder

--- routes/trading.js
+++ routes/trading.js
@@ -4,47 +4,6 @@ const authenticate = require("../middlewares/authenticate");
 const { trade } = require("../controllers/trading");
 const { newTrade } = require("../middlewares/validators/trading");
 
-/**
- * @swagger
- * /trade/stock/new/self:
- *  post:
- *   summary: Used for new trading request
- *   tags:
- *     - Trading
- *   security:
- *     - bearerAuth: []
- *   requestBody:
- *     description: Trading details
- *     content:
- *       application/json:
- *         schema:
- *           $ref: '#/components/schemas/trading'
- *   responses:
- *     200:
- *       description: Successful trading details
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/tradingSuccess'
- *     401:
- *       description: unAuthorized
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/errors/unAuthorized'
- *     403:
- *       description: forbidden
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/errors/forbidden'
- *     500:
- *       description: badImplementation
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/errors/badImplementation'
- */
 router.post("/stock/new/self", authenticate, newTrade, trade);
 
 module.exports = router;
