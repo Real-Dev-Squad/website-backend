102929d858adb58514c6fa5645e43fb691a0e548
spverma536 <spverma536@gmail.com>
2022-05-15 22:44:10 +0530

    added a comment

--- middlewares/validators/newsletter.js
+++ middlewares/validators/newsletter.js
@@ -2,7 +2,8 @@ const joi = require("joi");
 
 const validateEmail = async (req, res, next) => {
   const schema = joi.object().strict().keys({
-    email: joi.string().required(),
+    email: joi.string().email({ tlds: { allow: false } }),
+    // reason to disable tlds: https://stackoverflow.com/questions/57972358/joi-email-validation
   });
 
   try {
