89d7837e5c86f2e4857026c4d4be19e89c3d9ddd
akash2819 <ashu0863736@gmail.com>
2022-06-13 02:14:53 +0530

    added no flag found testcase

--- test/unit/models/flag.test.js
+++ test/unit/models/flag.test.js
@@ -1,7 +1,7 @@
 /* eslint-disable security/detect-object-injection */
 const chai = require("chai");
 const { expect } = chai;
-
+const should = require("chai").should();
 const cleanDb = require("../../utils/cleanDb");
 
 const flagModel = require("../../../models/flag");
@@ -39,6 +39,11 @@ describe("flags", function () {
       Object.keys(flagData).forEach((key) => {
         expect(flagData[key]).to.deep.equal(fetchedData[key]);
       });
+      expect(fetchedData).to.be.a("Object");
+    });
+    it("Should return  no flag found", async function () {
+      const fetched = await flagFirestore.doc(flagId).get();
+      should.not.equal(fetched.id, 0);
     });
   });
 });
