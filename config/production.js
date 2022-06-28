646e336b8a8629e1de719a1413529798de8534e7
rohan09-raj <rajrohan1914@gmail.com>
2022-06-28 23:21:21 +0530

    added config

--- config/production.js
+++ config/production.js
@@ -6,4 +6,10 @@ module.exports = {
   userToken: {
     cookieName: "rds-session",
   },
+
+  cloudflare: {
+    CLOUDFLARE_ZONE_ID: "123123123123123",
+    CLOUDFLARE_X_AUTH_KEY: "123abc123abc123abc",
+    CLOUDFLARE_X_AUTH_EMAIL: "abc@xyz.com",
+  },
 };
