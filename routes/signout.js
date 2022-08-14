7ae363db7b5a93944a2b2952a080a5ccccd4d0bd
RitikJaiswal75 <ritik12103032000@gmail.com>
2022-08-14 16:01:06 +0530

    changed to post request

--- routes/signout.js
+++ routes/signout.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.clearCookie("rds-session-development");
-  res.redirect("/healthcheck");
+    res.clearCookie("rds-session");
+    res.redirect("/healthcheck");
 });
 
 module.exports = router;
