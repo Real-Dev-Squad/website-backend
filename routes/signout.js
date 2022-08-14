7e645fbeebf5cd81b559e3f9ac2d44b6e6f18988
RitikJaiswal75 <ritik12103032000@gmail.com>
2022-08-14 17:23:46 +0530

    remove redirect

--- routes/signout.js
+++ routes/signout.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  const cookie = req.cookies;
+  const cookie = "rds-session";
   res.clearCookie(cookie);
+  res.end();
 });
 
 module.exports = router;
