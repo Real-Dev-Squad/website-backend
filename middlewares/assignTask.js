44fdf6f6c3d774a2d9ee7225b98921fb787423d6
vinayak <trivedivinayak57@gmail.com>
2022-09-20 12:32:56 +0530

    added try catch block

--- middlewares/assignTask.js
+++ middlewares/assignTask.js
@@ -3,14 +3,18 @@ const db = require("../utils/firestore");
 
 const assignTask = async function (req, res) {
   if (req.body.percentCompleted === 100) {
-    const { task } = await fetchSkillLevelTasks("frontend", 1);
-    if (task) {
-      const docId = task.id;
-      const userId = req.userData.id;
-      db.collection("tasks").doc(docId).set({ assignee: userId, status: "ASSIGNED" }, { merge: true });
-      return res.json({ message: "task updated and another task got assigned" });
-    } else {
-      return res.json({ message: "Task updated but another task not found" });
+    try {
+      const { task } = await fetchSkillLevelTasks("frontend", 1);
+      if (task) {
+        const docId = task.id;
+        const userId = req.userData.id;
+        db.collection("tasks").doc(docId).set({ assignee: userId, status: "ASSIGNED" }, { merge: true });
+        return res.json({ message: "task updated and another task got assigned" });
+      } else {
+        return res.json({ message: "Task updated but another task not found" });
+      }
+    } catch {
+      return res.boom.badImplementation("Something went wrong!");
     }
   } else {
     return res.json({ message: "Task updated successfully!" });
