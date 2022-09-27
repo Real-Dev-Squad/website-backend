38581ba35884657a076994ecef6dc95f4b245384
vinayak <trivedivinayak57@gmail.com>
2022-09-27 22:28:01 +0530

    refactored the code a bit

--- middlewares/assignTask.js
+++ middlewares/assignTask.js
@@ -1,17 +1,16 @@
 const { fetchSkillLevelTask } = require("../models/tasks");
-const db = require("../utils/firestore");
+const firestore = require("../utils/firestore");
+const tasks = firestore.collection("tasks");
 
 const assignTask = async function (req, res) {
   try {
     const { task } = await fetchSkillLevelTask("frontend", 1);
-    if (task) {
-      const docId = task.id;
-      const userId = req.userData.id;
-      db.collection("tasks").doc(docId).set({ assignee: userId, status: "ASSIGNED" }, { merge: true });
-      return res.json({ message: "task updated and another task got assigned" });
-    } else {
-      return res.json({ message: "Task updated but another task not found" });
-    }
+    if (!task) return res.json({ message: "Task updated but another task not found" });
+
+    const docId = task.id;
+    const userId = req.userData.id;
+    await tasks.doc(docId).set({ assignee: userId, status: "ASSIGNED" }, { merge: true });
+    return res.json({ message: "task updated and another task got assigned" });
   } catch {
     return res.boom.badImplementation("Something went wrong!");
   }
