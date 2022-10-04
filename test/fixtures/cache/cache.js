ec3ea99b0a3346e71d4b5fdd3f97cc025751bf7f
yash <yashraj0207@outlook.com>
2022-10-04 19:09:53 +0530

    fixed eslint errors

--- test/fixtures/cache/cache.js
+++ test/fixtures/cache/cache.js
@@ -1,9 +1,10 @@
 const tasksList = require("../tasks/tasks");
 
-const DEFAULT_EXPIRY_TIME_MIN = 2
+const EXPIRY_TIME_IN_MIN = 2;
+const INVALID_KEY = "keyNotPresentInCache";
 
 // Returns a dummy sample value to be stored in cache
-const getDummyResponse = (expiryTime = DEFAULT_EXPIRY_TIME_MIN) => {
+const getDummyResponse = (expiryTime = EXPIRY_TIME_IN_MIN) => {
   const apiResponse = { message: "Tasks returned successfully!", tasks: tasksList() };
   const stringifiedResponse = JSON.stringify(apiResponse);
 
@@ -15,4 +16,4 @@ const getDummyResponse = (expiryTime = DEFAULT_EXPIRY_TIME_MIN) => {
   };
 };
 
-module.exports = { getDummyResponse };
+module.exports = { getDummyResponse, INVALID_KEY };
--- test/unit/utils/cache.test.js
+++ test/unit/utils/cache.test.js
@@ -1,13 +1,17 @@
 const { expect } = require("chai");
 
 const timeData = require("../../fixtures/time/time");
-const { getDummyResponse } = require("../../fixtures/cache/cache");
+const { getDummyResponse, INVALID_KEY } = require("../../fixtures/cache/cache");
 
 const { cachePool, minutesToMilliseconds } = require("../../../utils/cache");
 
 describe("cache pool", function () {
   const testKey = "__cache__/testkey";
-  const pool = cachePool();
+  let pool;
+
+  before(function () {
+    pool = cachePool();
+  });
 
   describe("minutesToMilliSeconds", function () {
     it("should convert minutes and return in milliseconds", function () {
@@ -21,21 +25,21 @@ describe("cache pool", function () {
   describe("cachePool.set", function () {
     it("should add an item to cache pool and return value `true`", async function () {
       const response = await pool.set(testKey, getDummyResponse());
-      expect(response).to.be.true;
+      expect(response).to.be.equal(true);
     });
   });
 
   describe("cachePool.get", function () {
     it("should return value `null` if key is not present in cache", function () {
-      const value = pool.get("dummyInvalidKey");
-      expect(value).to.be.null;
+      const value = pool.get(INVALID_KEY);
+      expect(value).to.be.equal(null);
     });
 
     it("successfully read from cache pool", function () {
       const value = pool.get(testKey);
 
       expect(value).to.have.all.keys("message", "tasks");
-      // commenting it for now 
+      // commenting it for now
       // this test will fail due to a bug in the cache module.
       // expect(pool.hits).to.be.equal(1);
     });
@@ -44,7 +48,7 @@ describe("cache pool", function () {
       pool.set(testKey, getDummyResponse(-10));
       const response = pool.get(testKey);
 
-      expect(response).to.be.null;
+      expect(response).to.be.equal(null);
     });
   });
 });
