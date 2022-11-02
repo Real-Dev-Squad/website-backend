49e86b968d20bf81a33ba3077626ad0d3afc5f05
Mahima Khandelwal <mahi.khandelwal28@gmail.com>
2022-11-02 21:49:35 +0530

    fix lint error

--- config/test.js
+++ config/test.js
@@ -31,9 +31,9 @@ module.exports = {
       baseUrl: `http://localhost:${port}`,
     },
   },
-  
+
   cors: {
-    allowedOrigins: /(https:\/\/([a-zA-Z0-9-_]+\.)?realdevsquad\.com$)|(localhost)/ // Allow realdevsquad.com, *.realdevsquad.com
+    allowedOrigins: /(https:\/\/([a-zA-Z0-9-_]+\.)?realdevsquad\.com$)|(localhost)/, // Allow realdevsquad.com, *.realdevsquad.com and localhost for non-production envs
   },
 
   userToken: {
