3df4c8fe5d1ac75588876f2f7c449442d18ca9b3
DashDeipayan <deipayan.dash8@gmail.com>
2022-11-06 22:04:00 +0530

    fixed joi validation middleware

--- middlewares/validators/tags.js
+++ middlewares/validators/tags.js
@@ -1,20 +1,30 @@
 const Joi = require("joi");
 const { TYPES } = require("../../constants/tags");
 
-const tagValidationSchema = Joi.object({
-  name: Joi.string().trim().required(),
-  type: Joi.string()
-    .uppercase()
-    .custom((value, helper) => {
-      if (!TYPES.includes(value)) return helper.message("Not a valid type");
-      return value;
-    }),
+const createTag = async (req, res, next) => {
+  const schema = Joi.object({
+    name: Joi.string().trim().required(),
+    type: Joi.string()
+      .uppercase()
+      .custom((value, helper) => {
+        if (!TYPES.includes(value)) return helper.message("Not a valid type");
+        return value;
+      }),
 
-  createdBy: Joi.string().required(),
-  date: Joi.date().required(),
-  reason: Joi.string().exist(),
-});
+    createdBy: Joi.string().required(),
+    date: Joi.date().required(),
+    reason: Joi.string().exist(),
+  });
+
+  try {
+    await schema.validateAsync(req.body);
+    next();
+  } catch (error) {
+    logger.error(`Error validating createChallenge payload : ${error}`);
+    res.boom.badRequest(error.details[0].message);
+  }
+};
 
 module.exports = {
-  tagSchema: tagValidationSchema,
+  createTag,
 };
--- routes/tags.js
+++ routes/tags.js
@@ -1,16 +1,12 @@
 const express = require("express");
 
-const { createValidator } = require("express-joi-validation");
 const { addTag, deleteTag, getAllTags } = require("../controllers/tags");
-const { tagSchema } = require("../middlewares/validators/tags");
+const { createTag } = require("../middlewares/validators/tags");
 
 const router = express.Router();
-const validator = createValidator();
 
-router.post("/", validator.body(tagSchema), addTag);
+router.post("/", createTag, addTag);
 router.delete("/:tagid", deleteTag);
 router.get("/", getAllTags);
 
-module.exports = {
-  routes: router,
-};
+module.exports = router;
