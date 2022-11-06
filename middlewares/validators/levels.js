48bb1e5878cd23ccc6352f49255636ae75ad59d4
DashDeipayan <deipayan.dash8@gmail.com>
2022-11-06 21:58:05 +0530

    fixed joi validation

--- middlewares/validators/levels.js
+++ middlewares/validators/levels.js
@@ -1,11 +1,21 @@
 const Joi = require("joi");
 
-const levelValidationSchema = Joi.object({
-  name: Joi.string().trim().required(),
-  createdBy: Joi.string().required(),
-  date: Joi.date().required(),
-});
+const createLevel = async (req, res, next) => {
+  const schema = Joi.object({
+    name: Joi.string().trim().required(),
+    createdBy: Joi.string().required(),
+    date: Joi.date().required(),
+  });
+
+  try {
+    await schema.validateAsync(req.body);
+    next();
+  } catch (error) {
+    logger.error(`Error validating createChallenge payload : ${error}`);
+    res.boom.badRequest(error.details[0].message);
+  }
+};
 
 module.exports = {
-  levelSchema: levelValidationSchema,
+  createLevel,
 };
--- routes/levels.js
+++ routes/levels.js
@@ -1,15 +1,11 @@
 const express = require("express");
-const { createValidator } = require("express-joi-validation");
 const { addLevel, deleteLevel, getAllLevels } = require("../controllers/levels");
-const { levelSchema } = require("../middlewares/validators/levels");
+const { createLevel } = require("../middlewares/validators/levels");
 
 const router = express.Router();
-const validator = createValidator();
 
-router.post("/", validator.body(levelSchema), addLevel);
+router.post("/", createLevel, addLevel);
 router.delete("/:levelid", deleteLevel);
 router.get("/", getAllLevels);
 
-module.exports = {
-  routes: router,
-};
+module.exports = router;
