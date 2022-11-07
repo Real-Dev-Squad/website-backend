3dcb356042be3163952e1e3a2966f5af57ed1eba
DashDeipayan <deipayan.dash8@gmail.com>
2022-11-07 23:06:53 +0530

    added authentication middleware

--- routes/items.js
+++ routes/items.js
@@ -9,15 +9,18 @@ const {
   getItemsBasedOnTagType,
 } = require("../controllers/items");
 const { validateItemsPayload } = require("../middlewares/validators/items");
+const authenticate = require("../middlewares/authenticate");
+const authorizeRoles = require("../middlewares/authorizeRoles");
+const { SUPERUSER } = require("../constants/roles");
 
 const router = express.Router();
 
-router.post("/", validateItemsPayload, addTagsToItem);
-router.delete("/", removeTagsFromItem);
-router.get("/bytype/:type", getItemsBasedOnType);
-router.get("/bytag/:tagid", getItemsBasedOnTagId);
-router.get("/byitem/:itemid", getItemsBasedOnitemId);
-router.get("/bytagtype/:tagtype", getItemsBasedOnTagType);
-router.get("/", getAllItems);
+router.post("/", authenticate, authorizeRoles([SUPERUSER]), validateItemsPayload, addTagsToItem);
+router.delete("/", authenticate, authorizeRoles([SUPERUSER]), removeTagsFromItem);
+router.get("/bytype/:type", authenticate, getItemsBasedOnType);
+router.get("/bytag/:tagid", authenticate, getItemsBasedOnTagId);
+router.get("/byitem/:itemid", authenticate, getItemsBasedOnitemId);
+router.get("/bytagtype/:tagtype", authenticate, getItemsBasedOnTagType);
+router.get("/", authenticate, getAllItems);
 
 module.exports = router;
