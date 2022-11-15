97850c382577d1d0090af737f1195f114d903b5d
Randhir Kumar Singh <97341921+heyrandhir@users.noreply.github.com>
2022-11-15 22:23:24 +0530

    authenticates super user role before deleting the user status for an userid

--- routes/userStatus.js
+++ routes/userStatus.js
@@ -8,12 +8,14 @@ const {
 } = require("../controllers/userStatus");
 const router = express.Router();
 const authenticate = require("../middlewares/authenticate");
+const authorizeRoles = require("../middlewares/authorizeRoles");
+const { SUPERUSER } = require("../constants/roles");
 const { validateUserStatus, validateUpdatedUserStatus } = require("../middlewares/validators/userStatus");
 
+router.post("/:userId", authenticate, validateUserStatus, addUserStatus);
 router.get("/", getAllUserStatus);
 router.get("/:userId", authenticate, getUserStatus);
-router.post("/:userId", authenticate, validateUserStatus, addUserStatus);
-router.delete("/:userId", authenticate, deleteUserStatus);
 router.patch("/:userId", authenticate, validateUpdatedUserStatus, updateUserStatus);
+router.delete("/:userId", authenticate, authorizeRoles([SUPERUSER]), deleteUserStatus);
 
 module.exports = router;
