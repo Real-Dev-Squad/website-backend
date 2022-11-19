eda5a302922cf51c243824609c5c27a88e38a710
YashJain24 <75324135+YashJain24-chief@users.noreply.github.com>
2022-11-19 13:21:28 +0530

    Initial commit

--- models/logs.js
+++ models/logs.js
@@ -35,10 +35,22 @@ const fetchLogs = async (query, param) => {
     let call = logsModel.where("type", "==", param);
     Object.keys(query).forEach((key) => {
       // eslint-disable-next-line security/detect-object-injection
-      call = call.where(key, "==", query[key]);
+      if (key !== "limit" && key !== "lastDocId") {
+        call = call.where(key, "==", query[key]);
+      }
     });
 
-    const snapshot = await call.get();
+    const { limit, lastDocId } = query;
+    let lastDoc;
+    const limitDocuments = Number(limit);
+
+    if (lastDocId) {
+      lastDoc = await logsModel.doc(lastDocId).get();
+    }
+
+    const logsSnapshotQuery = call.orderBy("timestamp", "desc").startAfter(lastDoc ? lastDoc : "");
+    const snapshot = limit ? await logsSnapshotQuery.limit(limitDocuments).get() : await logsSnapshotQuery.get();
+
     const logs = [];
     snapshot.forEach((doc) => {
       logs.push({
