a55217515e87e5117018d603042a2f3dd5b61289
Shubham Bajaj <shmbajaj32@gmail.com>
2022-11-24 01:46:48 +0530

    feat[add]: routes for add, assign and unassign badges

--- routes/badges.js
+++ routes/badges.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const badge = require("../controllers/badge.js");
 
 router.get("/", badge.getBadges);
+router.post("/", badge.postBadge);
 router.get("/:username", badge.getUserBadges);
+router.post("/:username", badge.postUserBadge);
+router.delete("/:username", badge.deleteUserBadge);
 
 module.exports = router;
