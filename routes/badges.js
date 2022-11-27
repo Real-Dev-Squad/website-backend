68e6fbc59f402a805dfff4393096f0346ff4dcc1
Shubham Bajaj <shmbajaj32@gmail.com>
2022-11-27 19:21:53 +0530

    feat: added authorized role

--- routes/badges.js
+++ routes/badges.js
@@ -4,11 +4,32 @@ const authenticate = require("../middlewares/authenticate");
 const badgeValidator = require("../middlewares/validators/badges");
 const { upload } = require("../utils/multer");
 const badge = require("../controllers/badges");
+const authorizeRoles = require("../middlewares/authorizeRoles");
+const { SUPERUSER } = require("../constants/roles");
 
 router.get("/", badge.getBadges);
 router.get("/:username", badge.getUserBadgeIds);
-router.post("/", authenticate, upload.single("badge"), badgeValidator.createBadge, badge.postBadge);
-router.post("/assign/:username", authenticate, badgeValidator.assignOrUnassignBadges, badge.postUserBadges);
-router.delete("/unassign/:username", authenticate, badgeValidator.assignOrUnassignBadges, badge.deleteUserBadges);
+router.post(
+  "/",
+  authenticate,
+  authorizeRoles([SUPERUSER]),
+  upload.single("badge"),
+  badgeValidator.createBadge,
+  badge.postBadge
+);
+router.post(
+  "/assign/:username",
+  authenticate,
+  authorizeRoles([SUPERUSER]),
+  badgeValidator.assignOrUnassignBadges,
+  badge.postUserBadges
+);
+router.delete(
+  "/unassign/:username",
+  authenticate,
+  authorizeRoles([SUPERUSER]),
+  badgeValidator.assignOrUnassignBadges,
+  badge.deleteUserBadges
+);
 
 module.exports = router;
