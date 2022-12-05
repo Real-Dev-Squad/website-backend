bc3493c31d79624dd7510a467f5e12f52f7261d4
Mahima Khandelwal <mahi.khandelwal28@gmail.com>
2022-12-05 23:05:21 +0530

    remove date conversion

--- controllers/userStatus.js
+++ controllers/userStatus.js
@@ -68,35 +68,7 @@ const getUserStatus = async (req, res) => {
 
 const getSortedUsers = (allUsers) => {
   const sorted = allUsers.sort((a, b) => a.currentStatus.from - b.currentStatus.from);
-  const today = new Date();
-  sorted.forEach((data) => {
-    const idleSincedate = new Date(data.currentStatus.from);
-    const dateArray = idleSincedate.toISOString().split("-");
-    const year = today.getFullYear();
-    const month = today.getMonth() + 1;
-    const day = today.getDate();
-    const yy = parseInt(dateArray[0]);
-    const mm = parseInt(dateArray[1]);
-    const dd = parseInt(dateArray[2]);
-    let years, months;
-    // months
-    months = month - mm;
-    if (day < dd) {
-      months = months - 1;
-    }
-    // years
-    years = year - yy;
-    if (month * 100 + day < mm * 100 + dd) {
-      years = years - 1;
-      months = months + 12;
-    }
-    // days
-    const days = Math.floor(
-      (today.getTime() - new Date(yy + years, mm + months - 1, dd).getTime()) / (24 * 60 * 60 * 1000)
-    );
-    //
-    data.currentStatus.from = `${years} years , ${months} months , ${days} days`;
-  });
+  return sorted;
 };
 
 /**
