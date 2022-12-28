c259e5e6156f0076b329e41e88d4fa1817fa38d6
Satyam <bajpai1973satyam@gmail.com>
2022-12-28 19:10:36 +0530

    wrote test for join data already present

--- test/integration/users.test.js
+++ test/integration/users.test.js
@@ -17,7 +17,6 @@ const config = require("config");
 const joinData = require("../fixtures/user/join");
 const { addJoinData, addOrUpdate } = require("../../models/users");
 const cookieName = config.get("userToken.cookieName");
-
 chai.use(chaiHttp);
 
 describe("Users", function () {
@@ -457,6 +456,24 @@ describe("Users", function () {
   });
 
   describe("PUT /users/self/intro", function () {
+    it("should return 409 if the data already present", function (done) {
+      chai
+        .request(app)
+        .put(`/users/self/intro`)
+        .set("Cookie", `${cookieName}=${jwt}`)
+        .end((err, res) => {
+          if (err) {
+            return done(err);
+          }
+          if (joinData()[1].length === 1) {
+            expect(res).to.have.status(409);
+            expect(res.body).to.be.a("object");
+            expect(res.body.message).to.equal("User data is already present!");
+          }
+          return done();
+        });
+    });
+
     it("Should store the info in db", function (done) {
       chai
         .request(app)
