d935f6802a95e7f2d9ee71cd84957e0c0548d0af
shubham bajaj <shmbajaj32@gmail.com>
2023-01-14 15:52:17 +0530

    feat: bulk write create or delete documents

--- utils/badges.js
+++ utils/badges.js
@@ -1,3 +1,6 @@
+const firestore = require("./firestore");
+const userBadgeModel = firestore.collection("userBadges");
+
 /**
  * Convert firebase timestamp to locale date and time
  * @param  createdAt: firebase timestamp
@@ -37,7 +40,33 @@ function convertFirebaseDocumentToBadgeDocument(id, data) {
   };
 }
 
+/**
+ * Creates or Deletes bulk document in batch
+ * @param { Object<userId: string, array: Array<badgeId|docReferrence>, isUnassign: boolean> } user-id to assign or unassign badges, array: badgeIds or documentReferrences and isUnassign boolean flag to unassign badges(defualt value is `false`).
+ * @return { Promise<void> }
+ */
+/**
+ * [1]: https://github.com/FrangSierra/firestore-cloud-functions-typescript/blob/master/functions/src/atomic-operations/index.ts#L52
+ * [2]: https://stackoverflow.com/questions/49121877/upload-more-than-500-documents-to-firestore-database-from-cloud-function (last-answer)
+ * [3]: https://firebase.google.com/docs/firestore/quotas#writes_and_transactions
+ * [4]: https://firebase.google.com/docs/firestore/quotas#security_rules
+ * [5]: https://firebase.google.com/docs/firestore/manage-data/transactions#security_rules_limits
+ */
+// TODO: write generic function
+async function assignUnassignBadgesInBulk({ userId, array, isUnassign = false }) {
+  const bulkWriter = firestore.bulkWriter();
+  array.forEach((value) => {
+    if (isUnassign) {
+      bulkWriter.delete(value);
+      return;
+    }
+    bulkWriter.create(userBadgeModel.doc(), { userId, badgeId: value });
+  });
+  return await bulkWriter.close();
+}
+
 module.exports = {
   convertFirebaseDocumentToBadgeDocument,
   convertFirebaseTimestampToDateTime,
+  assignUnassignBadgesInBulk,
 };
