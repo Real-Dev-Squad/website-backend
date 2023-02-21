2e4091c419f8ad7902927ba10f48de7354d38459
rohan09-raj <rajrohan1914@gmail.com>
2023-02-21 13:28:04 +0530

    removed redudant loop

--- models/external-accounts.js
+++ models/external-accounts.js
@@ -14,23 +14,24 @@ const addExternalAccountData = async (data) => {
 const fetchExternalAccountData = async (query, param) => {
   try {
     const userExternalAccountData = [];
-
     let externalAccountData;
-    externalAccountData = await externalAccountsModel.where("token", "==", param);
 
-    Object.keys(query).forEach((key) => {
-      if (key === "type") {
-        externalAccountData = externalAccountData.where(key, "==", query?.key);
-      }
-    });
+    externalAccountData = externalAccountsModel.where("token", "==", param);
 
-    externalAccountData = await externalAccountData.limit(1).get();
+    if (query && query?.type) {
+      externalAccountData = externalAccountData.where("type", "==", query?.type);
+    }
 
-    externalAccountData.forEach((data) => {
-      userExternalAccountData.push({
-        id: data.id,
-        ...data.data(),
-      });
+    const querySnapshot = await externalAccountData.limit(1).get();
+
+    if (querySnapshot.empty) {
+      return userExternalAccountData;
+    }
+
+    const data = querySnapshot.docs[0];
+    userExternalAccountData.push({
+      id: data.id,
+      ...data.data(),
     });
 
     return userExternalAccountData;
