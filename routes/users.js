639c020f6b27a9d9824e3b8d461c6b4c347edfba
Ujjawal Kumar <79056054+endeavourmonk@users.noreply.github.com>
2023-03-25 10:46:05 +0530

    updating routes
    users/userId/{userId} to users/{userId}

--- routes/users.js
+++ routes/users.js
@@ -9,23 +9,22 @@ const { upload } = require("../utils/multer");
 const { getUserBadges } = require("../controllers/badges");
 
 router.post("/verify", authenticate, users.verifyUser);
-router.get("/userId/:userId", users.getUserById);
+router.get("/users/:userId", users.getUserById);
 router.patch("/self", authenticate, userValidator.updateUser, users.updateSelf);
 router.get("/", authenticate, userValidator.getUsers, users.getUsers);
 router.get("/self", authenticate, users.getSelfDetails);
 router.get("/isUsernameAvailable/:username", authenticate, users.getUsernameAvailabilty);
-router.get("/chaincode", authenticate, users.generateChaincode);
-router.get("/:username", users.getUser);
-router.get("/:userId/intro", authenticate, authorizeRoles([SUPERUSER]), users.getUserIntro);
+router.get("/users/:username", users.getUser);
+router.get("/users/:userId/intro", authenticate, authorizeRoles([SUPERUSER]), users.getUserIntro);
 router.put("/self/intro", authenticate, userValidator.validateJoinData, users.addUserIntro);
-router.get("/:id/skills", users.getUserSkills);
-router.get("/:id/badges", getUserBadges);
+router.get("/users/:id/skills", users.getUserSkills);
+router.get("/users/:id/badges", getUserBadges);
 
 // upload.single('profile') -> multer inmemory storage of file for type multipart/form-data
 router.post("/picture", authenticate, upload.single("profile"), users.postUserPicture);
 router.patch("/profileURL", authenticate, userValidator.updateProfileURL, users.profileURL);
 router.patch("/rejectDiff", authenticate, authorizeRoles([SUPERUSER]), users.rejectProfileDiff);
-router.patch("/:userId", authenticate, authorizeRoles([SUPERUSER]), users.updateUser);
-router.get("/suggestedUsers/:skillId", authenticate, authorizeRoles([SUPERUSER]), users.getSuggestedUsers);
+router.patch("/users/:userId", authenticate, authorizeRoles([SUPERUSER]), users.updateUser);
+router.get("/users/suggestedUsers/:skillId", authenticate, authorizeRoles([SUPERUSER]), users.getSuggestedUsers);
 
 module.exports = router;
