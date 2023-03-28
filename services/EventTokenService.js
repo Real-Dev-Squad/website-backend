6f673cfd97b9d44873cc9d791302198d8bf993d9
prerana1821 <prerananawar1@gmail.com>
2023-03-28 05:31:12 +0530

    feat: initial setup for 100ms

---
+++ services/EventTokenService.js
@@ -0,0 +1,69 @@
+const jwt = require("jsonwebtoken");
+const uuid4 = require("uuid4");
+
+// A service class for Token generation and management
+logger.info(config.get("Event100ms.APP_ACCESS_KEY"));
+
+class EventTokenService {
+  static #app_access_key = config.get("Event100ms.APP_ACCESS_KEY");
+  static #app_secret = config.get("Event100ms.APP_SECRET");
+  #managementToken;
+  constructor() {
+    this.#managementToken = this.getManagementToken(true);
+  }
+
+  // A private method that uses JWT to sign the payload with APP_SECRET
+  #signPayloadToToken(payload) {
+    const token = jwt.sign(payload, EventTokenService.#app_secret, {
+      algorithm: "HS256",
+      expiresIn: "24h",
+      jwtid: uuid4(),
+    });
+    return token;
+  }
+
+  // A private method to check if a JWT token has expired or going to expire soon
+  #isTokenExpired(token) {
+    try {
+      const { exp } = jwt.decode(token);
+      const buffer = 30; // generate new if it's going to expire soon
+      const currTimeSeconds = Math.floor(Date.now() / 1000);
+      return !exp || exp + buffer < currTimeSeconds;
+    } catch (err) {
+      logger.info("error in decoding token", err);
+      return true;
+    }
+  }
+
+  // Generate new Management token, if expired or forced
+  getManagementToken(forceNew) {
+    if (forceNew || this.#isTokenExpired(this.#managementToken)) {
+      const payload = {
+        access_key: EventTokenService.#app_access_key,
+        type: "management",
+        version: 2,
+        iat: Math.floor(Date.now() / 1000),
+        nbf: Math.floor(Date.now() / 1000),
+      };
+      this.#managementToken = this.#signPayloadToToken(payload);
+    }
+    return this.#managementToken;
+  }
+
+  // Generate new Auth token for a peer
+  getAuthToken({ roomId, userId, role }) {
+    const payload = {
+      access_key: EventTokenService.#app_access_key,
+      room_id: roomId,
+      user_id: userId,
+      role: role,
+      type: "app",
+      version: 2,
+      iat: Math.floor(Date.now() / 1000),
+      nbf: Math.floor(Date.now() / 1000),
+    };
+    return this.#signPayloadToToken(payload);
+  }
+}
+
+module.exports = { EventTokenService };
