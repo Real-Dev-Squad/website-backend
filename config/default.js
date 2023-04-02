201cf6ec66ff54a263d32686d67a8d1bb484f78c
khalatevarun <khalatevarun@gmail.com>
2023-04-03 03:14:39 +0530

    fix: revert default.js changes

--- config/default.js
+++ config/default.js
@@ -49,7 +49,7 @@ module.exports = {
   },
 
   cors: {
-    allowedOrigins: /(http:\/\/localhost:[0-9]{4}$)/, // Allow realdevsquad.com, *.realdevsquad.com
+    allowedOrigins: /(https:\/\/([a-zA-Z0-9-_]+\.)?realdevsquad\.com$)/, // Allow realdevsquad.com, *.realdevsquad.com
   },
 
   userToken: {
