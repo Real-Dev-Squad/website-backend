14251f67142c0c39957402a2febe519525752a53
Bharati Subramanian <bharatisharada@gmail.com>
2023-04-06 19:01:57 +0530

    refactor: remove route path param

--- routes/issues.js
+++ routes/issues.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const issues = require("../controllers/issues");
 
-router.get("/:repo", issues.getIssues);
+router.get("/", issues.getIssues);
 router.post("/updates", issues.issueUpdates);
 
 module.exports = router;
