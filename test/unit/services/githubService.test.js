aa7c8454f616abf84352667a08c2a8413b7e4de5
khalatevarun <khalatevarun@gmail.com>
2023-04-08 21:54:48 +0530

    add unit tests to fetch correct url with additional params for github services

--- test/unit/services/githubService.test.js
+++ test/unit/services/githubService.test.js
@@ -27,6 +27,23 @@ describe("githubService", function () {
     });
   });
 
+  describe("fetchOpenPRs with additional params", function () {
+    it("Should generate the correct url to fetch open PRs", async function () {
+      const params = {
+        searchParams: {
+          created: "2023-01-01..2023-02-01",
+        },
+        resultOptions: {
+          order: "desc",
+        },
+      };
+      const response = await githubService.fetchOpenPRs(params);
+      expect(response).to.be.equal(
+        "https://api.github.com/search/issues?q=org%3AReal-Dev-Squad+type%3Apr+is%3Aopen+created%3A2023-01-01..2023-02-01&sort=created&order=desc&per_page=100&page=1"
+      );
+    });
+  });
+
   describe("fetchPRsByUser", function () {
     it("Should generate the correct url to fetch PRs by given user", async function () {
       await addUser();
@@ -46,6 +63,23 @@ describe("githubService", function () {
     });
   });
 
+  describe("fetchMergedPRs with additional params", function () {
+    it("Should generate the correct url to fetch merged Prs", async function () {
+      const params = {
+        searchParams: {
+          updated: "<=2023-01-01",
+        },
+        resultOptions: {
+          order: "asc",
+        },
+      };
+      const response = await githubService.fetchMergedPRs(params);
+      expect(response).to.be.equal(
+        "https://api.github.com/search/issues?q=org%3AReal-Dev-Squad+type%3Apr+is%3Amerged+merged%3A%3C%3D2023-01-01&sort=updated&order=asc&per_page=100&page=1"
+      );
+    });
+  });
+
   describe("fetchOpenIssues", function () {
     it("Should generate the correct url to fetch open issues", async function () {
       const response = await githubService.fetchOpenIssues();
@@ -55,11 +89,45 @@ describe("githubService", function () {
     });
   });
 
+  describe("fetchOpenIssues with additional params", function () {
+    it("Should generate the correct url to fetch open issues", async function () {
+      const params = {
+        searchParams: {
+          created: ">=2023-01-01",
+        },
+        resultOptions: {
+          order: "desc",
+        },
+      };
+      const response = await githubService.fetchOpenIssues(params);
+      expect(response).to.be.equal(
+        "https://api.github.com/search/issues?q=org%3AReal-Dev-Squad+type%3Aissue+is%3Aopen+created%3A%3E%3D2023-01-01&sort=created&order=desc&per_page=100&page=1"
+      );
+    });
+  });
+
   describe("fetchClosedIssues", function () {
     it("Should generate the correct url to fetch closed issues", async function () {
       const response = await githubService.fetchClosedIssues();
       expect(response).to.be.equal(
         "https://api.github.com/search/issues?q=org%3AReal-Dev-Squad+type%3Aissue+is%3Aclosed&sort=updated&per_page=100&page=1"
+      );
+    });
+  });
+
+  describe("fetchClosedIssues with additional params", function () {
+    it("Should generate the correct url to fetch closed issues", async function () {
+      const params = {
+        searchParams: {
+          closed: "2023-01-01..2023-02-01",
+        },
+        resultOptions: {
+          order: "desc",
+        },
+      };
+      const response = await githubService.fetchClosedIssues(params);
+      expect(response).to.be.equal(
+        "https://api.github.com/search/issues?q=org%3AReal-Dev-Squad+type%3Aissue+is%3Aclosed+closed%3A2023-01-01..2023-02-01&sort=updated&order=desc&per_page=100&page=1"
       );
     });
   });
