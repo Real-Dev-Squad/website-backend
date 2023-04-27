e670fe09f98b0868888aef59716c00512ce856cb
shubhamsigdar1 <shubhamsigdar@gmail.com>
2023-04-27 18:30:03 +0530

    fix lint issue

--- test/fixtures/userStatus/userStatus.js
+++ test/fixtures/userStatus/userStatus.js
@@ -51,18 +51,18 @@ const generateUserStatusData = (state, updatedAt, from, until = "", message = ""
   };
 };
 
-const generateMonthlyHours = (committed=0) =>{
+const generateMonthlyHours = (committed = 0) => {
   return {
     monthlyHours: {
       committed,
     },
   };
-}
+};
 
 module.exports = {
   userStatusDataForNewUser,
   userStatusDataForOooState,
   oooStatusDataForShortDuration,
   generateUserStatusData,
-  generateMonthlyHours
+  generateMonthlyHours,
 };
