e54700921b8dd4056b4d3fd3f26c681c28be39ac
Randhir Kumar Singh <97341921+heyrandhir@users.noreply.github.com>
2023-05-07 01:07:41 +0530

    refactor the validator file

--- middlewares/validators/progresses.js
+++ middlewares/validators/progresses.js
@@ -1,29 +1,46 @@
 const joi = require("joi");
 
 const validateProgresses = async (req, res, next) => {
-  const schema = joi
+  const baseSchema = joi
     .object()
     .strict()
     .keys({
-      type: joi.string().valid("user", "task").required(),
-      completed: joi.string().required(),
-      planned: joi.string().required(),
-      blockers: joi.string().allow("").required(),
-    });
+      type: joi.string().trim().valid("user", "task").required().messages({
+        "any.required": "Required field 'type' is missing.",
+        "any.only": "Type field is restricted to either 'user' or 'task'.",
+      }),
+      completed: joi.string().trim().required().messages({
+        "any.required": "Required field 'completed' is missing.",
+        "string.trim": "completed must not have leading or trailing whitespace",
+      }),
+      planned: joi.string().trim().required().messages({
+        "any.required": "Required field 'planned' is missing.",
+        "string.trim": "planned must not have leading or trailing whitespace",
+      }),
+      blockers: joi.string().trim().allow("").required().messages({
+        "any.required": "Required field 'blockers' is missing.",
+        "string.trim": "blockers must not have leading or trailing whitespace",
+      }),
+    })
+    .messages({ "object.unknown": "Invalid field provided." });
 
-  if (req.body.type === "task") {
-    schema.keys({ taskId: joi.string().required() });
-  }
+  const taskSchema = joi.object().keys({
+    taskId: joi.string().trim().required().messages({
+      "any.required": "Required field 'taskId' is missing.",
+      "string.trim": "taskId must not have leading or trailing whitespace",
+    }),
+  });
+  const schema = req.body.type === "task" ? baseSchema.concat(taskSchema) : baseSchema;
 
   try {
-    await schema.validateAsync(req.body);
+    await schema.validateAsync(req.body, { abortEarly: false });
     next();
   } catch (error) {
-    logger.error(`Error validating createStock payload: ${error}`);
+    logger.error(`Error validating payload: ${error}`);
     res.boom.badRequest(error.details[0].message);
   }
 };
 
 module.exports = {
-  createStock: validateProgresses,
+  validateProgresses,
 };
