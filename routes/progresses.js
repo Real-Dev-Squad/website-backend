2e71dc58e7575b9886efbfe31ce5514a62dc5fc9
Randhir Kumar Singh <97341921+heyrandhir@users.noreply.github.com>
2023-05-07 15:25:23 +0530

    updates the controller fn name in route

--- routes/progresses.js
+++ routes/progresses.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const authenticate = require("../middlewares/authenticate");
 const { validateProgresses } = require("../middlewares/validators/progresses");
-const { addProgresses } = require("../controllers/progresses");
+const { createProgress } = require("../controllers/progresses");
 
-router.post("/", authenticate, validateProgresses, addProgresses);
+router.post("/", authenticate, validateProgresses, createProgress);
 
 module.exports = router;
