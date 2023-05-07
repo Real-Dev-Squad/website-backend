c280aaa1b1938dc8b69a097b337ae6f53c68963f
Randhir Kumar Singh <97341921+heyrandhir@users.noreply.github.com>
2023-05-07 14:51:27 +0530

    create model and controller function for POST

--- controllers/progresses.js
+++ controllers/progresses.js
@@ -1,13 +1,25 @@
+const { createProgressDocument } = require("../models/progresses");
+
 /**
  * Add Progresses Document
  *
  * @param req {Object} - Express request object
  * @param res {Object} - Express response object
  */
-const addProgresses = async (req, res) => {
-  return res.json({
-    message: "Progresses document created successfully.",
-  });
+const createProgress = async (req, res) => {
+  try {
+    const data = await createProgressDocument({ ...req.body, userId: req.userData.id });
+    return res.json({
+      data,
+      message: `${
+        req.body.type.charAt(0).toUpperCase() + req.body.type.slice(1)
+      } Progress document created successfully.`,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      message: error.message,
+    });
+  }
 };
 
-module.exports = { addProgresses };
+module.exports = { createProgress };
---
+++ models/progresses.js
@@ -0,0 +1,44 @@
+const firestore = require("../utils/firestore");
+const progressesCollection = firestore.collection("progresses");
+const { fetchTask } = require("./tasks");
+
+/**
+ * Adds a new progress document for the given user or task, with a limit of one progress document per day.
+ * @param progressData {object} The data to be added. It should be an object containing key-value pairs of the fields to be added, including a "type" field set to either "user" or "task".
+ * @returns {Promise<object>} A Promise that resolves with the added progress document object, or rejects with an error object if the add operation fails.
+ * @throws {Error} If a progress document has already been created for the given user or task on the current day.
+ **/
+
+const createProgressDocument = async (progressData) => {
+  const { type, userId, taskId } = progressData;
+  const createdAtTimestamp = new Date().getTime();
+  const currentHourIST = new Date().getUTCHours() + 5.5; // IST offset is UTC+5:30
+  const isBefore6amIST = currentHourIST < 6;
+  const progressDateTimestamp = isBefore6amIST
+    ? new Date().setUTCHours(0, 0, 0, 0) - 24 * 60 * 60 * 1000
+    : new Date().setUTCHours(0, 0, 0, 0);
+
+  if (type === "task") {
+    const { taskData } = await fetchTask(taskId);
+    if (!taskData) {
+      throw new Error(`Task with id ${taskId} does not exist`);
+    }
+  }
+
+  const query =
+    type === "user"
+      ? progressesCollection.where("userId", "==", userId)
+      : progressesCollection.where("taskId", "==", taskId);
+
+  const existingDocumentSnapshot = await query.where("date", "==", progressDateTimestamp).get();
+
+  if (!existingDocumentSnapshot.empty) {
+    throw new Error(`${type.charAt(0).toUpperCase() + type.slice(1)} Progress for the day has already been created`);
+  }
+
+  const progressDocument = { ...progressData, createdAt: createdAtTimestamp, date: progressDateTimestamp };
+  const { id } = await progressesCollection.add(progressDocument);
+  return { ...progressDocument, id };
+};
+
+module.exports = { createProgressDocument };
