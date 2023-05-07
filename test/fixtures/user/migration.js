7353c587e4e088c4230274a182c04f17401e0159
sinhayash9546 <sinhayash9546@gmail.com>
2023-05-08 05:20:12 +0530

    fixed tests and added migration fixtures

---
+++ test/fixtures/user/migration.js
@@ -0,0 +1,39 @@
+const githubUserInfo = require("../auth/githubUserInfo")();
+
+/**
+ * User info for GitHub auth response
+ * Multiple responses can be added to the array if required
+ *
+ * @return {Object}
+ */
+module.exports = () => {
+  return [
+    {
+      username: "ankur",
+      first_name: "Ankur",
+      last_name: "Narkhede",
+      yoe: 0,
+      img: "./img.png",
+      linkedin_id: "ankurnarkhede",
+      github_user_id: githubUserInfo[0].id,
+      github_id: githubUserInfo[0].username,
+      github_display_name: githubUserInfo[0].displayName,
+      isMember: true,
+      phone: "1234567890",
+      email: "abc@gmail.com",
+      roles: {
+        member: true,
+      },
+      tokens: {
+        githubAccessToken: "githubAccessToken",
+      },
+      status: "active",
+      profileURL: "https://abcde.com",
+      picture: {
+        publicId: "profile/mtS4DhUvNYsKqI7oCWVB/aenklfhtjldc5ytei3ar",
+        url: "https://res.cloudinary.com/realdevsquad/image/upload/v1667685133/profile/mtS4DhUvNYsKqI7oCWVB/aenklfhtjldc5ytei3ar.jpg",
+      },
+      incompleteUserDetails: false,
+    },
+  ];
+};
