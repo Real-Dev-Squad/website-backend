2265cee0b3f5803a203c78552d349e570502df5a
Randhir Kumar Singh <97341921+heyrandhir@users.noreply.github.com>
2023-05-12 09:10:34 +0530

    Used JSDoc annotations to provide type information for the controller functions

--- controllers/progresses.js
+++ controllers/progresses.js
@@ -4,11 +4,40 @@ const { RESPONSE_MESSAGES } = require("../constants/progresses");
 const { PROGRESS_DOCUMENT_RETRIEVAL_SUCCEEDED, PROGRESS_DOCUMENT_CREATED_SUCCEEDED } = RESPONSE_MESSAGES;
 
 /**
- * Adds Progress Document
- *
- * @param req {Object} - Express request object
- * @param res {Object} - Express response object
+ * @typedef {Object} ProgressRequestBody
+ * @property {string} type - The type of progress document.
+ * @property {string} completed - The completed progress.
+ * @property {string} planned - The planned progress.
+ * @property {string} blockers - The blockers.
+ * @property {string} [taskId] - The task ID (optional).
  */
+
+/**
+ * @typedef {Object} ProgressDocument
+ * @property {string} type - The type of progress document.
+ * @property {string} completed - The completed progress.
+ * @property {string} planned - The planned progress.
+ * @property {string} blockers - The blockers.
+ * @property {string} userId - The User ID
+ * @property {string} [taskId] - The task ID (optional).
+ * @property {number} createdAt - The timestamp when the progress document was created.
+ * @property {number} date - The timestamp for the day the progress document was created.
+ */
+
+/**
+ * @typedef {Object} ProgressResponse
+ * @property {ProgressDocument} data - The progress document data.
+ * @property {string} message - The success message.
+ */
+
+/**
+ * Creates a new progress document.
+ * @param {Object} req - The HTTP request object.
+ * @param {ProgressRequestBody} req.body - The progress document data.
+ * @param {Object} res - The HTTP response object.
+ * @returns {Promise<void>} A Promise that resolves when the response is sent.
+ */
+
 const createProgress = async (req, res) => {
   const {
     body: { type },
@@ -36,11 +65,39 @@ const createProgress = async (req, res) => {
 };
 
 /**
- * Gets Progress Document
- *
- * @param req {Object} - Express request object
- * @param res {Object} - Express response object
+ * @typedef {Object} ProgressQueryParams
+ * @property {string} [type] - The type of progress document.
+ * @property {string} [taskId] - The task ID (optional).
+ * @property {string} [userId] - The user ID (optional).
  */
+
+/**
+ * @typedef {Object} ProgressDocument
+ * @property {string} type - The type of progress document.
+ * @property {string} completed - The completed progress.
+ * @property {string} planned - The planned progress.
+ * @property {string} blockers - The blockers.
+ * @property {string} userId - The User ID
+ * @property {string} [taskId] - The task ID (optional).
+ * @property {number} createdAt - The timestamp when the progress document was created.
+ * @property {number} date - The timestamp for the day the progress document was created.
+ */
+
+/**
+ * @typedef {Object} GetProgressResponse
+ * @property {string} message - The success message.
+ * @property {number} count - The no of progress documents retrieved
+ * @property {[ProgressDocument]} data - An array of progress documents
+ */
+
+/**
+ * Retrieves the progress documents based on provided query parameters.
+ * @param {Object} req - The HTTP request object.
+ * @param {ProgressQueryParams} req.query - The query parameters
+ * @param {Object} res - The HTTP response object.
+ * @returns {Promise<void>} A Promise that resolves when the response is sent.
+ */
+
 const getProgress = async (req, res) => {
   try {
     const data = await getProgressDocument(req.query);
@@ -62,11 +119,39 @@ const getProgress = async (req, res) => {
 };
 
 /**
- * Gets Progress Records within the specified date range
- *
- * @param req {Object} - Express request object
- * @param res {Object} - Express response object
+ * @typedef {Object} ProgressQueryParams
+ * @property {string} [taskId] - The task ID (optional).
+ * @property {string} [userId] - The user ID (optional).
+ * @property {string} startDate - The start date of the date range to retrieve progress records for.
+ * @property {string} endDate - The end date of the date range to retrieve progress records for.
  */
+
+/**
+ * @typedef {Object} progressRecord
+ * @property {boolean} date - the boolean indicating whether the progress was recorded or not for that date
+/**
+
+/**
+ * @typedef {Object} ProgressRangeData
+ * @property {string} startDate - the start date for the progress records
+ * @property {string} endDate - the end date for the progress records
+ * @property {Object.<string, progressRecord>} progressRecords - An object where the keys are dates and the values are progress records.
+/**
+
+/**
+ * @typedef {Object} GetProgressRangeDataResponse
+ * @property {string} message - The success message.
+ * @property {ProgressRangeData} data - The progress range data.
+ */
+
+/**
+ * Retrieves the progress documents based on provided query parameters.
+ * @param {Object} req - The HTTP request object.
+ * @param {ProgressQueryParams} req.query - The query parameters
+ * @param {Object} res - The HTTP response object.
+ * @returns {Promise<void>} A Promise that resolves when the response is sent.
+ */
+
 const getProgressRangeData = async (req, res) => {
   try {
     const data = await getRangeProgressData(req.query);
