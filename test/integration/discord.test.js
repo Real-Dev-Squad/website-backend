6d6a425aafc12d7ddafe3bbc3dd95a3a86e62919
ritikjaiswal75 <ritik12103032000@gmail.com>
2023-06-05 02:03:40 +0530

    add test to cover foe unarchived and active users

--- test/integration/discord.test.js
+++ test/integration/discord.test.js
@@ -6,48 +6,73 @@ const addUser = require("../utils/addUser");
 const cleanDb = require("../utils/cleanDb");
 const authService = require("../../services/authService");
 const userData = require("../fixtures/user/user")();
-const { roleData } = require("../fixtures/discordactions/discordactions");
+const { requestRoleData } = require("../fixtures/discordactions/discordactions");
 
 const cookieName = config.get("userToken.cookieName");
 
 let userId;
 let jwt;
+
 describe("test discord actions", function () {
-  beforeEach(async function () {
-    userId = await addUser(userData[4]);
-    jwt = authService.generateAuthToken({ userId });
+  describe("test discord actions for archived users", function (done) {
+    beforeEach(async function () {
+      userId = await addUser(userData[5]);
+      jwt = authService.generateAuthToken({ userId });
+    });
+
+    afterEach(async function () {
+      await cleanDb();
+    });
+
+    it("returns 403 for archived users post method", function (done) {
+      chai
+        .request(app)
+        .post("/discord-actions/groups")
+        .set("Cookie", `${cookieName}=${jwt}`)
+        .send(requestRoleData)
+        .end((err, res) => {
+          if (err) {
+            return done(err);
+          }
+          expect(res).to.have.status(403);
+          return done();
+        });
+    });
+
+    it("returns 403 for archived users get method", function (done) {
+      chai
+        .request(app)
+        .get("/discord-actions/groups")
+        .set("Cookie", `${cookieName}=${jwt}`)
+        .end((err, res) => {
+          if (err) {
+            return done(err);
+          }
+          expect(res).to.have.status(403);
+          return done();
+        });
+    });
   });
 
-  afterEach(async function () {
-    await cleanDb();
-  });
+  describe("test discord actions for active users", function () {
+    beforeEach(async function () {
+      const user = { ...userData[4], discordId: "123456789" };
+      userId = await addUser(user);
+      jwt = authService.generateAuthToken({ userId });
+    });
 
-  it("returns 403 for archived users post method", function (done) {
-    chai
-      .request(app)
-      .post("/discord-actions/groups")
-      .set("Cookie", `${cookieName}=${jwt}`)
-      .send(roleData)
-      .end((err, res) => {
-        if (err) {
-          return done(err);
-        }
-        expect(res).to.have.status(403);
-        return done();
-      });
-  });
-
-  it("returns 403 for archived users get method", function (done) {
-    chai
-      .request(app)
-      .get("/discord-actions/groups")
-      .set("Cookie", `${cookieName}=${jwt}`)
-      .end((err, res) => {
-        if (err) {
-          return done(err);
-        }
-        expect(res).to.have.status(403);
-        return done();
-      });
+    it("returns 200 for active users get method", function (done) {
+      chai
+        .request(app)
+        .get("/discord-actions/groups")
+        .set("Cookie", `${cookieName}=${jwt}`)
+        .end((err, res) => {
+          if (err) {
+            return done(err);
+          }
+          expect(res).to.have.status(200);
+          return done();
+        });
+    });
   });
 });
