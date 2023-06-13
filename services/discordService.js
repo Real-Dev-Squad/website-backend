f3b87551eaf83994ab2c96e505d7454f2c92edd2
ritikjaiswal75 <ritik12103032000@gmail.com>
2023-06-13 07:10:52 +0530

    better error handling in discordService.js

--- services/discordService.js
+++ services/discordService.js
@@ -3,18 +3,29 @@ const jwt = require("jsonwebtoken");
 const DISCORD_BASE_URL = config.get("services.discordBot.baseUrl");
 
 const getDiscordMembers = async () => {
-  const authToken = jwt.sign({}, config.get("rdsServerlessBot.rdsServerLessPrivateKey"), {
-    algorithm: "RS256",
-    expiresIn: config.get("rdsServerlessBot.ttl"),
-  });
+  let authToken;
+  try {
+    authToken = jwt.sign({}, config.get("rdsServerlessBot.rdsServerLessPrivateKey"), {
+      algorithm: "RS256",
+      expiresIn: config.get("rdsServerlessBot.ttl"),
+    });
+  } catch (err) {
+    logger.error("Error in generating auth token", err);
+    throw err;
+  }
 
-  const response = await (
-    await fetch(`${DISCORD_BASE_URL}/discord-members`, {
-      method: "GET",
-      headers: { "Content-Type": "application/json", Authorization: `Bearer ${authToken}` },
-    })
-  ).json();
-  return response;
+  try {
+    const response = await (
+      await fetch(`${DISCORD_BASE_URL}/discord-members`, {
+        method: "GET",
+        headers: { "Content-Type": "application/json", Authorization: `Bearer ${authToken}` },
+      })
+    ).json();
+    return response;
+  } catch (err) {
+    logger.error("Error in fetching the discord data", err);
+    throw err;
+  }
 };
 
 module.exports = {
