7cdf03d67cd5239be94c83988303840ee0eaee38
Khushi Shukla <khushishukla5890@gmail.com>
2023-06-15 22:51:59 +0530

    feat/change-nickname

--- routes/discordactions.js
+++ routes/discordactions.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const authenticate = require("../middlewares/authenticate");
-const { createGroupRole, getAllGroupRoles, addGroupRoleToMember } = require("../controllers/discordactions");
-const { validateGroupRoleBody, validateMemberRoleBody } = require("../middlewares/validators/discordactions");
+const { createGroupRole, getAllGroupRoles, addGroupRoleToMember, changeNicknameOfUsers } = require("../controllers/discordactions");
+const { validateGroupRoleBody, validateMemberRoleBody , ValidateNickNamechangeBody} = require("../middlewares/validators/discordactions");
 const checkIsVerifiedDiscord = require("../middlewares/verifydiscord");
 
 const router = express.Router();
@@ -9,5 +9,6 @@ const router = express.Router();
 router.post("/groups", authenticate, checkIsVerifiedDiscord, validateGroupRoleBody, createGroupRole);
 router.get("/groups", authenticate, checkIsVerifiedDiscord, getAllGroupRoles);
 router.post("/roles", authenticate, checkIsVerifiedDiscord, validateMemberRoleBody, addGroupRoleToMember);
+router.post("/nickname" , authenticate,checkIsVerifiedDiscord, ValidateNickNamechangeBody, changeNicknameOfUsers)
 
 module.exports = router;
