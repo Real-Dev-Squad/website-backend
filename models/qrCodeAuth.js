7c255ae2ed5f048d60295a7ff22cb50eaf1ce16b
vivek lokhande <vivek2811999@gmail.com>
2023-06-22 06:42:14 +0530

    (#1011): updated model for qrCodeAuth

---
+++ models/qrCodeAuth.js
@@ -0,0 +1,40 @@
+const firestore = require("../utils/firestore");
+const QrCodeAuthModel = firestore.collection("QrCodeAuth");
+const userModel = firestore.collection("users");
+const USER_DOES_NOT_EXIST_ERROR = "User does not exist.";
+/**
+ * Stores the user device info
+ *
+ * @param userDeviceInfoData { Object }: User device info data object to be stored in DB
+ * @return {Promise<{userDeviceInfoData|Object}>}
+ */
+const storeUserDeviceInfo = async (userDeviceInfoData) => {
+  try {
+    const { user_id: userId } = userDeviceInfoData;
+    const user = await userModel.doc(userId).get();
+    if (user.data()) {
+      await QrCodeAuthModel.doc(userId).set(userDeviceInfoData);
+    } else {
+      throw new Error(USER_DOES_NOT_EXIST_ERROR);
+    }
+    return { userDeviceInfoData };
+  } catch (err) {
+    logger.error("Error in storing user device info.", err);
+    throw err;
+  }
+};
+
+const getUserAuthStatus = async (userId) => {
+  try {
+    const userAuthStatus = await QrCodeAuthModel.where("user_id", "==", userId).limit(1).get();
+    return userAuthStatus;
+  } catch (err) {
+    logger.log("Could not get", err);
+    throw err;
+  }
+};
+
+module.exports = {
+  storeUserDeviceInfo,
+  getUserAuthStatus,
+};
