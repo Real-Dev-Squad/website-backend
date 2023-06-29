7aa48298d1dc1555c4e8f1cb8ab5a613392e7870
Bharati Subramanian <bharatisharada@gmail.com>
2023-06-29 12:03:54 +0530

    feat: condition to filter tasks on query param

--- models/tasks.js
+++ models/tasks.js
@@ -68,9 +68,16 @@ const addDependency = async (data) => {
  *
  * @return {Promise<tasks|Array>}
  */
-const fetchTasks = async () => {
+const fetchTasks = async (dev = false, status = "") => {
   try {
-    const tasksSnapshot = await tasksModel.get();
+    let tasksSnapshot;
+
+    if (dev && status) {
+      tasksSnapshot = await tasksModel.where("status", "==", status).get();
+    } else {
+      tasksSnapshot = await tasksModel.get();
+    }
+
     const tasks = buildTasks(tasksSnapshot);
     const promises = tasks.map(async (task) => fromFirestoreData(task));
     const updatedTasks = await Promise.all(promises);
