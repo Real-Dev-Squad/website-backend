6e1bc601041bea7d7af3b54915fa024a0a6ca126
FMK2312 <fmkhandwala39@gmail.com>
2023-06-29 12:53:38 +0530

    added tests

--- models/qrCodeAuth.js
+++ models/qrCodeAuth.js
@@ -1,5 +1,7 @@
 const firestore = require("../utils/firestore");
 const qrAuthModel = firestore.collection("QrCodeAuth");
+const USER_DOES_NOT_EXIST_ERROR = "User does not exist.";
+const userModel = firestore.collection("users");
 
 const updateStatus = async (userId, authStatus = "NOT_INIT") => {
   try {
@@ -29,6 +31,23 @@ const updateStatus = async (userId, authStatus = "NOT_INIT") => {
   }
 };
 
+const storeUserDeviceInfo = async (userDeviceInfoData) => {
+  try {
+    const { user_id: userId } = userDeviceInfoData;
+    const user = await userModel.doc(userId).get();
+    if (user.data()) {
+      await qrAuthModel.doc(userId).set(userDeviceInfoData);
+      return { userDeviceInfoData };
+    } else {
+      throw new Error(USER_DOES_NOT_EXIST_ERROR);
+    }
+  } catch (err) {
+    logger.error("Error in storing user device info.", err);
+    throw err;
+  }
+};
+
 module.exports = {
   updateStatus,
+  storeUserDeviceInfo,
 };
