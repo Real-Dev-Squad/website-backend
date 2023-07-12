e75d63073b2786f2a7e15d53176e6587dafdabc9
khalatevarun <khalatevarun@gmail.com>
2023-07-12 23:42:57 +0530

    update condition to disable task updation if marked completed

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -233,10 +233,8 @@ const updateTaskStatus = async (req, res, next) => {
     if (task.taskData.status === TASK_STATUS.VERIFIED || req.body.status === TASK_STATUS.MERGED)
       return res.boom.forbidden("Status cannot be updated. Please contact admin.");
 
-    if (task.taskData.status === TASK_STATUS.COMPLETED && req.body.percentCompleted < 100) {
-      if (req.body.status === TASK_STATUS.COMPLETED || !req.body.status) {
-        return res.boom.badRequest("Task percent can't be updated as status is COMPLETED");
-      }
+    if (task.taskData.status === TASK_STATUS.COMPLETED) {
+      return res.boom.badRequest("Task details can't be updated as it is COMPLETED");
     }
 
     if (req.body.status === TASK_STATUS.COMPLETED && task.taskData.percentCompleted !== 100) {
