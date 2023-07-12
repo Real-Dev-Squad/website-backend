9431959045643775dccf9848af3d595656b2d472
Sriza Sen <sriza20124@iiitd.ac.in>
2023-07-12 22:32:18 +0530

    Create a data access layer, which always return the user's filtered data for `/users` route (#1222)
    * removal of sensitive info(phone,email,tokens,chaincode) on /users route
    
    * update in removal of sensitive info(phone,email,tokens,chaincode) on /users route
    
    * Update dataAccessLayer.js
    
    * updated query id and qualifiers params in /users in removal of sensitive info(phone,email,tokens,chaincode) on /users route
    
    * updated dataAccessLayer for lint issues
    
    * Update users.js
    
    * updated delete statement to hasOwnProperty
    
    * updated default arguments to function removeSensitiveInfo
    
    * made updates in the dataAccessLayer
    
    * Update users.js
    
    * typo fixes
    
    * Added unit tests
    
    * updated to using in operator
    
    * updated to query as argument in place of req
    
    * made the sensitive data to a constants file
    
    * addded updates to constants and made query as non-null in retrieveUsers
    
    * updated constants variable name
    
    ---------
    
    Co-authored-by: Sen, Sriza <14099+Sriza-Sen@users.noreply.eos2git.cec.lab.emc.com>

--- test/fixtures/user/user.js
+++ test/fixtures/user/user.js
@@ -257,5 +257,53 @@ module.exports = () => {
         url: "https://res.cloudinary.com/realdevsquad/image/upload/v1667685133/profile/mtS4DhUvNYsKqI7oCWVB/aenklfhtjldc5ytei3ar.jpg",
       },
     },
+    {
+      id: 11,
+      username: "testuser1",
+      first_name: "test1",
+      last_name: "user1",
+      yoe: 1,
+      img: "./img.png",
+      linkedin_id: "testuser1",
+      github_id: "testuser1",
+      github_display_name: "Test User",
+      phone: "1234567890",
+      email: "tu@gmail.com",
+      chaincode: "1234",
+      tokens: {
+        githubAccessToken: "githubAccessToken",
+      },
+      roles: {
+        member: true,
+      },
+      picture: {
+        publicId: "profile/mtS4DhUvNYsKqI7oCWVB/aenklfhtjldc5ytei3ar",
+        url: "https://res.cloudinary.com/realdevsquad/image/upload/v1667685133/profile/mtS4DhUvNYsKqI7oCWVB/aenklfhtjldc5ytei3ar.jpg",
+      },
+    },
+    {
+      id: 12,
+      username: "testuser2",
+      first_name: "test2",
+      last_name: "user2",
+      yoe: 1,
+      img: "./img.png",
+      linkedin_id: "testuser1",
+      github_id: "testuser1",
+      github_display_name: "Test User",
+      phone: "1234567890",
+      email: "tu@gmail.com",
+      chaincode: "12345",
+      tokens: {
+        githubAccessToken: "githubAccessToken",
+      },
+      roles: {
+        member: true,
+      },
+      picture: {
+        publicId: "profile/mtS4DhUvNYsKqI7oCWVB/aenklfhtjldc5ytei3ar",
+        url: "https://res.cloudinary.com/realdevsquad/image/upload/v1667685133/profile/mtS4DhUvNYsKqI7oCWVB/aenklfhtjldc5ytei3ar.jpg",
+      },
+    },
   ];
 };
