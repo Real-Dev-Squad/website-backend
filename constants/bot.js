25994b121d45d0efa35fc4fdd11e7070d1ba1408
ritikjaiswal75 <ritik12103032000@gmail.com>
2023-07-17 02:56:39 +0530

    add a middleware to identify RDS cron jobs

--- constants/bot.js
+++ constants/bot.js
@@ -1,4 +1,5 @@
 const CLOUDFLARE_WORKER = "Cloudflare Worker";
 const BAD_TOKEN = "BAD.JWT.TOKEN";
+const CRON_JOB_HANDLER = "Cron Job Handler";
 
-module.exports = { CLOUDFLARE_WORKER, BAD_TOKEN };
+module.exports = { CLOUDFLARE_WORKER, BAD_TOKEN, CRON_JOB_HANDLER };
--- test/unit/middlewares/authorizeBot.test.js
+++ test/unit/middlewares/authorizeBot.test.js
@@ -2,52 +2,99 @@ const authorizeBot = require("../../../middlewares/authorizeBot");
 const sinon = require("sinon");
 const expect = require("chai").expect;
 const bot = require("../../utils/generateBotToken");
-const { BAD_TOKEN, CLOUDFLARE_WORKER } = require("../../../constants/bot");
+const { BAD_TOKEN, CLOUDFLARE_WORKER, CRON_JOB_HANDLER } = require("../../../constants/bot");
 
-describe("Check authorization of bot", function (done) {
-  it("return false when token is invalid", function () {
-    const request = {
-      headers: `Bearer ${BAD_TOKEN}`,
-    };
+describe("Middleware | Authorize Bot", function () {
+  describe("Check authorization of bot", function (done) {
+    it("return false when token is invalid", function () {
+      const request = {
+        headers: `Bearer ${BAD_TOKEN}`,
+      };
 
-    const response = {
-      statusCode: 401,
-      error: "Unauthorized",
-      message: "Unauthorized Bot",
-    };
+      const response = {
+        statusCode: 401,
+        error: "Unauthorized",
+        message: "Unauthorized Bot",
+      };
 
-    const nextSpy = sinon.spy();
-    authorizeBot(request, response, nextSpy);
-    expect(nextSpy.calledOnce).to.be.equal(false);
+      const nextSpy = sinon.spy();
+      authorizeBot.verifyDiscordBot(request, response, nextSpy);
+      expect(nextSpy.calledOnce).to.be.equal(false);
+    });
+
+    it("return false when header is not present", function () {
+      const request = {};
+
+      const response = {
+        statusCode: 400,
+        error: "Invalid Request",
+        message: "Invalid Request",
+      };
+
+      const nextSpy = sinon.spy();
+      authorizeBot.verifyDiscordBot(request, response, nextSpy);
+      expect(nextSpy.calledOnce).to.be.equal(false);
+    });
+
+    it("return true when token is valid", function () {
+      const jwtToken = bot.generateToken({ name: CLOUDFLARE_WORKER });
+
+      const request = {
+        headers: {
+          authorization: `Bearer ${jwtToken}`,
+        },
+      };
+
+      const response = {};
+
+      const nextSpy = sinon.spy();
+      authorizeBot.verifyDiscordBot(request, response, nextSpy);
+      expect(nextSpy.calledOnce).to.be.equal(true);
+    });
   });
 
-  it("return false when header is not present", function () {
-    const request = {};
+  describe("Check Authorization of cron job", function () {
+    it("Check fails on bad auth token", function () {
+      const request = {
+        headers: {
+          authorization: `Bearer ${BAD_TOKEN}`,
+        },
+      };
 
-    const response = {
-      statusCode: 400,
-      error: "Invalid Request",
-      message: "Invalid Request",
-    };
+      const response = {};
 
-    const nextSpy = sinon.spy();
-    authorizeBot(request, response, nextSpy);
-    expect(nextSpy.calledOnce).to.be.equal(false);
-  });
+      const nextSpy = sinon.spy();
 
-  it("return true when token is valid", function () {
-    const jwtToken = bot.generateToken({ name: CLOUDFLARE_WORKER });
+      authorizeBot.verifyCronJob(request, response, nextSpy);
+      expect(nextSpy.calledOnce).to.be.equal(false);
+    });
 
-    const request = {
-      headers: {
-        authorization: `Bearer ${jwtToken}`,
-      },
-    };
+    it("Should stop propagation for no auth token", function () {
+      const request = {
+        headers: {},
+      };
 
-    const response = {};
+      const response = {};
 
-    const nextSpy = sinon.spy();
-    authorizeBot(request, response, nextSpy);
-    expect(nextSpy.calledOnce).to.be.equal(true);
+      const nextSpy = sinon.spy();
+
+      authorizeBot.verifyCronJob(request, response, nextSpy).catch((err) => {
+        expect(err).to.be.instanceOf(Error);
+      });
+      expect(nextSpy.calledOnce).to.be.equal(false);
+    });
+
+    it("Should allow request Propogation for valid request", function () {
+      const jwtToken = bot.generateCronJobToken({ name: CRON_JOB_HANDLER });
+      const request = {
+        headers: {
+          authorization: `Bearer ${jwtToken}`,
+        },
+      };
+      const response = {};
+      const nextSpy = sinon.spy();
+      authorizeBot.verifyCronJob(request, response, nextSpy);
+      expect(nextSpy.calledOnce).to.be.equal(true);
+    });
   });
 });
