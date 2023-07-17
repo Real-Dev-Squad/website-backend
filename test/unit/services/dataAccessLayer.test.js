d55b20a1162d6641bcb495047c51a82e9ab8874e
mailmesriza98 <sriza20124@iiitd.ac.in>
2023-07-17 10:32:17 +0530

    added unit tests

--- test/unit/services/dataAccessLayer.test.js
+++ test/unit/services/dataAccessLayer.test.js
@@ -8,11 +8,11 @@ const userData = require("../../fixtures/user/user")();
 const { USER_SENSITIVE_DATA } = require("../../../constants/users");
 chai.use(chaiHttp);
 const expect = chai.expect;
-
+let fetchUserStub;
 describe("Data Access Layer", function () {
   describe("retrieveUsers", function () {
     it("should fetch a single user by ID and remove sensitive info", async function () {
-      const fetchUserStub = sinon.stub(userQuery, "fetchUser");
+      fetchUserStub = sinon.stub(userQuery, "fetchUser");
       fetchUserStub.returns(Promise.resolve({ user: userData[12] }));
 
       const result = await retrieveUsers({ id: userData[12].id });
@@ -23,8 +23,18 @@ describe("Data Access Layer", function () {
       });
     });
 
+    it("should check if the given username is available and remove sensitive info", async function () {
+      fetchUserStub.returns(Promise.resolve({ user: userData[12] }));
+      const result = await retrieveUsers({ username: userData[12].username });
+      removeSensitiveInfo(userData[12]);
+      expect(result.user).to.deep.equal(userData[12]);
+      USER_SENSITIVE_DATA.forEach((key) => {
+        expect(result.user).to.not.have.property(key);
+      });
+    });
+
     it("should fetch users by username and remove sensitive info", async function () {
-      const fetchUserStub = sinon.stub(userQuery, "fetchUsers");
+      fetchUserStub = sinon.stub(userQuery, "fetchUsers");
       fetchUserStub.returns(Promise.resolve({ users: [userData[12]] }));
       const result = await retrieveUsers({ usernames: [userData[12].username] });
       removeSensitiveInfo(userData[12]);
@@ -37,7 +47,7 @@ describe("Data Access Layer", function () {
     });
 
     it("should fetch paginated users and remove sensitive info", async function () {
-      const fetchUserStub = sinon.stub(userQuery, "fetchPaginatedUsers");
+      fetchUserStub = sinon.stub(userQuery, "fetchPaginatedUsers");
       fetchUserStub.returns(Promise.resolve({ allUsers: [userData[12]], nextId: 3, prevId: 1 }));
       const query = { page: 1 };
       const result = await retrieveUsers({ query });
@@ -49,6 +59,17 @@ describe("Data Access Layer", function () {
         });
       });
     });
+
+    it("should return /users/self data and remove sensitive info", async function () {
+      const userdata = userData[12];
+      const result = await retrieveUsers({ userdata });
+      removeSensitiveInfo(userData[12]);
+      USER_SENSITIVE_DATA.forEach((key) => {
+        expect(userdata).to.not.have.property(key);
+      });
+    });
+
+    
   });
 
   describe("removeSensitiveInfo", function () {
