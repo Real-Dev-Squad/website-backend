184ba39c4b69daac9b1d90e503d3c186dfd11f20
ritikjaiswal75 <ritik12103032000@gmail.com>
2023-08-01 00:56:59 +0530

    add an api to remove privilaged roles to all users

--- routes/staging.js
+++ routes/staging.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const authenticate = require("../middlewares/authenticate");
 const { validateUserRoles } = require("../middlewares/validators/staging");
-const { updateRoles } = require("../controllers/staging");
+const { updateRoles, removePrivileges } = require("../controllers/staging");
 const router = express.Router();
 
 router.patch("/user", validateUserRoles, authenticate, updateRoles);
+router.post("/users", removePrivileges);
 
 module.exports = router;
