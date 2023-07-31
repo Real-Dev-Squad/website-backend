9d10d117a5d224f5e72b5ebeaee67878c8a46d01
Randhir Kumar Singh <97341921+heyrandhir@users.noreply.github.com>
2023-07-31 10:56:29 +0530

    adds the logic to modify the user status of old assignee

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -256,7 +256,12 @@ const updateTask = async (req, res) => {
     }
     await tasks.updateTask(req.body, req.params.id);
     if (isUserStatusEnabled && req.body.assignee) {
+      // New Assignee Status Update
       await updateUserStatusOnTaskUpdate(req.body.assignee);
+      // Old Assignee Status Update if available
+      if (task.taskData.assigneeId) {
+        await updateStatusOnTaskCompletion(task.taskData.assigneeId);
+      }
     }
 
     return res.status(204).send();
