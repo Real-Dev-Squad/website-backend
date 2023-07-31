1bf0bcec67055ba1a7a27802bd599112be4b518e
ritikjaiswal75 <ritik12103032000@gmail.com>
2023-08-01 01:15:12 +0530

    add middleware to allow body

--- routes/staging.js
+++ routes/staging.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const authenticate = require("../middlewares/authenticate");
-const { validateUserRoles } = require("../middlewares/validators/staging");
+const { validateUserRoles, validateRevokePrivileges } = require("../middlewares/validators/staging");
 const { updateRoles, removePrivileges } = require("../controllers/staging");
 const router = express.Router();
 
 router.patch("/user", validateUserRoles, authenticate, updateRoles);
-router.post("/users", removePrivileges);
+router.post("/users/privileges", validateRevokePrivileges, removePrivileges);
 
 module.exports = router;
