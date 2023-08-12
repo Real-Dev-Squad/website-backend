26544e69f1041ba03bf9c8b150f88ef23d08c0d0
sanketumesh.dhabarde <sanketumesh.dhabarde@clarivate.com>
2023-08-12 13:03:27 +0530

    [Refactor]: Move get to top to avoid conflict

--- routes/events.js
+++ routes/events.js
@@ -7,12 +7,12 @@ const eventsValidator = require("../middlewares/validators/events");
 router.post("/", authenticate, eventsValidator.createEvent, events.createEvent);
 router.get("/", eventsValidator.getAllEvents, events.getAllEvents);
 router.post("/join", eventsValidator.joinEvent, events.joinEvent);
+router.get("/roomCodes", events.getRoomCodes);
 router.get("/:id", eventsValidator.getEventById, events.getEventById);
 router.patch("/", authenticate, eventsValidator.updateEvent, events.updateEvent);
 router.patch("/end", authenticate, eventsValidator.endActiveEvent, events.endActiveEvent);
-router.post("/:id/peers", authenticate, eventsValidator.addPeerToEvent, events.addPeerToEvent);
+router.post("/:id/peers", eventsValidator.addPeerToEvent, events.addPeerToEvent);
 router.patch("/:id/peers/kickout", authenticate, eventsValidator.kickoutPeer, events.kickoutPeer);
 router.post("/roomCodes", authenticate, events.createRoomCodes);
-router.get("/roomCodes", events.getRoomCodes);
 
 module.exports = router;
