6ebfaac97426eb1c2597c4792e3d35be3583e7e5
Bikash Singh <bksh05@gmail.com>
2023-08-12 19:37:37 +0530

    reverting script

--- scripts/tests/testUnit.sh
+++ scripts/tests/testUnit.sh
@@ -8,4 +8,4 @@ json=$(node -e "console.log(require('config').get('firestore'))")
 project_id=$(echo $json | grep -o '"project_id":[^,}]*' | cut -d':' -f2 | tr -d '"' | tr -d '[:space:]')
 
 echo 'Start firestore emulator and run unit tests:'
-firebase emulators:exec 'nyc --x=controllers --x=test --x=docs  --recursive --x=mockdata --x=dist mocha --timeout 10000 test/unit/**' --project=$project_id
+firebase emulators:exec 'nyc --x=controllers --x=test --x=docs  --recursive --x=mockdata --x=dist mocha test/unit/**' --project=$project_id
