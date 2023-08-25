6146ef30ed5c28047f89cfe8bcb6162b88280dca
vinayak <trivedivinayak57@gmail.com>
2023-08-25 22:18:27 +0530

    completed get application tests

--- test/integration/application.test.ts
+++ test/integration/application.test.ts
@@ -1,13 +1,13 @@
 import chai from "chai";
 import chaiHttp from "chai-http";
 const { expect } = chai;
-import config from 'config'
+import config from "config";
 const app = require("../../server");
 const addUser = require("../utils/addUser");
 const cleanDb = require("../utils/cleanDb");
 const authService = require("../../services/authService");
 const userData = require("../fixtures/user/user")();
-const applicationModel = require("../../models/applications")
+const applicationModel = require("../../models/applications");
 const { requestRoleData } = require("../fixtures/discordactions/discordactions");
 
 const applicationsData = require("../fixtures/applications/applications")();
@@ -19,31 +19,30 @@ const superUser = userData[4];
 chai.use(chaiHttp);
 
 let userId: string;
+let superUserId: string;
 let jwt: string;
 let superUserJwt: string;
 
 describe("Application", function () {
   before(async function () {
     userId = await addUser(appOwner);
-    const superUserId = await addUser(superUser);
+    superUserId = await addUser(superUser);
     jwt = authService.generateAuthToken({ userId });
     superUserJwt = authService.generateAuthToken({ userId: superUserId });
-    console.log(applicationsData, 'applicationData...')
-    const applicationOne = {...applicationsData[0], userId}
-    const applicationTwo = {...applicationsData[1], superUserId}
-
+    console.log(applicationsData, "applicationData...");
+    const applicationOne = { ...applicationsData[0], userId };
+    const applicationTwo = { ...applicationsData[1], superUserId };
 
     const promises = [applicationModel.addApplication(applicationOne), applicationModel.addApplication(applicationTwo)];
 
-    await Promise.all(promises)
-
+    await Promise.all(promises);
   });
 
   after(async function () {
     await cleanDb();
   });
   describe("GET /applications", function () {
-    it("should return all the application if the user is super user and there is no user id", function(done) {
+    it("should return all the application if the user is super user and there is no user id", function (done) {
       chai
         .request(app)
         .get("/applications")
@@ -62,24 +61,80 @@ describe("Application", function () {
         });
     });
 
-    it("should return application of the user if the user is super user and user id is there in query params", function(done) {
-       chai
-         .request(app)
-         .get(`/applications?userId=${userId}`)
-         .set("cookie", `${cookieName}=${superUserJwt}`)
-         .end((err, res) => {
-           if (err) {
-             return done(err);
-           }
+    it("should return application of the user if the user is super user and user id is there in query params", function (done) {
+      chai
+        .request(app)
+        .get(`/applications?userId=${userId}`)
+        .set("cookie", `${cookieName}=${superUserJwt}`)
+        .end((err, res) => {
+          if (err) {
+            return done(err);
+          }
 
-           expect(res).to.have.status(200);
-           expect(res.body).to.be.a("object");
-           expect(res.body.message).to.equal("application returned successfully!");
-           expect(res.body.application).to.be.a("object");
-           expect(res.body.application.userId).to.be.equal(userId);
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.a("object");
+          expect(res.body.message).to.equal("application returned successfully!");
+          expect(res.body.application).to.be.a("object");
+          expect(res.body.application.userId).to.be.equal(userId);
 
-           return done();
-         });
-    })
+          return done();
+        });
+    });
+
+    it("should return 403 in case the user is not super user and there is no userId in query", function (done) {
+      chai
+        .request(app)
+        .get(`/applications`)
+        .set("cookie", `${cookieName}=${jwt}`)
+        .end((err, res) => {
+          if (err) {
+            return done(err);
+          }
+
+          expect(res).to.have.status(403);
+          expect(res.body.message).to.be.equal("Unauthorized User");
+          expect(res.body.error).to.be.equal("Forbidden");
+          return done();
+        });
+    });
+
+    it("should return the user object if the userId of user is same as userId in the application object", function (done) {
+      chai
+        .request(app)
+        .get(`/applications?userId=${userId}`)
+        .set("cookie", `${cookieName}=${jwt}`)
+        .end((err, res) => {
+          if (err) {
+            return done(err);
+          }
+
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.a("object");
+          expect(res.body.message).to.equal("application returned successfully!");
+          expect(res.body.application).to.be.a("object");
+          expect(res.body.application.userId).to.be.equal(userId);
+
+          return done();
+        });
+    });
+
+    it("should return 403 if the userId of user is not same as userId in the application object and user is not super user", function (done) {
+      chai
+        .request(app)
+        .get(`/applications?userId=${superUserId}`)
+        .set("cookie", `${cookieName}=${jwt}`)
+        .end((err, res) => {
+          if (err) {
+            return done(err);
+          }
+
+          expect(res).to.have.status(403);
+          expect(res.body.message).to.be.equal("Unauthorized User");
+          expect(res.body.error).to.be.equal("Forbidden");
+
+          return done();
+        });
+    });
+
   });
 });
