161e54e067a88c5ed1098904ea56185f54c2c44a
kotesh-arya <koteswarraomudila@gmail.com>
2023-09-05 06:28:55 +0530

    REFACTOR: Add middlewares

--- routes/users.js
+++ routes/users.js
@@ -61,5 +61,11 @@ router.patch("/profileURL", authenticate, userValidator.updateProfileURL, users.
 router.patch("/rejectDiff", authenticate, authorizeRoles([SUPERUSER]), users.rejectProfileDiff);
 router.patch("/:userId", authenticate, authorizeRoles([SUPERUSER]), users.updateUser);
 router.get("/suggestedUsers/:skillId", authenticate, authorizeRoles([SUPERUSER]), users.getSuggestedUsers);
-router.post("/discord/update-nicknames", users.updateDiscordNicknames);
+router.post(
+  "/discord/update-nicknames",
+  authenticate,
+  authorizeRoles([SUPERUSER]),
+  checkIsVerifiedDiscord,
+  users.updateDiscordNicknames
+);
 module.exports = router;
