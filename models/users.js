7936aae2b6be071a6564d1a74b4871a7d7b10085
Jyotsna Mehta <53934353+j24m@users.noreply.github.com>
2023-09-14 23:24:25 +0530

    Refactor: Addressed pr review comments

--- models/users.js
+++ models/users.js
@@ -260,12 +260,12 @@ const fetchUser = async ({ userId = null, username = null, githubUsername = null
         userData = doc.data();
       });
     } else if (discordId) {
-      const user = await userModel.where("discordId", "==", discordId).limit(1).get();
+      const user = await userModel.where("discordId", "==", discordId).get();
       user.forEach((doc) => {
         id = doc.id;
         userData = doc.data();
       });
-      const userStatusDocs = await userStatusModel.where("userId", "==", id).limit(1).get();
+      const userStatusDocs = await userStatusModel.where("userId", "==", id).get();
       const [userStatusDoc] = userStatusDocs.docs;
 
       const data = userStatusDoc.data();
