56f5981682501a8d9a897a515bd8f5e1abf7fd72
Sunny Sahsi <sahsisunny@gmail.com>
2023-09-18 00:54:56 +0530

    Fetch User Details from Discord Id (#1457) (#1531)
    * Fetch users based on discord id
    
    * Fix : remove console logs
    
    * Wrote integration test for fetch user with discord id
    
    * fix: minor fix
    
    * Feat: Added feature flag and added test for the same, Refactor: Removed commented code
    
    * Added unit tests
    
    * Addressed pr review comments
    
    * Refactor: Addressed pr review comments
    
    * Refactor : Changes based on pr reviews
    
    * Resolved pr review comments
    
    * Refactor
    
    * Refactor : Handled the error case when feature flag not provided
    
    * Refactor
    
    * Added test case for invalid discord id
    
    Co-authored-by: Jyotsna Mehta <53934353+j24m@users.noreply.github.com>

--- test/unit/models/users.test.js
+++ test/unit/models/users.test.js
@@ -117,6 +117,23 @@ describe("users", function () {
     });
   });
 
+  describe("fetch user details based on discord id", function () {
+    let [userId0] = [];
+    beforeEach(async function () {
+      const userArr = userData();
+      userId0 = await addUser(userArr[0]);
+      await userStatusModel.doc("userStatus000").set(generateStatusDataForState(userId0, userState.IDLE));
+    });
+
+    afterEach(async function () {
+      await cleanDb();
+    });
+    it("It should have discordId field", async function () {
+      const result = await users.fetchUser({ discordId: "12345" });
+      expect(result.user).to.haveOwnProperty("discordId");
+    });
+  });
+
   describe("user image verification", function () {
     let userId, discordId, profileImageUrl, discordImageUrl;
     beforeEach(async function () {
@@ -310,6 +327,7 @@ describe("users", function () {
       });
     });
   });
+
   describe("get users by roles", function () {
     beforeEach(async function () {
       const addUsersPromises = [];
@@ -357,6 +375,7 @@ describe("users", function () {
       expect(result.length).to.equal(2);
     });
   });
+
   describe("fetch users by id", function () {
     let allIds = [];
     before(async function () {
@@ -385,6 +404,7 @@ describe("users", function () {
       expect(fetchedUserIds).to.deep.equal([]);
     });
   });
+
   describe("generateUniqueUsername", function () {
     it("should generate a unique username when existing users are present", async function () {
       const userData = userDataArray[15];
