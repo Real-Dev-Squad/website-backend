13bf2b0a07b0a12ee7c963039a102cc1efcc3c4d
Aryex82 <98796547+Aryex82@users.noreply.github.com>
2023-09-21 00:19:33 +0530

    Fix overdue test case (#1540)
    * fix : test case fix
    
    * fix : add new data
    
    * fix: lint

--- test/unit/models/tasks.test.js
+++ test/unit/models/tasks.test.js
@@ -284,13 +284,16 @@ describe("tasks", function () {
 
     it("should return the overdue tasks for the given days", async function () {
       const days = 10;
+      const overdueTask = { ...tasksData[0] };
+      overdueTask.endsOn = Date.now() / 1000 + 24 * 60 * 60 * 7;
+      await tasks.updateTask(overdueTask);
       const usersWithOverdueTasks = await tasks.getOverdueTasks(days);
-      expect(usersWithOverdueTasks.length).to.be.equal(4);
+      expect(usersWithOverdueTasks.length).to.be.equal(5);
     });
 
     it("should return all users which have overdue tasks if days is not passed", async function () {
       const usersWithOverdueTasks = await tasks.getOverdueTasks();
-      expect(usersWithOverdueTasks.length).to.be.equal(3);
+      expect(usersWithOverdueTasks.length).to.be.equal(4);
     });
   });
 });
