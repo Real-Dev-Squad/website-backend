0460b5302abfa864da5e45df8c4fa95fc3e0140f
Ajeyakrishna <connect.ajeya@gmail.com>
2023-09-21 00:16:11 +0530

    fix: lint

--- test/unit/models/tasks.test.js
+++ test/unit/models/tasks.test.js
@@ -284,8 +284,8 @@ describe("tasks", function () {
 
     it("should return the overdue tasks for the given days", async function () {
       const days = 10;
-      const overdueTask = {...tasksData[0]};
-      overdueTask.endsOn = (Date.now() / 1000) + (24 * 60 * 60 *7);
+      const overdueTask = { ...tasksData[0] };
+      overdueTask.endsOn = Date.now() / 1000 + 24 * 60 * 60 * 7;
       await tasks.updateTask(overdueTask);
       const usersWithOverdueTasks = await tasks.getOverdueTasks(days);
       expect(usersWithOverdueTasks.length).to.be.equal(5);
