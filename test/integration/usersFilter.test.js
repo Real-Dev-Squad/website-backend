18214100f17950bf06a67ddad06438ad8b614ca0
Sunny Sahsi <sahsisunny@gmail.com>
2023-09-26 17:36:03 +0530

    Refactor to Retrieve Users in Discord only (#1555)

--- test/integration/usersFilter.test.js
+++ test/integration/usersFilter.test.js
@@ -190,12 +190,7 @@ describe("Filter Users", function () {
           expect(res).to.have.status(200);
           expect(res.body).to.be.a("object");
           expect(res.body.count).to.be.a("number");
-          expect(res.body.message).to.equal("Users found successfully!");
-          expect(res.body.users).to.be.a("array");
-          expect(res.body.users.length).to.equal(1);
-          res.body.users.forEach((user) => {
-            expect(user).to.have.property("discordJoinedAt");
-          });
+          expect(res.body.message).to.equal("No users found");
           return done();
         });
     });
