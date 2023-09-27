1f70de47c8b0327cd15841689f9e8d25b9a78015
AnjaliDeshwani <deshwanianjali@gmail.com>
2023-09-27 21:06:14 +0530

    updated test discord users response

--- test/fixtures/discordResponse/discord-response.js
+++ test/fixtures/discordResponse/discord-response.js
@@ -148,75 +148,7 @@ const updatedNicknameResponse = {
 };
 
 const getOnboarding31DPlusMembers = [
-  {
-    avatar: "",
-    communication_disabled_until: "",
-    flags: 0,
-    is_pending: false,
-    joined_at: "some date", // ISO8601 timestamp
-    nick: "jhon",
-    pending: false,
-    premium_since: null,
-    roles: ["hero", "racer"],
-    user: {
-      id: "123456789098765432",
-      username: "jhondoe",
-      global_name: "jhondoe",
-      display_name: "jhondoe",
-      avatar: "56yu6567uuju7636w4e6yr7e4utbw8r4", // avatar hash
-      discriminator: "4455", // 4-digit discord-tag
-      public_flags: 0,
-      avatar_decoration: null,
-    },
-    mute: false,
-    deaf: false,
-  },
-  {
-    avatar: "",
-    communication_disabled_until: "",
-    flags: 0,
-    is_pending: false,
-    joined_at: "some_intensional_date", // ISO8601 timestamp
-    nick: "david",
-    pending: false,
-    premium_since: null,
-    roles: ["hero", "racer"],
-    user: {
-      id: "12345678909867666",
-      username: "david",
-      global_name: "david",
-      display_name: "david",
-      avatar: "56yu6567uuju7636w4e6yr7e4utbw8r4", // avatar hash
-      discriminator: "4455", // 4-digit discord-tag
-      public_flags: 0,
-      avatar_decoration: null,
-    },
-    mute: false,
-    deaf: false,
-  },
-  {
-    avatar: "",
-    communication_disabled_until: "",
-    flags: 0,
-    is_pending: false,
-    joined_at: "some_intensional_date", // ISO8601 timestamp
-    nick: "goliath",
-    pending: false,
-    premium_since: null,
-    roles: ["giant", "hero"],
-    user: {
-      id: "123456",
-      username: "goliath",
-      global_name: "goliath",
-      display_name: "goliath",
-      avatar: "56yu6567uuju7636w4e6yr7e9utbw8r4", // avatar hash
-      discriminator: "44551", // 4-digit discord-tag
-      public_flags: 0,
-      avatar_decoration: null,
-    },
-    mute: false,
-    deaf: false,
-  },
+  ...getDiscordMembers,
   {
     avatar: "",
     communication_disabled_until: "",
