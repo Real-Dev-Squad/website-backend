b21d88682018c23c94d75d45f5b26d4d4887ee54
shreya-mishra <shreyamishra040398@gmail.com>
2023-11-08 19:50:50 +0530

    updated notify method where we will be fetching userId from the discordId

--- controllers/notify.js
+++ controllers/notify.js
@@ -1,5 +1,7 @@
 const admin = require("firebase-admin");
 const firestore = require("../utils/firestore");
+const memberRoleModel = firestore.collection("member-group-roles");
+const usersModel = firestore.collection("users");
 
 const fcmTokenModel = firestore.collection("fcmToken");
 
@@ -10,40 +12,62 @@ const fcmTokenModel = firestore.collection("fcmToken");
  * @param res {Object} - Express response object
  */
 const notifyController = async (req, res) => {
-  // const fcmTokenSnapshot = fcmTokenModel.where("userId", "==", fcmTokenData.userId).limit(1).get();
-  // console.log('fcmTokenSnap', fcmTokenSnapshot)
-  // title , description ,
-  const {
-    title,
-    body,
-    userId,
-    // groupRole
-  } = req.body;
 
+  const { title, body, userId, groupRoleId } = req.body;
   let fcmTokens = [];
-  if (userId) {
+
+  const getFcmTokenFromUserId = async (userId) => {
+    if (!userId) return [];
     const fcmTokenSnapshot = await fcmTokenModel.where("userId", "==", userId).limit(1).get();
 
     if (!fcmTokenSnapshot.empty) {
-      fcmTokenSnapshot.forEach((item) => {
-        fcmTokens = [...fcmTokens, ...item.data().fcmTokens];
-      });
+      return fcmTokenSnapshot.docs[0].data().fcmTokens;
     }
+    return [];
+  };
+  if (userId) {
+
+    const fcmTokensFromUserId = await getFcmTokenFromUserId(userId);
+    fcmTokens = [...fcmTokens, ...fcmTokensFromUserId];
   }
+
+  if (groupRoleId) {
+    let discordIds = [];
+    const querySnapshot = await memberRoleModel.where("roleid", "==", groupRoleId).get();
+    if (!querySnapshot.empty) {
+      discordIds = querySnapshot.docs.map((doc) => doc.data());
+    }
+
+    const discordIdsPromiseArray = discordIds.map(async (item) => {
+      const userSnapshot = await usersModel.where("discordId", "==", item.userid).get();
+      if (!userSnapshot.empty) {
+        return userSnapshot.docs[0].id;
+      }
+      return undefined;
+    });
+
+    const userIdFromDiscordId = await Promise.all(discordIdsPromiseArray);
+
+    const fcmtokensPromiseArray = userIdFromDiscordId.map(async (userId) => {
+      const fcmTokensFromUserId = await getFcmTokenFromUserId(userId);
+      fcmTokens = [...fcmTokens, ...fcmTokensFromUserId];
+    });
+    await Promise.all(fcmtokensPromiseArray);
+  }
+
+  const setOfFcmTokens = new Set(fcmTokens);
 
   const message = {
     notification: {
-      title: title && "Notification Title",
-      body: body && "Notification Body",
+      title: title || "Notification Title",
+      body: body || "Notification Body",
     },
     data: {
       key1: "value1",
       key2: "value2",
     },
-    tokens: fcmTokens,
+    tokens: Array.from(setOfFcmTokens),
   };
-
-  // const { userName,  } = req.body;
 
   // Save the FCM token to your database or perform other necessary actions.
   // You can associate the token with a user or device, for example.
@@ -58,7 +82,7 @@ const notifyController = async (req, res) => {
     .catch((error) => {
       logger.error("Error sending message:", error);
     });
-  res.status(200).send(`fcm tokens device will get notified: ${fcmTokens}`);
+  res.status(200).send(`fcm tokens device will get notified: ${Array.from(setOfFcmTokens)}`);
 };
 
 module.exports = {
