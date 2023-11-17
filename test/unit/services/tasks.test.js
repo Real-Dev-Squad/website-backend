577b8eeb882956309f577c40f47116a03f13db5c
shubhamsigdar1 <shubhamsigdar@gmail.com>
2023-11-18 00:58:23 +0530

    implement batch operation for task status from COMPLETED to DONE

--- test/unit/services/tasks.test.js
+++ test/unit/services/tasks.test.js
@@ -1,76 +1,74 @@
-// const Sinon = require("sinon");
-// const { expect } = require("chai");
+const Sinon = require("sinon");
+const { expect } = require("chai");
 
-// const firestore = require("../../../utils/firestore");
-// const tasksModel = firestore.collection("tasks");
-// const cleanDb = require("../../utils/cleanDb");
-// const taskDataArray = require("../../fixtures/tasks/tasks");
-// const { updateStatusToDone } = require("../../../services/users");
+const firestore = require("../../../utils/firestore");
+const tasksModel = firestore.collection("tasks");
+const cleanDb = require("../../utils/cleanDb");
+const taskDataArray = require("../../fixtures/tasks/tasks")();
+const { updateTaskStatusToDone } = require("../../../services/tasks");
 
-// const { skip } = require("node:test");
+describe("Tasks services", function () {
+  describe("task status COMPLETED to DONE in bulk", function () {
+    const tasks = [];
+    const taskDetails = [];
+    beforeEach(async function () {
+      const addTasksPromises = [];
+      taskDataArray.forEach((task) => {
+        const taskData = {
+          ...task,
+          status: "COMPLETED",
+        };
+        addTasksPromises.push(tasksModel.add(taskData));
+      });
 
-// describe("Tasks services", function () {
-//   describe("task status COMPLETED to DONE in bulk", function () {
-//     const tasks = [];
-//     const taskDetails = [];
-//     beforeEach(async function () {
-//       const addTasksPromises = [];
-//       taskDataArray.forEach((task) => {
-//         const taskData = {
-//           ...task,
-//           status: "DONE",
-//         };
-//         addTasksPromises.push(tasksModel.add(taskData));
-//       });
+      await Promise.all(addTasksPromises);
 
-//       await Promise.all(addTasksPromises);
+      tasks.length = 0;
+      taskDetails.length = 0;
 
-//       tasks.length = 0;
-//       taskDetails.length = 0;
+      const snapshot = await tasksModel.where("status", "==", "COMPLETED").get();
 
-//       const snapshot = await tasksModel.where("status", "==", "COMPLETED").get();
+      snapshot.forEach((task) => {
+        const id = task.id;
+        const taskData = task.data();
+        tasks.push({ ...taskData, id });
+        taskDetails.push(id);
+      });
+    });
 
-//       snapshot.forEach((task) => {
-//         const id = task.id;
-//         const userData = task.data();
-//         tasks.push({ ...userData, id });
-//         taskDetails.push(userData.id);
-//       });
-//     });
+    afterEach(async function () {
+      await cleanDb();
+      Sinon.restore();
+    });
 
-//     afterEach(async function () {
-//       await cleanDb();
-//       Sinon.restore();
-//     });
+    it("successfully updated task status COMPLETED To DONE", async function () {
+      const res = await updateTaskStatusToDone(tasks);
 
-// skip("Should return successful response", async function () {
-//     const res = await updateTaskStatusToDone(tasks);
+      expect(res).to.deep.equal({
+        totalUpdatedStatus: 8,
+        totalOperationsFailed: 0,
+        updatedTaskDetails: taskDetails,
+        failedTaskDetails: [],
+      });
+    });
 
-//   expect(res).to.deep.equal({
-//     totalUsersArchived: 8,
-//     totalOperationsFailed: 0,
-//     updatedUserDetails: taskDetails,
-//     failedUserDetails: [],
-//   });
-// });
+    it("should throw an error if firebase batch operation fails", async function () {
+      const batchStub = Sinon.stub(firestore, "batch");
+      batchStub.returns({
+        update: function () {},
+        commit: function () {
+          throw new Error("Firebase batch operation failed");
+        },
+      });
 
-// skip("should return failed response", async function () {
-//   const batchStub = Sinon.stub(firestore, "batch");
-//   batchStub.returns({
-//     update: function () {},
-//     commit: function () {
-//       throw new Error("Firebase batch operation failed");
-//     },
-//   });
+      const res = await updateTaskStatusToDone(tasks);
 
-//   const res = await updateTaskStatusToDone(tasks);
-
-//   expect(res).to.deep.equal({
-//     totalUpdatedStatus: 0,
-//     totalOperationsFailed: 8,
-//     updatedTaskDetails: [],
-//     failedTaskDetails: taskDetails,
-//   });
-// });
-//   });
-// });
+      expect(res).to.deep.equal({
+        totalUpdatedStatus: 0,
+        totalOperationsFailed: 8,
+        updatedTaskDetails: [],
+        failedTaskDetails: taskDetails,
+      });
+    });
+  });
+});
