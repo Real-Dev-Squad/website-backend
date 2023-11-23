72c9f26913998ce29195e3bcf659214e2c66b894
Shubham Kumar Singh <107163260+shubhamsigdar1@users.noreply.github.com>
2023-11-24 01:24:59 +0530

    Revert "remove feature flag (#1703)" (#1716)
    This reverts commit 6d31b557143111f081e929c9797ccb2b83de96ea.
    
    Co-authored-by: Prakash Choudhary <34452139+prakashchoudhary07@users.noreply.github.com>

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -317,6 +317,7 @@ const updateTaskStatus = async (req, res, next) => {
     req.body.updatedAt = Math.round(new Date().getTime() / 1000);
     let userStatusUpdate;
     const taskId = req.params.id;
+    const { userStatusFlag } = req.query;
     const { id: userId, username } = req.userData;
     const task = await tasks.fetchSelfTask(taskId, userId);
 
@@ -325,18 +326,34 @@ const updateTaskStatus = async (req, res, next) => {
     if (task.taskData.status === TASK_STATUS.VERIFIED || req.body.status === TASK_STATUS.MERGED)
       return res.boom.forbidden("Status cannot be updated. Please contact admin.");
 
-    if (task.taskData.status === TASK_STATUS.DONE && req.body.percentCompleted < 100) {
-      if (req.body.status === TASK_STATUS.DONE || !req.body.status) {
-        return res.boom.badRequest("Task percentCompleted can't updated as status is DONE");
+    if (userStatusFlag) {
+      if (task.taskData.status === TASK_STATUS.DONE && req.body.percentCompleted < 100) {
+        if (req.body.status === TASK_STATUS.DONE || !req.body.status) {
+          return res.boom.badRequest("Task percentCompleted can't updated as status is DONE");
+        }
+      }
+
+      if (
+        (req.body.status === TASK_STATUS.DONE || req.body.status === TASK_STATUS.VERIFIED) &&
+        task.taskData.percentCompleted !== 100
+      ) {
+        if (req.body.percentCompleted !== 100) {
+          return res.boom.badRequest("Status cannot be updated. Task is not done yet");
+        }
       }
     }
 
+    if (task.taskData.status === TASK_STATUS.COMPLETED && req.body.percentCompleted < 100) {
+      if (req.body.status === TASK_STATUS.COMPLETED || !req.body.status) {
+        return res.boom.badRequest("Task percentCompleted can't updated as status is COMPLETED");
+      }
+    }
     if (
-      (req.body.status === TASK_STATUS.DONE || req.body.status === TASK_STATUS.VERIFIED) &&
+      (req.body.status === TASK_STATUS.COMPLETED || req.body.status === TASK_STATUS.VERIFIED) &&
       task.taskData.percentCompleted !== 100
     ) {
       if (req.body.percentCompleted !== 100) {
-        return res.boom.badRequest("Status cannot be updated. Task is not done yet");
+        return res.boom.badRequest("Status cannot be updated. Task is not completed yet");
       }
     }
 
