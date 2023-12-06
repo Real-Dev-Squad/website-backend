30daafe2068161d8c4e634d1f515461e4150b232
shreya-mishra <shreyamishra040398@gmail.com>
2023-12-06 16:39:25 +0530

    resolved the comment

--- routes/fcmToken.js
+++ routes/fcmToken.js
@@ -4,17 +4,5 @@ const router = express.Router();
 const authenticate = require("../middlewares/authenticate");
 const { fcmTokenController } = require("../controllers/fcmToken");
 
-// the route should be authenticated
 router.post("/", authenticate, fcmTokenController);
 module.exports = router;
-
-// controller
-/*
-* 2 -> fcm token -> userId in /fcm-token route.
-* multiple fcm token will store in different db : {userId:, fcmTokens: []}
-
- * should store fcm token in a different collection with user-id and the role-ids
- * route - /notify -> POST: data:{group-role, user-id, title, description} [mobile-app ]
- *
- * * PATCH call for fcm token modification
- */
