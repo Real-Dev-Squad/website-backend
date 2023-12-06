5c7323cccf615ee3fcd79de04054b2d05a665b17
shreya-mishra <shreyamishra040398@gmail.com>
2023-12-07 05:18:38 +0530

    resolved the comments

--- controllers/fcmToken.js
+++ controllers/fcmToken.js
@@ -7,14 +7,18 @@ const { saveFcmToken } = require("../models/fcmToken");
  * @param res {Object} - Express response object
  */
 const fcmTokenController = (req, res) => {
-  const { fcmToken } = req.body;
+  try {
+    const { fcmToken } = req.body;
+
+    const fcmTokenId = saveFcmToken({ userId: req.userData.id, fcmToken });
+
+    res.status(200).send(`FCM token saved: ${fcmTokenId}`);
+  } catch (error) {
+    throw new Error(error);
+  }
 
   // Save the FCM token to your database or perform other necessary actions.
   // You can associate the token with a user or device, for example.
-
-  const fcmTokenId = saveFcmToken({ userId: req.userData.id, fcmToken });
-
-  res.status(200).send(`FCM token saved: ${fcmTokenId}`);
 };
 
 module.exports = {
