83eabcded86b0f3ee7062e2fb2c473797606fa6a
Satyam <bajpai1973satyam@gmail.com>
2023-12-24 14:18:28 +0530

    chore: change question middleware from js to ts

--- middlewares/validators/questions.ts
+++ middlewares/validators/questions.ts
@@ -1,7 +1,9 @@
-const joi = require("joi");
+import { NextFunction } from "express";
+import { CustomRequest, CustomResponse } from "../../types/global";
 const logger = require("../../utils/logger.ts");
+const joi = require("joi");
 
-const createQuestion = async (req, res, next) => {
+const createQuestion = async (req: CustomRequest, res: CustomResponse, next: NextFunction) => {
   const schema = joi.object({
     question: joi.string().required(),
     createdBy: joi.string().required(),
