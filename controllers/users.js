7dacd3e248d81653a4b8fdbd8ee291aeab3ba854
Joy <joygupta501@gmail.com>
2023-12-28 09:48:25 +0530

    Feat : Added API to add github_user_id to all users

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,4 @@
+const firestore = require("../utils/firestore");
 const chaincodeQuery = require("../models/chaincodes");
 const userQuery = require("../models/users");
 const profileDiffsQuery = require("../models/profileDiffs");
@@ -856,6 +857,70 @@ async function usersPatchHandler(req, res) {
     return res.boom.badImplementation(INTERNAL_SERVER_ERROR);
   }
 }
+const addGithubId = async (req, res) => {
+  const usersNotFound = [];
+  let countUserFound = 0;
+  let countUserNotFound = 0;
+  try {
+    const requestOptions = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization:
+          "Basic " + btoa(`${config.get("githubOauth.clientId")}:${config.get("githubOauth.clientSecret")}`),
+      },
+    };
+    const usersSnapshot = await firestore.collection("users").get();
+    const totalUsers = usersSnapshot.docs.length;
+    const batchCount = Math.ceil(totalUsers / 500);
+    // Create batch write operations for each batch of documents
+    for (let i = 0; i < batchCount; i++) {
+      const batchDocs = usersSnapshot.docs.slice(i * 500, (i + 1) * 500);
+      const batchWrite = firestore.batch();
+      const batchWrites = [];
+      for (const userDoc of batchDocs) {
+        const githubUsername = userDoc.data().github_id;
+        const username = userDoc.data().username;
+        const userId = userDoc.id;
+        batchWrite.update(userDoc.ref, { github_user_id: null });
+
+        const promise = fetch(`https://api.github.com/users/${githubUsername}`, requestOptions)
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error("Network response was not ok");
+            }
+            return response.json();
+          })
+          .then((data) => {
+            const githubUserId = data.id;
+            batchWrite.update(userDoc.ref, { github_user_id: `${githubUserId}` });
+            countUserFound++;
+          })
+          .catch((error) => {
+            countUserNotFound++;
+            const invalidUsers = { userId, username, githubUsername };
+            usersNotFound.push(invalidUsers);
+            logger.error("An error occurred at fetch:", error);
+          });
+        batchWrites.push(promise);
+      }
+      await Promise.all(batchWrites);
+      await batchWrite.commit();
+    }
+    return res.status(200).json({
+      message: "Result of migration",
+      data: {
+        totalUsers: totalUsers,
+        usersUpdated: countUserFound,
+        usersNotUpdated: countUserNotFound,
+        invalidUsersDetails: usersNotFound,
+      },
+    });
+  } catch (error) {
+    logger.error(`Error while Updating all users: ${error}`);
+    return res.boom.badImplementation(INTERNAL_SERVER_ERROR);
+  }
+};
 
 module.exports = {
   verifyUser,
@@ -886,4 +951,5 @@ module.exports = {
   updateDiscordUserNickname,
   archiveUserIfNotInDiscord,
   usersPatchHandler,
+  addGithubId,
 };
--- routes/users.js
+++ routes/users.js
@@ -61,4 +61,6 @@ router.patch("/profileURL", authenticate, userValidator.updateProfileURL, users.
 router.patch("/rejectDiff", authenticate, authorizeRoles([SUPERUSER]), users.rejectProfileDiff);
 router.patch("/:userId", authenticate, authorizeRoles([SUPERUSER]), users.updateUser);
 router.get("/suggestedUsers/:skillId", authenticate, authorizeRoles([SUPERUSER]), users.getSuggestedUsers);
+// WARNING!! - One time Script/Route to do migration
+router.post("/migrate", authenticate, authorizeRoles([SUPERUSER]), users.addGithubId);
 module.exports = router;
