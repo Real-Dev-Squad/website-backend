b7ea59d4da8a0d7fe5ba05adea633d900e02f0db
Satyam Bajpai <bajpai1973satyam@gmail.com>
2024-01-04 13:16:38 +0530

    feat: add questions API with tests (#1711)

---
+++ middlewares/validators/questions.ts
@@ -0,0 +1,23 @@
+import { NextFunction } from "express";
+import { CustomRequest, CustomResponse } from "../../types/global";
+const logger = require("../../utils/logger.ts");
+const joi = require("joi");
+
+const createQuestion = async (req: CustomRequest, res: CustomResponse, next: NextFunction) => {
+  const schema = joi.object({
+    question: joi.string().required(),
+    createdBy: joi.string().required(),
+    eventId: joi.string().required(),
+    maxCharacters: joi.optional(),
+  });
+
+  try {
+    await schema.validateAsync(req.body);
+    next();
+  } catch (error) {
+    logger.error(`Error validating question: ${error}`);
+    res.boom.badRequest(error.details[0].message);
+  }
+};
+
+module.exports = { createQuestion };
