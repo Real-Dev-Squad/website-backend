3c22ef29bcf313a64ebf6baebde6725ed8149304
Shubham Sharma <shbh541@gmail.com>
2024-01-15 21:38:01 +0530

    Fixed the testcase file name

--- test/unit/utils/genrateCloudFlareHeaders.test.js
+++ test/unit/utils/genrateCloudFlareHeaders.test.js
@@ -5,10 +5,10 @@ describe("genrateCloudFlareHeaders", function () {
   it("generates headers with prop Content-Type and Authorization", function () {
     const data = genrateCloudFlareHeaders();
     expect(data["Content-Type"]).to.be.eq("application/json");
-    expect(data).to.include("eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9");
+    expect(data.Authorization).to.include("eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9");
   });
   it("generates headers with prop Content-Type and Authorization and X-Audit-Log-Reason when id and userName is passed", function () {
-    const data = genrateCloudFlareHeaders({ id: "is", userName: "userName" });
+    const data = genrateCloudFlareHeaders({ id: "id", userName: "userName" });
     expect(data["Content-Type"]).to.be.eq("application/json");
     expect(data.Authorization).to.include("eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9");
     expect(data["X-Audit-Log-Reason"]).to.be.eq("Action initiator's username=>userName and id=id");
