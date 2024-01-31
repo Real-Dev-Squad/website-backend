f11483934e69067d96ca4770c339cf12ade834f0
Sunny Sahsi <sahsisunny@gmail.com>
2024-01-31 15:49:00 +0530

    chore: added constants for changes

---
+++ constants/oooRequest.ts
@@ -0,0 +1,7 @@
+export const OOO_REQUEST_ALREADY_PENDING =
+    "You already have a pending request. Please wait for it to be approved or rejected.";
+export const ERROR_WHILE_CREATING_OOO_REQUEST = "Error while creating ooo request";
+export const OOO_STATUS_REQUEST_CREATED_SUCCESSFULLY = "OOO status requested successfully";
+export const OOO_REQUEST = "oooRequest";
+export const CREATE = "create";
+export const ERRORS = "errors";
