19e461456f2a4c6bb224d2e2bece21550c0f94e1
Sunny Sahsi <sahsisunny@gmail.com>
2024-02-07 15:26:49 +0530

    test: added test for update ooo requests (#1879)

--- test/unit/middlewares/request.test.ts
+++ test/unit/middlewares/request.test.ts
@@ -2,8 +2,8 @@ import chai from "chai";
 import sinon from "sinon";
 const { expect } = chai;
 
-import { createRequestsMiddleware } from "./../../../middlewares/validators/requests";
-import { validOooStatusRequests, invalidOooStatusRequests } from "../../fixtures/oooRequest/oooRequest";
+import { createRequestsMiddleware, updateRequestsMiddleware } from "./../../../middlewares/validators/requests";
+import { validOooStatusRequests, invalidOooStatusRequests, validOooStatusUpdate, invalidOooStatusUpdate } from "../../fixtures/oooRequest/oooRequest";
 import { OooRequestCreateRequest, OooRequestResponse } from "../../../types/oooRequest";
 
 describe("OOO Status Request Validators", function () {
@@ -40,4 +40,36 @@ describe("OOO Status Request Validators", function () {
       expect(nextSpy.calledOnce).to.equal(false);
     });
   });
+  describe('updateOooStatusRequestValidator', function() {
+    it("Should pass validation for a valid update ooo rqeust", async function() {
+      const req = {
+        body: validOooStatusUpdate,
+        query: {
+          dev: "true",
+        },
+      };
+      const res = {};
+      const nextSpy = sinon.spy();
+      await updateRequestsMiddleware(req as any, res as any, nextSpy);
+      expect(nextSpy.calledOnce).to.equal(true);
+    });
+
+    it("Should throw an error for an invalid update ooo request", async function() {
+      const req = {
+        body: invalidOooStatusUpdate,
+        query: {
+          dev: "true",
+        },
+      };
+      const res = {
+        boom: {
+          badRequest: sinon.spy(),
+        },
+      };
+      const nextSpy = sinon.spy();
+      await updateRequestsMiddleware(req as any, res as any, nextSpy);
+      expect(res.boom.badRequest.calledOnce).to.equal(true);
+      expect(nextSpy.calledOnce).to.equal(false);
+    });
+  })
 });
