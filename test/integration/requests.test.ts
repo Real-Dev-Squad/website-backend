9ddfe90988134b006158672e9157e83dbf819ba9
palak <palakgupta2712@gmail.com>
2024-02-10 22:30:04 +0530

    tests/requests: POST related func added test cases

--- test/integration/requests.test.ts
+++ test/integration/requests.test.ts
@@ -1,6 +1,7 @@
 import chai from "chai";
-import chaiHttp from "chai-http";
 const { expect } = chai;
+import chaiHttp from "chai-http";
+import _ from "lodash";
 import config from "config";
 import app from "../../server";
 import cleanDb from "../utils/cleanDb";
@@ -8,9 +9,21 @@ import authService from "../../services/authService";
 import userDataFixture from "../fixtures/user/user";
 const cookieName = config.get("userToken.cookieName");
 import addUser from "../utils/addUser";
-import { createOooRequests, validOooStatusRequests, validOooStatusUpdate,createOooRequests2 } from "../fixtures/oooRequest/oooRequest";
+import {
+  createOooRequests,
+  validOooStatusRequests,
+  validOooStatusUpdate,
+  createOooRequests2,
+} from "../fixtures/oooRequest/oooRequest";
 import { createRequest, updateRequest } from "../../models/requests";
-import { REQUEST_ALREADY_APPROVED, REQUEST_CREATED_SUCCESSFULLY, REQUEST_DOES_NOT_EXIST, REQUEST_UPDATED_SUCCESSFULLY } from "../../constants/requests";
+import {
+  REQUEST_ALREADY_APPROVED,
+  REQUEST_CREATED_SUCCESSFULLY,
+  REQUEST_DOES_NOT_EXIST,
+  REQUEST_UPDATED_SUCCESSFULLY,
+  REQUEST_STATE,
+  REQUEST_TYPE,
+} from "../../constants/requests";
 
 const userData = userDataFixture();
 chai.use(chaiHttp);
@@ -68,6 +81,96 @@ describe("/requests", function () {
           done();
         });
     });
+
+    it("should create a new request and have all the required fields in the response", function (done) {
+      chai
+        .request(app)
+        .post("/requests?dev=true")
+        .set("cookie", `${cookieName}=${authToken}`)
+        .send(validOooStatusRequests)
+        .end(function (err, res) {
+          expect(res).to.have.status(201);
+          expect(res.body).to.have.property("message");
+          expect(Object.keys(res.body.data)).to.have.lengthOf(9);
+          expect(res.body.data.until).to.be.above(res.body.data.from);
+          expect(res.body.data).to.have.property("requestedBy");
+          expect(res.body.data.type).to.equal(REQUEST_TYPE.OOO);
+          expect(res.body.data.state).to.equal(REQUEST_STATE.PENDING);
+          expect(res.body.message).to.equal(REQUEST_CREATED_SUCCESSFULLY);
+          done();
+        });
+    });
+
+    it("should return error if feature flag is not used", function (done) {
+      chai
+        .request(app)
+        .post("/requests")
+        .set("cookie", `${cookieName}=${authToken}`)
+        .send(validOooStatusRequests)
+        .end(function (err, res) {
+          expect(res).to.have.status(400);
+          expect(res.body).to.have.property("message");
+          expect(res.body.message).to.equal("Please use feature flag to make this requests");
+          done();
+        });
+    });
+
+    it("should create a new request", function (done) {
+      chai
+        .request(app)
+        .post("/requests?dev=true")
+        .set("cookie", `${cookieName}=${authToken}`)
+        .send(validOooStatusRequests)
+        .end(function (err, res) {
+          expect(res).to.have.status(201);
+          expect(res.body).to.have.property("message");
+          expect(res.body.message).to.equal(REQUEST_CREATED_SUCCESSFULLY);
+          done();
+        });
+    });
+
+    it("should return error if invalid type is passed", function (done) {
+      const type = "ACTIVE";
+      chai
+        .request(app)
+        .post("/requests?dev=true")
+        .set("cookie", `${cookieName}=${authToken}`)
+        .send({ ...validOooStatusRequests, type })
+        .end(function (err, res) {
+          expect(res).to.have.status(400);
+          expect(res.body).to.have.property("message");
+          expect(res.body.message).to.equal(`Invalid request type: ${type}`);
+          done();
+        });
+    });
+
+    it("should return error if message is not present in body", function (done) {
+      chai
+        .request(app)
+        .post("/requests?dev=true")
+        .set("cookie", `${cookieName}=${authToken}`)
+        .send(_.omit(validOooStatusRequests, "message"))
+        .end(function (err, res) {
+          expect(res).to.have.status(400);
+          expect(res.body).to.have.property("message");
+          expect(res.body.message).to.equal("message is required");
+          done();
+        });
+    });
+
+    it("should return error if state in the body is not PENDING", function (done) {
+      chai
+        .request(app)
+        .post("/requests?dev=true")
+        .set("cookie", `${cookieName}=${authToken}`)
+        .send({ ...validOooStatusRequests, state: REQUEST_STATE.APPROVED })
+        .end(function (err, res) {
+          expect(res).to.have.status(400);
+          expect(res.body).to.have.property("message");
+          expect(res.body.message).to.equal("state must be PENDING");
+          done();
+        });
+    });
   });
 
   describe("PUT /requests/:id", function () {
@@ -92,6 +195,35 @@ describe("/requests", function () {
           expect(res).to.have.status(201);
           expect(res.body).to.have.property("message");
           expect(res.body.message).to.equal(REQUEST_UPDATED_SUCCESSFULLY);
+          done();
+        });
+    });
+
+    it("should update a request", function (done) {
+      chai
+        .request(app)
+        .put(`/requests/${pendingOooRequestId}`)
+        .set("cookie", `${cookieName}=${superUserToken}`)
+        .send(validOooStatusUpdate)
+        .end(function (err, res) {
+          expect(res).to.have.status(400);
+          expect(res.body).to.have.property("message");
+          expect(res.body.message).to.equal("Please use feature flag to make this requests");
+          done();
+        });
+    });
+
+    it("should update a request", function (done) {
+      const type = "ACTIVE";
+      chai
+        .request(app)
+        .put(`/requests/${pendingOooRequestId}?dev=true`)
+        .set("cookie", `${cookieName}=${superUserToken}`)
+        .send({ ...validOooStatusUpdate, type })
+        .end(function (err, res) {
+          expect(res).to.have.status(400);
+          expect(res.body).to.have.property("message");
+          expect(res.body.message).to.equal("Invalid request type: ACTIVE");
           done();
         });
     });
--- test/unit/models/requests.test.ts
+++ test/unit/models/requests.test.ts
@@ -1,71 +1,86 @@
 import { expect } from "chai";
 import cleanDb from "../../utils/cleanDb";
 import { createRequest, updateRequest } from "../../../models/requests";
-import { createOooStatusRequests, updateOooApprovedRequests,updateOooRejectedRequests } from "./../../fixtures/oooRequest/oooRequest";
+import {
+  createOooStatusRequests,
+  updateOooApprovedRequests,
+  updateOooRejectedRequests,
+} from "./../../fixtures/oooRequest/oooRequest";
+import { REQUEST_ALREADY_PENDING } from "../../../constants/requests";
 
-describe("models/oooRequests", () => {
-    afterEach(async () => {
-        await cleanDb();
+describe.only("models/oooRequests", () => {
+  afterEach(async () => {
+    await cleanDb();
+  });
+
+  describe("createRequest", () => {
+    it("should add a new OOO request to the database", async () => {
+      const oooRequest = await createRequest(createOooStatusRequests);
+      expect(oooRequest).to.not.be.null;
+      expect(oooRequest).to.have.property("id");
+      expect(oooRequest).to.have.property("requestedBy");
     });
 
-    describe("createRequest", () => {
-        it("should add a new OOO request to the database", async () => {
-            const oooRequest = await createRequest(createOooStatusRequests);
-            expect(oooRequest).to.not.be.null;
-            expect(oooRequest).to.have.property("id");
-            expect(oooRequest).to.have.property("requestedBy");
-        });
-
-        it("should throw an error if the user already has an OOO request", async () => {
-            await createRequest(createOooStatusRequests);
-              try {
-                await createRequest(createOooStatusRequests);
-                expect.fail("User already has an OOO request");
-
-              } catch (error) {
-                expect(error.message).to.equal("User already has an OOO request");
-              }
-        });
+    it("should throw an error if the user already has an OOO request", async () => {
+      await createRequest(createOooStatusRequests);
+      try {
+        await createRequest(createOooStatusRequests);
+        expect.fail("User already has an OOO request");
+      } catch (error) {
+        expect(error.message).to.equal("User already has an OOO request");
+      }
     });
 
-    describe("updateRequest", () => {
-        it("should update an existing OOO request", async () => {
-            const oooRequest :any= await createRequest(createOooStatusRequests);
-            const updatedOooRequest:any = await updateRequest(oooRequest.id, updateOooApprovedRequests, updateOooApprovedRequests.lastModifiedBy);
-            expect(updatedOooRequest).to.not.be.null;
-            expect(updatedOooRequest).to.have.property("state");
-            expect(updatedOooRequest.state).to.equal(updateOooApprovedRequests.state);
-        });
-
-        it("should throw an error if the OOO request does not exist", async () => {
-            try {
-                await updateRequest("randomId", updateOooApprovedRequests, updateOooApprovedRequests.lastModifiedBy);
-                expect.fail("OOO request does not exist");
-            } catch (error) {
-                expect(error.message).to.equal("OOO request does not exist");
-            }
-        });
-
-        it("should throw an error if the OOO request is already approved", async () => {
-            const oooRequest :any= await createRequest(createOooStatusRequests);
-            await updateRequest(oooRequest.id, updateOooApprovedRequests, updateOooApprovedRequests.lastModifiedBy);
-            try {
-                await updateRequest(oooRequest.id, updateOooApprovedRequests, updateOooApprovedRequests.lastModifiedBy);
-                expect.fail("OOO request is already approved");
-            } catch (error) {
-                expect(error.message).to.equal("OOO request is already approved");
-            }
-        });
-
-        it("should throw an error if the OOO request is already rejected", async () => {
-            const oooRequest:any = await createRequest(createOooStatusRequests);
-            await updateRequest(oooRequest.id, updateOooRejectedRequests, updateOooRejectedRequests.lastModifiedBy);
-            try {
-                await updateRequest(oooRequest.id, updateOooApprovedRequests, updateOooApprovedRequests.lastModifiedBy);
-                expect.fail("OOO request is already rejected");
-            } catch (error) {
-                expect(error.message).to.equal("OOO request is already rejected");
-            }
-        });
+    it("should throw an error if the user already has an OOO request", async () => {
+      await createRequest(createOooStatusRequests);
+      const oooRequest = await createRequest(createOooStatusRequests);
+      expect(oooRequest).to.not.be.null;
+      expect(oooRequest.error).to.equal(REQUEST_ALREADY_PENDING);
     });
+  });
+
+  describe("updateRequest", () => {
+    it("should update an existing OOO request", async () => {
+      const oooRequest: any = await createRequest(createOooStatusRequests);
+      const updatedOooRequest: any = await updateRequest(
+        oooRequest.id,
+        updateOooApprovedRequests,
+        updateOooApprovedRequests.lastModifiedBy
+      );
+      expect(updatedOooRequest).to.not.be.null;
+      expect(updatedOooRequest).to.have.property("state");
+      expect(updatedOooRequest.state).to.equal(updateOooApprovedRequests.state);
+    });
+
+    it("should throw an error if the OOO request does not exist", async () => {
+      try {
+        await updateRequest("randomId", updateOooApprovedRequests, updateOooApprovedRequests.lastModifiedBy);
+        expect.fail("OOO request does not exist");
+      } catch (error) {
+        expect(error.message).to.equal("OOO request does not exist");
+      }
+    });
+
+    it("should throw an error if the OOO request is already approved", async () => {
+      const oooRequest: any = await createRequest(createOooStatusRequests);
+      await updateRequest(oooRequest.id, updateOooApprovedRequests, updateOooApprovedRequests.lastModifiedBy);
+      try {
+        await updateRequest(oooRequest.id, updateOooApprovedRequests, updateOooApprovedRequests.lastModifiedBy);
+        expect.fail("OOO request is already approved");
+      } catch (error) {
+        expect(error.message).to.equal("OOO request is already approved");
+      }
+    });
+
+    it("should throw an error if the OOO request is already rejected", async () => {
+      const oooRequest: any = await createRequest(createOooStatusRequests);
+      await updateRequest(oooRequest.id, updateOooRejectedRequests, updateOooRejectedRequests.lastModifiedBy);
+      try {
+        await updateRequest(oooRequest.id, updateOooApprovedRequests, updateOooApprovedRequests.lastModifiedBy);
+        expect.fail("OOO request is already rejected");
+      } catch (error) {
+        expect(error.message).to.equal("OOO request is already rejected");
+      }
+    });
+  });
 });
