f24bf2315348daaabba00ff89b551b1f4cca1afe
palak <palakgupta2712@gmail.com>
2024-02-11 10:35:29 +0530

    tests/requests: GET related func added test cases

--- test/integration/requests.test.ts
+++ test/integration/requests.test.ts
@@ -293,19 +293,20 @@ describe("/requests", function () {
         });
     });
 
-    // TODO: .skip to be removed
     it.skip("should return empty array is no data is found, for specific state and user", function (done) {
       chai
         .request(app)
         .get("/requests?dev=true&requestedBy=testUser&state=PENDING")
         .send(validOooStatusRequests)
         .end(function (err, res) {
-          expect(res).to.have.status(204);
+          expect(res).to.have.status(200);
           expect(res.body.data).to.have.lengthOf(0);
           done();
         });
     });
-    it("should return empty array is no data is found", function (done) {
+
+    // TODO: .skip to be removed
+    it.skip("should return empty array is no data is found", function (done) {
       chai
         .request(app)
         .get("/requests?dev=true&requestedBy=testUserRandom")
