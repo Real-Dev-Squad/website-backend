f72b30381fb002a9d4028269f96f2d8c1e4c144f
Sunny Sahsi <sahsisunny@gmail.com>
2024-02-21 06:32:41 +0530

    removed commented code

--- models/requests.ts
+++ models/requests.ts
@@ -116,19 +116,3 @@ export const getRequests = async (query: RequestQuery) => {
     throw error;
   }
 };
-
-// export const getRequestById = async (id: string) => {
-//   try {
-//     const requestDoc = await requestModel.doc(id).get();
-//     if (!requestDoc.exists) {
-//       return null;
-//     }
-//     return {
-//       id,
-//       ...requestDoc.data(),
-//     };
-//   } catch (error) {
-//     logger.error(ERROR_WHILE_FETCHING_REQUEST, error);
-//     throw error;
-//   }
-// };
