d34acb16db29bab7c12ca0a74776db9aa06140e1
Lakshay Manchanda <lakshaymanchanda73@gmail.com>
2024-03-11 01:38:00 +0530

    Tests Fixed

--- test/fixtures/usersStatus/usersStatus.js
+++ test/fixtures/usersStatus/usersStatus.js
@@ -1,9 +1,8 @@
 const userStatusDataForNewUser = {
   appliedOn: 1669256009000,
-  endsOn: 1669487368000,
-  message: "Bad Health",
-  status: "OOO",
+  status: "IDLE",
   state: "CURRENT",
+  message: "",
 };
 
 module.exports = {
