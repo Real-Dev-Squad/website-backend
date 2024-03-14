345d7e0cc398048407dd1738180004174e3ed931
Shubham Sharma <shbh541@gmail.com>
2024-03-14 07:01:16 +0530

    Added msg on  for user for not allowed task status change

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -347,6 +347,32 @@ const updateTaskStatus = async (req, res, next) => {
           return res.boom.badRequest("Status cannot be updated. Task is not done yet");
         }
       }
+      if (status) {
+        const isCurrentTaskStatusInProgress = task.taskData.status === TASK_STATUS.IN_PROGRESS;
+        const isCurrentTaskStatusBlock = task.taskData.status === TASK_STATUS.BLOCKED;
+        const isNewTaskStatusInProgress = status === TASK_STATUS.IN_PROGRESS;
+        const isNewTaskStatusBlock = status === TASK_STATUS.BLOCKED;
+        const isCurrProgress100 = parseInt(task.taskData.percentCompleted || 0) === 100;
+        const isCurrProgress0 = parseInt(task.taskData.percentCompleted || 0) === 0;
+        const isNewProgress100 = !!req.body.percentCompleted && parseInt(req.body.percentCompleted) === 100;
+        const isNewProgress0 = !!req.body.percentCompleted !== undefined && parseInt(req.body.percentCompleted) === 0;
+        if (isCurrentTaskStatusInProgress && !isNewTaskStatusBlock && !isCurrProgress100 && !isNewProgress100) {
+          return res.boom.badRequest(
+            "The status of task can not be changed from In progress until progress of task is not 100%."
+          );
+        }
+
+        if (isCurrentTaskStatusBlock && !isNewTaskStatusInProgress && !isCurrProgress100 && !isNewProgress100) {
+          return res.boom.badRequest(
+            "The status of task can not be changed from Blocked until progress of task is not 100%."
+          );
+        }
+        if (isNewTaskStatusInProgress && !isCurrentTaskStatusBlock && !isCurrProgress0 && !isNewProgress0) {
+          return res.boom.badRequest(
+            "The status of task can not be changed to In progress until progress task is not 0%."
+          );
+        }
+      }
     }
 
     if (task.taskData.status === TASK_STATUS.COMPLETED && req.body.percentCompleted < 100) {
