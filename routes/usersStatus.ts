9de5eb6b88b3d5375905f31529baab5fbb95f718
Lakshay Manchanda <lakshaymanchanda73@gmail.com>
2024-04-08 21:46:20 +0530

    Added re-implemented functions

--- routes/usersStatus.ts
+++ routes/usersStatus.ts
@@ -4,8 +4,17 @@ import authenticate from "../middlewares/authenticate";
 import usersStatusController from "../controllers/usersStatus";
 import { validateUsersStatus } from "../middlewares/validators/usersStatus";
 import { authorizeOwnUserIdParamOrSuperUser } from "../middlewares/authorizeOwnOrSuperUser";
+import { authorizeAndAuthenticate } from "../middlewares/authorizeUsersAndService";
+const ROLES = require("../constants/roles");
+const { Services } = require("../constants/bot");
 
 router.get("/:userId", usersStatusController.getUserStatus);
 router.patch("/:userId", authenticate, authorizeOwnUserIdParamOrSuperUser, validateUsersStatus, usersStatusController.updateUserStatus);
-
+router.patch("/update", authorizeAndAuthenticate([ROLES.SUPERUSER], [Services.CRON_JOB_HANDLER]), usersStatusController.updateAllUserStatus);
+// router.patch(
+//   "/batch",
+//   authorizeAndAuthenticate([ROLES.SUPERUSER], [Services.CRON_JOB_HANDLER]),
+//   validateMassUpdate,
+//   batchUpdateUsersStatus
+// );
 module.exports = router;
