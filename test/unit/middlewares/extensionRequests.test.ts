1c733946ec8ed3e5f0738fb7ef89c6b6f3f15077
Sunny Sahsi <sahsisunny@gmail.com>
2024-05-02 03:59:14 +0530

    test: add types for res and req and remove consoles

--- test/unit/middlewares/extensionRequests.test.ts
+++ test/unit/middlewares/extensionRequests.test.ts
@@ -5,39 +5,28 @@ const { expect } = chai;
 import { createExtensionRequestValidator } from "../../../middlewares/validators/extensionRequestsv2";
 import { extensionCreateObject } from "../../fixtures/extension-requests/extensionRequests";
 import { REQUEST_STATE } from "../../../constants/requests";
+import { ExtensionRequestRequest, ExtensionRequestResponse } from "../../../types/extensionRequests";
 
 describe("Extension Request Validators", function () {
-    let req: any;
-    let res: any;
-    let nextSpy: any;
-
-    beforeEach(function () {
-        res = {
-            boom: {
-                badRequest: sinon.spy(),
-            },
-        };
-        nextSpy = sinon.spy();
-    });
-
     describe("createExtensionRequestValidator", function () {
         it("should validate for a valid create extension request", async function () {
-            req = {
+            const req = {
                 body: extensionCreateObject,
             };
-            res = {};
-            console.log("req", req);
-            const response = await createExtensionRequestValidator(req as any, res as any, nextSpy);
-            console.log("response", response);
+            const res = {};
+            const nextSpy = sinon.spy();
+            const response = await createExtensionRequestValidator(req as ExtensionRequestRequest, res as ExtensionRequestResponse, nextSpy);
             expect(nextSpy.calledOnce);
         });
 
         it("should not validate for an invalid extension request on wrong type", async function () {
-            req = {
+            const req = {
                 body: { ...extensionCreateObject, type: "ACTIVE" },
             };
+            const res = {};
+            const nextSpy = sinon.spy();
             try {
-                await createExtensionRequestValidator(req as any, res as any, nextSpy);
+                await createExtensionRequestValidator(req as ExtensionRequestRequest, res as ExtensionRequestResponse, nextSpy);
             } catch (error) {
                 expect(error).to.be.an.instanceOf(Error);
                 expect(error.details[0].message).to.equal(`"type" must be [EXTENSION]`);
@@ -45,11 +34,13 @@ describe("Extension Request Validators", function () {
         });
 
         it("should not validate for an invalid extension request on wrong status", async function () {
-            req = {
+            const req = {
                 body: { ...extensionCreateObject, state: REQUEST_STATE.APPROVED },
             };
+            const res = {};
+            const nextSpy = sinon.spy();
             try {
-                await createExtensionRequestValidator(req as any, res as any, nextSpy);
+                await createExtensionRequestValidator(req as ExtensionRequestRequest, res as ExtensionRequestResponse, nextSpy);
             } catch (error) {
                 expect(error).to.be.an.instanceOf(Error);
                 expect(error.details[0].message).to.equal(`"state" must be [PENDING]`);
@@ -57,11 +48,13 @@ describe("Extension Request Validators", function () {
         });
 
         it("should not validate for an invalid extension request on missing taskId", async function () {
-            req = {
+            const req = {
                 body: { ...extensionCreateObject, taskId: "" },
             };
+            const res = {};
+            const nextSpy = sinon.spy();
             try {
-                await createExtensionRequestValidator(req as any, res as any, nextSpy);
+                await createExtensionRequestValidator(req as ExtensionRequestRequest, res as ExtensionRequestResponse, nextSpy);
             } catch (error) {
                 expect(error).to.be.an.instanceOf(Error);
                 expect(error.details[0].message).to.equal(`taskId cannot be empty`);
