e5de75c858477c96ffec96e48b985e1ccf712298
Sunny Sahsi <sahsisunny@gmail.com>
2024-05-04 22:47:42 +0530

    Added middleware for extension request for /request route (#2028)
    * added middleware for extension request
    
    * test: fix failing tests
    
    * test: add types for res and req and remove consoles

---
+++ middlewares/validators/extensionRequestsv2.ts
@@ -0,0 +1,46 @@
+import joi from "joi";
+import { ExtensionRequestRequest, ExtensionRequestResponse } from "../../types/extensionRequests";
+import { NextFunction } from "express";
+import { REQUEST_TYPE,REQUEST_STATE } from "../../constants/requests";
+
+export const createExtensionRequestValidator = async (
+  req: ExtensionRequestRequest,
+  res: ExtensionRequestResponse,
+  next: NextFunction
+) => {
+
+  const schema = joi
+    .object()
+    .strict()
+    .keys({
+      taskId: joi.string().required().messages({
+        "string.empty": "taskId cannot be empty",
+        "any.required": "taskId is required",
+      }),
+      title: joi.string().required().messages({
+        "string.empty": "title cannot be empty",
+        "any.required": "title is required",
+      }),
+      oldEndsOn: joi.number().required().messages({
+        "number.base": "oldEndsOn must be a number",
+        "any.required": "oldEndsOn is required",
+      }),
+      newEndsOn: joi.number().required().min(joi.ref("oldEndsOn")).messages({
+        "number.base": "newEndsOn must be a number",
+        "any.required": "newEndsOn is required",
+      }),
+      message: joi.string().required().messages({
+        "string.empty": "message cannot be empty",
+      }),
+      state: joi.string().valid(REQUEST_STATE.PENDING).required().messages({
+        "string.empty": "state cannot be empty",
+        "any.required": "state is required",
+      }),
+      type: joi.string().valid(REQUEST_TYPE.EXTENSION).required().messages({
+        "string.empty": "type cannot be empty",
+        "any.required": "type is required",
+      }),
+    });
+
+  await schema.validateAsync(req.body, { abortEarly: false });
+};
