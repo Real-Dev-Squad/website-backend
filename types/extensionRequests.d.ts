940badd395edef76424e7061b0241db0a172bdae
Sunny Sahsi <sahsisunny@gmail.com>
2024-05-04 09:02:07 +0530

    added test and controller for extension request in /request

--- types/extensionRequests.d.ts
+++ types/extensionRequests.d.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { Boom } from "express-boom";
 import { REQUEST_STATE, REQUEST_TYPE } from "../constants/requests";
+import { userData } from "./global";
 
 export type ExtensionRequest = {
     id: string;
@@ -40,9 +41,6 @@ export type ExtensionRequestUpdateBody = {
     state: REQUEST_STATE.APPROVED | REQUEST_STATE.REJECTED;
 };
 
-export type userData = {
-    id: string;
-};
 
 export type RequestQuery = {
     dev?: string;
