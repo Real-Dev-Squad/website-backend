60ef429905a4e1ca8e6831ee43cbd46a45aef326
TEJAS <98630752+tejaskh3@users.noreply.github.com>
2024-05-06 01:32:04 +0530

    feat:add taskTitle field to sendTaskUpdate util (#2029)
    Co-authored-by: Amit Prakash <34869115+iamitprakash@users.noreply.github.com>

--- utils/sendTaskUpdate.js
+++ utils/sendTaskUpdate.js
@@ -1,7 +1,7 @@
 import { generateCloudFlareHeaders } from "../utils/discord-actions.js";
 const DISCORD_BASE_URL = config.get("services.discordBot.baseUrl");
 
-export const sendTaskUpdate = async (completed, blockers, planned, userName, taskId) => {
+export const sendTaskUpdate = async (completed, blockers, planned, userName, taskId, taskTitle) => {
   try {
     const headers = generateCloudFlareHeaders();
     const body = {
@@ -11,6 +11,7 @@ export const sendTaskUpdate = async (completed, blockers, planned, userName, tas
         planned,
         userName,
         taskId,
+        taskTitle,
       },
     };
     await fetch(`${DISCORD_BASE_URL}/task/update`, {
