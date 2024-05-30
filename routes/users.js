1b0f8beb813324a4028f5ca81417584b36d36918
Somnath Golui <code.somu@gmail.com>
2024-05-30 14:26:52 +0530

    Removed /users/self GET route

--- routes/users.js
+++ routes/users.js
@@ -18,7 +18,6 @@ router.post("/verify", authenticate, users.verifyUser);
 router.get("/userId/:userId", users.getUserById);
 router.patch("/self", authenticate, userValidator.updateUser, users.updateSelf);
 router.get("/", userValidator.getUsers, users.getUsers);
-router.get("/self", authenticate, users.getSelfDetails);
 router.get("/isDeveloper", authenticate, users.isDeveloper);
 router.get("/isUsernameAvailable/:username", authenticate, users.getUsernameAvailabilty);
 router.get("/username", authenticate, userValidator.validateGenerateUsernameQuery, users.generateUsername);
