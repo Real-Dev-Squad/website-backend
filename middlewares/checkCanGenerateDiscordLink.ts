83444a6abc3d6a634a77b7b0d3c2f860ba74b58d
vinit717 <vinit224488@gmail.com>
2024-08-19 20:26:45 +0530

    refactor: checkCanGenerateDiscordLink validator

--- middlewares/checkCanGenerateDiscordLink.ts
+++ middlewares/checkCanGenerateDiscordLink.ts
@@ -3,30 +3,48 @@ import { CustomRequest, CustomResponse } from "../types/global";
 const ApplicationModel = require("../models/applications");
 
 const checkCanGenerateDiscordLink = async (req: CustomRequest, res: CustomResponse, next: NextFunction) => {
-  const { id: userId } = req.userData;
+  const { id: userId, roles } = req.userData;
+  const isSuperUser = roles.super_user;
+  const userIdInQuery = req.query.userId;
   const currentTime = Date.now();
   const cutoffTime = 1724630399000;  // Epoch time for 25 August 2024
+
+  if (isSuperUser) {
+    return next();
+  }
+
+  if (userIdInQuery && userIdInQuery !== userId && !isSuperUser) {
+    return res.boom.forbidden("User should be super user to generate link for other users");
+  }
 
   if (currentTime >= cutoffTime) {
     return res.boom.forbidden("Discord invite link generation is not allowed after the cutoff time.");
   }
 
-  const applications = await ApplicationModel.getUserApplications(userId);
-  if (!applications || applications.length === 0) {
-    return res.boom.forbidden("No applications found.");
-  }
+  try {
+    const applications = await ApplicationModel.getUserApplications(userId);
+    
+    if (!applications || applications.length === 0) {
+      return res.boom.forbidden("No applications found.");
+    }
 
-  const approvedApplication = applications.find((application: { status: string; }) => application.status === 'accepted');
+    const approvedApplication = applications.find((application: { status: string; }) => application.status === 'accepted');
+    
+    if (!approvedApplication) {
+      return res.boom.forbidden("Only users with an approved application can generate a Discord invite link.");
+    }
 
-  if (!approvedApplication) {
-    return res.boom.forbidden("Only users with an approved application can generate a Discord invite link.");
+    return next();
+  } catch (error) {
+    return res.boom.badImplementation("An error occurred while checking user applications.");
   }
-  return next();
 };
 
 export default checkCanGenerateDiscordLink;
 
 // <------ We have to revisit this later ------->
+// <--- https://github.com/Real-Dev-Squad/website-backend/issues/2078 --->
+
 
 // const checkCanGenerateDiscordLink = async (req: CustomRequest, res: CustomResponse, next: NextFunction) => {
 //   const { discordId, roles, id: userId, profileStatus } = req.userData;
