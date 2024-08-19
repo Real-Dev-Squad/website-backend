cdaa5f483270a401dfb8a61b3f3c9b7d115b6e48
vinit717 <vinit224488@gmail.com>
2024-08-19 18:13:57 +0530

    fix: discord link generate validator

--- middlewares/checkCanGenerateDiscordLink.ts
+++ middlewares/checkCanGenerateDiscordLink.ts
@@ -1,28 +1,55 @@
 import { NextFunction } from "express";
 import { CustomRequest, CustomResponse } from "../types/global";
+const ApplicationModel = require("../models/applications");
 
 const checkCanGenerateDiscordLink = async (req: CustomRequest, res: CustomResponse, next: NextFunction) => {
-  const { discordId, roles, id: userId, profileStatus } = req.userData;
-  const isSuperUser = roles.super_user;
-  const userIdInQuery = req.query.userId;
+  const { id: userId } = req.userData;
+  const currentTime = Date.now();
+  const cutoffTime = 1724630399000;  // Epoch time for 25 August 2024
 
-  if (userIdInQuery && userIdInQuery !== userId && !isSuperUser) {
-    return res.boom.forbidden("User should be super user to generate link for other users");
+  if (currentTime >= cutoffTime) {
+    return res.boom.forbidden("Discord invite link generation is not allowed after the cutoff time.");
   }
 
-  if (!isSuperUser && discordId) {
-    return res.boom.forbidden("Only users who have never joined discord can generate invite link");
+  const applications = await ApplicationModel.getUserApplications(userId);
+  if (!applications || applications.length === 0) {
+    return res.boom.forbidden("No applications found.");
   }
 
-  if (roles.archived) {
-    return res.boom.forbidden("Archived users cannot generate invite");
-  }
+  const approvedApplication = applications.find((application: { status: string; }) => application.status === 'accepted');
 
-  if (!isSuperUser && !roles.maven && !roles.designer && !roles.product_manager && profileStatus !== "VERIFIED") {
-    return res.boom.forbidden("Only selected roles can generate discord link directly");
+  if (!approvedApplication) {
+    return res.boom.forbidden("Only users with an approved application can generate a Discord invite link.");
   }
-
   return next();
 };
+
+export default checkCanGenerateDiscordLink;
+
+// <------ We have to revisit this later ------->
+
+// const checkCanGenerateDiscordLink = async (req: CustomRequest, res: CustomResponse, next: NextFunction) => {
+//   const { discordId, roles, id: userId, profileStatus } = req.userData;
+//   const isSuperUser = roles.super_user;
+//   const userIdInQuery = req.query.userId;
+
+//   if (userIdInQuery && userIdInQuery !== userId && !isSuperUser) {
+//     return res.boom.forbidden("User should be super user to generate link for other users");
+//   }
+
+//   if (!isSuperUser && discordId) {
+//     return res.boom.forbidden("Only users who have never joined discord can generate invite link");
+//   }
+
+//   if (roles.archived) {
+//     return res.boom.forbidden("Archived users cannot generate invite");
+//   }
+
+//   if (!isSuperUser && !roles.maven && !roles.designer && !roles.product_manager && profileStatus !== "VERIFIED") {
+//     return res.boom.forbidden("Only selected roles can generate discord link directly");
+//   }
+
+//   return next();
+// };
 
 module.exports = checkCanGenerateDiscordLink;
