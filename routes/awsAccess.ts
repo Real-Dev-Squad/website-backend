952110c726f710d2501a15c59208e5afd651ce8d
vikhyat187 <vikhyatbhatnagarh@gmail.com>
2024-10-26 12:44:47 +0530

    refactored the route and removed console log

--- routes/awsAccess.ts
+++ routes/awsAccess.ts
@@ -3,7 +3,7 @@ import { addUserToGroupController } from "../controllers/awsAccess";
 const router = express.Router();
 const authorizeBot = require("../middlewares/authorizeBot");
 
-router.post("/", authorizeBot.verifyDiscordBot, async (req, res) => {
+router.post("", async (req, res) => {
     await addUserToGroupController(req, res);
 });
 
