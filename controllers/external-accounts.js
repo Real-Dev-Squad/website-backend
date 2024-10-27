8604f614c829676e4523658b7d6ded139fc3b2bb
Shobhan Sundar Goutam <shobhangoutam@gmail.com>
2024-10-28 00:17:11 +0530

    fix: staging issue

--- controllers/external-accounts.js
+++ controllers/external-accounts.js
@@ -4,6 +4,8 @@ const { getDiscordMembers } = require("../services/discordService");
 const { addOrUpdate, getUsersByRole, updateUsersInBatch } = require("../models/users");
 const { retrieveDiscordUsers, fetchUsersForKeyValues } = require("../services/dataAccessLayer");
 const { EXTERNAL_ACCOUNTS_POST_ACTIONS } = require("../constants/external-accounts");
+const discordServices = require("../services/discordService");
+const config = require("config");
 const logger = require("../utils/logger");
 const { markUnDoneTasksOfArchivedUsersBacklog } = require("../models/tasks");
 
@@ -68,6 +70,14 @@ const linkExternalAccount = async (req, res) => {
       },
       userId
     );
+
+    try {
+      const unverifiedRoleId = config.get("discordUnverifiedRoleId");
+      await discordServices.removeRoleFromUser(unverifiedRoleId, attributes.discordId, req.userData);
+    } catch (error) {
+      logger.error(`Error getting external account data: ${error}`);
+      return res.status(500).json({ message: `Role Deletion failed. Please contact admin.` });
+    }
 
     return res.status(204).json({ message: "Your discord profile has been linked successfully" });
   } catch (error) {
