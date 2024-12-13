0560a78bcd0b4e3ee851a98235b8fc8d134580bf
Vikas Singh <59792866+vikasosmium@users.noreply.github.com>
2024-12-14 01:19:56 +0530

    Merge pull request #2292 from vikasosmium/deprecate-stocks-self-GET-route
    Added New Route to fetch User Stocks

--- routes/stocks.js
+++ routes/stocks.js
@@ -2,12 +2,15 @@ const express = require("express");
 const router = express.Router();
 const authenticate = require("../middlewares/authenticate");
 const authorizeRoles = require("../middlewares/authorizeRoles");
-const { addNewStock, fetchStocks, getSelfStocks } = require("../controllers/stocks");
+const { addNewStock, fetchStocks, getSelfStocks, getUserStocks } = require("../controllers/stocks");
 const { createStock } = require("../middlewares/validators/stocks");
 const { SUPERUSER } = require("../constants/roles");
+const { devFlagMiddleware } = require("../middlewares/devFlag");
+const { userAuthorization } = require("../middlewares/userAuthorization");
 
 router.get("/", fetchStocks);
 router.post("/", authenticate, authorizeRoles([SUPERUSER]), createStock, addNewStock);
-router.get("/user/self", authenticate, getSelfStocks);
+router.get("/user/self", authenticate, getSelfStocks); // this route will soon be deprecated, please use `/stocks/:userId` route.
+router.get("/:userId", devFlagMiddleware, authenticate, userAuthorization, getUserStocks);
 
 module.exports = router;
