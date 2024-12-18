d60002ba7760243df2ec3bbb5ccef82ccd45315f
vikasosmium <59792866+vikasosmium@users.noreply.github.com>
2024-12-18 12:50:54 +0530

    added one get api to get user extension req

--- routes/extensionRequests.js
+++ routes/extensionRequests.js
@@ -11,10 +11,19 @@ const {
   getExtensionRequestsValidator,
 } = require("../middlewares/validators/extensionRequests");
 const skipAuthorizeRolesUnderFF = require("../middlewares/skipAuthorizeRolesWrapper");
+const { userAuthorization } = require("../middlewares/userAuthorization");
+const { devFlagMiddleware } = require("../middlewares/devFlag");
 
 router.post("/", authenticate, createExtensionRequest, extensionRequests.createTaskExtensionRequest);
 router.get("/", authenticate, getExtensionRequestsValidator, extensionRequests.fetchExtensionRequests);
 router.get("/self", authenticate, extensionRequests.getSelfExtensionRequests);
+router.get(
+  "/user/:userId",
+  devFlagMiddleware,
+  authenticate,
+  userAuthorization,
+  extensionRequests.getSelfExtensionRequests
+);
 router.get("/:id", authenticate, authorizeRoles([SUPERUSER, APPOWNER]), extensionRequests.getExtensionRequest);
 //  remove the skipAuthorizeRolesUnderFF & authorizeRoles middleware when removing the feature flag
 router.patch(
