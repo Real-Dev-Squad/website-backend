aaed53ff3d1f74b049d28748ab80c5eb6aa33065
vikasosmium <59792866+vikasosmium@users.noreply.github.com>
2024-12-25 13:11:40 +0530

    removed console line

--- middlewares/conditionalMiddleware.ts
+++ middlewares/conditionalMiddleware.ts
@@ -1,7 +1,6 @@
 const conditionalMiddleware = (validator) => {
   return async (req, res, next) => {
     if (req.params.userId === req.userData.id && req.query.profile === "true") {
-      console.log("Profile middleware");
       return validator(req, res, next);
     }
     next();
