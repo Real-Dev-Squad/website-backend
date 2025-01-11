4bf146e70f7f3aa1fec67d57b6948ca16453500f
Achintya Chatterjee <55826451+Achintya-Chatterjee@users.noreply.github.com>
2025-01-11 11:43:28 +0530

    Dev to Main Sync  (#2336)
    * feat: Add api to create onboarding extension request from discord server (#2307)
    
    * fix:added onboarding type in request
    
    * feat: added types for onboarding extension request
    
    * feat: added validator and skip-authenticate middleware
    
    * fix: added missing field in user type
    
    * feat: added controller for handling the create onboarding extension request feature
    
    * fix: remove requestedBy field because validation for super-users is done before hitting this api
    
    * refactor: moved constant messages from controller
    
    * fix: remove super-users validation check as it is done before making this api call
    
    * fix: create newEndsOn from current date when deadline has missed
    
    * fix: wrap schema validation logic in try-catch block
    
    * chore: refactor varibale name for better readability
    
    * chore: refactor new deadline calculation logic in a separate utils file for reuse
    
    * chore: use utils function to calculate days to milliseconds
    
    * feat: added utils function to validate date
    
    * fix: return error response for invalid date
    
    * fix: return forbidden response for non-onboarding user
    
    * chore: added semicolon for consistent code practise
    
    * chore: added jsDoc for functions and refactor import statment
    
    * fix: import addLog from service file and fix lint issue
    
    * Add tests for create onboarding extension request api (#2306)
    
    * feat: added tests for middlewares
    
    * feat:added tests for controller
    
    * refactor: create middleware in beforeEach hook
    
    * refactor: remove requestBy field and super-users validation tests
    
    * fix: expectation message as test was failing
    
    * fix: change order of middleware creation as test was failing
    
    * fix: assert statement and mock value
    
    * feat: added tests for handling edges cases and fixed existing test
    
    * chore: correct test name
    
    * chore: fix lint issue
    
    * feat: Integrate userData into Progresses API to reduce redundant calls (#2311)
    
    * initial
    
    * fix typos
    
    * using batches to fetch userIds
    
    * refactor the function
    
    * added test for dev false case
    
    * added unit tests
    
    * fix response body containing email
    
    ---------
    
    Co-authored-by: Achintya Chatterjee <55826451+Achintya-Chatterjee@users.noreply.github.com>
    Co-authored-by: Prakash Choudhary <34452139+prakashchoudhary07@users.noreply.github.com>
    
    * fix: Changed status code to 409 for sending Conflict response (#2339)
    
    * chore(#2342) - upgraded dependency packages
    
    * Revert "chore(#2342) - upgraded dependency packages" (#2343)
    
    This reverts commit 5bc49c679003b9eb929dab1473fc6995b82bc1f2.
    
    ---------
    
    Co-authored-by: Pankaj <pankajshadev@gmail.com>
    Co-authored-by: Anuj Chhikara <107175639+AnujChhikara@users.noreply.github.com>
    Co-authored-by: Prakash Choudhary <34452139+prakashchoudhary07@users.noreply.github.com>
    Co-authored-by: Vikas Singh <59792866+vikasosmium@users.noreply.github.com>
    Co-authored-by: Yash Raj <56453897+yesyash@users.noreply.github.com>

---
+++ test/unit/middlewares/onboardingExtensionRequestValidator.test.ts
@@ -0,0 +1,70 @@
+import { REQUEST_TYPE } from "../../../constants/requests";
+import { createOnboardingExtensionRequestValidator } from "../../../middlewares/validators/onboardingExtensionRequest";
+import sinon from "sinon";
+import { CreateOnboardingExtensionBody } from "../../../types/onboardingExtension";
+import { expect } from "chai";
+
+describe("Onboarding Extension Request Validators", () => {
+    let req: any;
+    let res: any;
+    let nextSpy: sinon.SinonSpy;
+    beforeEach(function () {
+        res = {
+            boom: {
+                badRequest: sinon.spy(),
+            },
+        };
+        nextSpy = sinon.spy();
+    });
+    
+    describe("createOnboardingExtensionRequestValidator", () => {
+        const requestBody:CreateOnboardingExtensionBody = {
+            numberOfDays: 1,
+            reason: "This is reason",
+            userId: "22222",
+            type: REQUEST_TYPE.ONBOARDING
+        }
+        it("should validate for a valid create request", async () => {
+            req = {
+                body: requestBody
+            };
+            res = {};
+
+            await createOnboardingExtensionRequestValidator(req as any, res as any, nextSpy);
+            expect(nextSpy.calledOnce, "next should be called once");
+        });
+        
+        it("should not validate for an invalid request on wrong type", async () => {
+            req = {
+                body: { ...requestBody, type: REQUEST_TYPE.EXTENSION },
+            };
+            try {
+                await createOnboardingExtensionRequestValidator(req as any, res as any, nextSpy);
+            } catch (error) {
+                expect(error.details[0].message).to.equal(`"type" must be [ONBOARDING]`);
+            }
+        });
+
+        it("should not validate for an invalid request on wrong numberOfDays", async () => {
+            req = {
+                body: { ...requestBody, numberOfDays: "2" },
+            };
+            try {
+                await createOnboardingExtensionRequestValidator(req as any, res as any, nextSpy);
+            } catch (error) {
+                expect(error.details[0].message).to.equal(`numberOfDays must be a number`);
+            }
+        });
+
+        it("should not validate for an invalid request on wrong userId", async () => {
+            req = {
+                body: { ...requestBody, userId: undefined },
+            };
+            try {
+                await createOnboardingExtensionRequestValidator(req as any, res as any, nextSpy);
+            } catch (error) {
+                expect(error.details[0].message).to.equal(`userId is required`);
+            }
+        });
+    });
+});
