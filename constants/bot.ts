fd1667be6ebea7bd05e8f7a84e184bd069022f12
Joy <joygupta501@gmail.com>
2025-05-03 13:22:54 +0530

    refactor: Remove DISCORD_SERVICE constant from bot configuration

--- constants/bot.ts
+++ constants/bot.ts
@@ -1,12 +1,10 @@
 const CLOUDFLARE_WORKER = "Cloudflare Worker";
-const DISCORD_SERVICE = "Discord Service";
 const BAD_TOKEN = "BAD.JWT.TOKEN";
 const CRON_JOB_HANDLER = "Cron Job Handler";
 
 const Services = {
   CLOUDFLARE_WORKER: CLOUDFLARE_WORKER,
-  DISCORD_SERVICE: DISCORD_SERVICE,
   CRON_JOB_HANDLER: CRON_JOB_HANDLER,
 };
 
-module.exports = { CLOUDFLARE_WORKER, BAD_TOKEN, CRON_JOB_HANDLER, Services, DISCORD_SERVICE };
+module.exports = { CLOUDFLARE_WORKER, BAD_TOKEN, CRON_JOB_HANDLER, Services };
