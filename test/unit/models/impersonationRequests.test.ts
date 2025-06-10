a969270597c0ffffe2eeaab3715f2fc5ff7f7138
suvidh <kaushiksuvidh6@gmail.com>
2025-06-11 03:57:15 +0530

    fixed tests according to route logic changes

--- test/unit/models/impersonationRequests.test.ts
+++ test/unit/models/impersonationRequests.test.ts
@@ -1,14 +1,13 @@
 import { expect } from "chai";
 import cleanDb from "../../utils/cleanDb";
 import * as impersonationModel from "../../../models/impersonationRequests";
-import {
-  impersonationRequestsBodyData,
-} from "../../fixtures/impersonation-requests/impersonationRequests";
-import { REQUEST_STATE, ERROR_WHILE_CREATING_REQUEST} from "../../../constants/requests";
+import { impersonationRequestsBodyData } from "../../fixtures/impersonation-requests/impersonationRequests";
+import { REQUEST_STATE, ERROR_WHILE_CREATING_REQUEST, REQUEST_ALREADY_PENDING, IMPERSONATION_NOT_COMPLETED } from "../../../constants/requests";
 import addUser from "../../utils/addUser";
-import userDataFixture from "./../../fixtures/user/user";
+import userDataFixture from "../../fixtures/user/user";
 import sinon from "sinon";
 import { CreateImpersonationRequestModelDto } from "../../../types/impersonationRequest";
+
 const userData = userDataFixture();
 
 let testUserId: string;
@@ -42,24 +41,30 @@ describe("models/impersonationRequests", () => {
       });
     });
 
-    it("should throw an error if there is an existing impersonation request", async () => {
+    it("should throw an error if there is an existing PENDING impersonation request", async () => {
+      await impersonationModel.createImpersonationRequest(requestBody);
       try {
         await impersonationModel.createImpersonationRequest(requestBody);
-        await impersonationModel.createImpersonationRequest(requestBody);
       } catch (error) {
-        expect(error.message).to.include("Error while creating request");
+        expect(error.message).to.include(REQUEST_ALREADY_PENDING);
       }
     });
 
     it("should create multiple requests for different users", async () => {
-      const request1 = await impersonationModel.createImpersonationRequest({ ...impersonationRequestsBodyData[0], createdBy: "user1" });
-      const request2 = await impersonationModel.createImpersonationRequest({ ...impersonationRequestsBodyData[0], createdBy: "user2" });
+      const request1 = await impersonationModel.createImpersonationRequest({
+        ...impersonationRequestsBodyData[0],
+        createdBy: "user1"
+      });
+      const request2 = await impersonationModel.createImpersonationRequest({
+        ...impersonationRequestsBodyData[1],
+        createdBy: "user2"
+      });
       expect(request1).to.have.property("id");
       expect(request1.createdBy).to.equal("user1");
-      expect(request1.impersonatedUserId).to.equal(requestBody.impersonatedUserId);
+      expect(request1.impersonatedUserId).to.equal(impersonationRequestsBodyData[0].impersonatedUserId);
       expect(request2).to.have.property("id");
       expect(request2.createdBy).to.equal("user2");
-      expect(request2.impersonatedUserId).to.equal(requestBody.impersonatedUserId);
+      expect(request2.impersonatedUserId).to.equal(impersonationRequestsBodyData[1].impersonatedUserId);
     });
 
     it("should fail if required fields are missing", async () => {
@@ -72,31 +77,17 @@ describe("models/impersonationRequests", () => {
         expect(error.message).to.include(ERROR_WHILE_CREATING_REQUEST);
       }
     });
-  });
 
-  describe("getImpersonationRequestByKeyValues", () => {
-    it("should return the request with the specified key value", async () => {
-      const impersonationRequestObj = { ...impersonationRequestsBodyData[0], createdBy: userData[16].username, userId: testUserId };
-      await impersonationModel.createImpersonationRequest(impersonationRequestObj);
-      const request = await impersonationModel.getImpersonationRequestByKeyValues({
-        createdBy: userData[16].username,
-        userId: testUserId,
-      });
-      expect(request).to.not.be.undefined;
-      expect(request.createdBy).to.equal(userData[16].username);
-    });
-
-    it("should return null if the request with the specified key value does not exist", async () => {
-      const request = await impersonationModel.getImpersonationRequestByKeyValues({
-        createdBy: "randomUser",
-        impersonatedUserId: "randomImpersonatedUser",
-      });
-      expect(request).to.be.null;
-    });
-
-    it("should return null for empty keyValues", async () => {
-      const request = await impersonationModel.getImpersonationRequestByKeyValues({});
-      expect(request).to.be.null;
+    it("should throw forbidden error if an unimpersonated APPROVED request is present", async () => {
+      try {
+        await impersonationModel.createImpersonationRequest({
+          ...impersonationRequestsBodyData[0],
+          status: REQUEST_STATE.APPROVED
+        });
+        await impersonationModel.createImpersonationRequest(impersonationRequestsBodyData[0]);
+      } catch (error) {
+        expect(error.message).to.include(IMPERSONATION_NOT_COMPLETED);
+      }
     });
   });
 });
--- test/unit/services/impersonationRequests.test.ts
+++ test/unit/services/impersonationRequests.test.ts
@@ -1,137 +1,31 @@
 import { expect } from "chai";
 import sinon from "sinon";
-import * as impersonationModel from "../../../models/impersonationRequests";
-const userQuery = require("../../../models/users");
 import * as impersonationService from "../../../services/impersonationRequests";
-import {
-  REQUEST_STATE,
-  REQUEST_ALREADY_PENDING,
-  IMPERSONATION_NOT_COMPLETED,
-  TASK_REQUEST_MESSAGES,
-} from "../../../constants/requests";
-import userDataFixture from "./../../fixtures/user/user";
-const userData = userDataFixture();
+import { TASK_REQUEST_MESSAGES } from "../../../constants/requests";
+import userDataFixture from "../../fixtures/user/user";
 import addUser from "../../utils/addUser";
 import cleanDb from "../../utils/cleanDb";
-import {
-  impersonationRequestsBodyData,
-} from "../../fixtures/impersonation-requests/impersonationRequests";
-import { CreateImpersonationRequestModelDto, ImpersonationRequest } from "../../../types/impersonationRequest";
-const logger = require("../../../utils/logger");
+import { impersonationRequestsBodyData } from "../../fixtures/impersonation-requests/impersonationRequests";
+import { CreateImpersonationRequestModelDto } from "../../../types/impersonationRequest";
+
+const userData = userDataFixture();
 
 describe("Tests Impersonation Requests Service", () => {
   let testUserId: string;
-  let testUserId2: string;
   let requestBody: CreateImpersonationRequestModelDto;
-  let impersonationRequest: ImpersonationRequest;
-  let userDetail;
+  let userDetail: any;
 
   beforeEach(async () => {
     await cleanDb();
-    const userIdPromises = [addUser(userData[20]), addUser(userData[18])];
-    const [userId1, userId2] = await Promise.all(userIdPromises);
+    const userIdPromises = [addUser(userData[20])];
+    const [userId1] = await Promise.all(userIdPromises);
     testUserId = userId1;
-    testUserId2 = userId2;
     userDetail = userData[20];
   });
 
   afterEach(async () => {
     await cleanDb();
     sinon.restore();
-  });
-
-  describe("validateImpersonationRequestService tests", () => {
-    beforeEach(async () => {
-      requestBody = impersonationRequestsBodyData[0];
-      impersonationRequest = await impersonationModel.createImpersonationRequest({
-        ...requestBody,
-        impersonatedUserId: testUserId,
-        createdFor: userDetail.username,
-      });
-    });
-
-    afterEach(async () => {
-      await cleanDb();
-    });
-
-    it("should return Forbidden error with REQUEST_ALREADY_PENDING if a pending request is already present", async () => {
-      try {
-        await impersonationService.validateImpersonationRequestService(requestBody.userId, requestBody.createdBy);
-      } catch (err) {
-        expect(err.name).to.equal("ForbiddenError");
-        expect(err.message).to.equal(REQUEST_ALREADY_PENDING);
-      }
-    });
-
-    it("should not throw any error if a pending request is not present", async () => {
-      await cleanDb();
-      try {
-        await impersonationService.validateImpersonationRequestService(requestBody.userId, requestBody.createdBy);
-      } catch (err) {
-          expect.fail(`Expected no error, but got: ${err.message}`);
-      }
-    });
-
-    it("should throw Forbidden error with IMPERSONATION_NOT_COMPLETED if an approved request is present but not impersonated", async () => {
-      await impersonationModel.createImpersonationRequest({
-        ...impersonationRequestsBodyData[1],
-        impersonatedUserId: testUserId,
-        createdFor: userDetail.username,
-        status: REQUEST_STATE.APPROVED,
-      });
-      try {
-        await impersonationService.validateImpersonationRequestService(requestBody.userId, requestBody.createdBy);
-      } catch (err) {
-        expect(err.name).to.equal("ForbiddenError");
-        expect(err.message).to.equal(IMPERSONATION_NOT_COMPLETED);
-      }
-    });
-
-    it("should not throw Forbidden error if an approved request is present and impersonated:", async () => {
-      await impersonationModel.createImpersonationRequest({
-        ...requestBody,
-        impersonatedUserId: testUserId2,
-        createdFor: userDetail.username,
-        status: REQUEST_STATE.APPROVED,
-        isImpersonationFinished: true,
-      });
-
-      try {
-        await impersonationService.validateImpersonationRequestService(
-          requestBody.userId,
-          testUserId2
-        );
-      } catch (err) {
-        expect.fail(`Expected no error, but got: ${err.message}`);
-      }
-    });
-
-    it("should not throw IMPERSONATION_NOT_COMPLETED if an approved request is not present", async () => {
-      try {
-        await impersonationService.validateImpersonationRequestService(
-          requestBody.userId,
-          testUserId
-        );
-      } catch (err) {
-        expect(err.message).to.equal(REQUEST_ALREADY_PENDING);
-      }
-    });
-
-    it("should log and throw an error when validateImpersonationRequestService fails", async () => {
-      const error = new Error("Something went wrong");
-      const loggerStub = sinon.stub(logger, "error");
-      sinon.stub(impersonationModel, "getImpersonationRequestByKeyValues").throws(error);
-
-      try {
-        await impersonationService.validateImpersonationRequestService(requestBody.userId, requestBody.createdBy);
-      } catch (err) {
-        expect(err.message).to.equal("Something went wrong");
-        expect(loggerStub.calledOnce).to.be.true;
-        expect(loggerStub.firstCall.args[0]).to.equal("Error while validating the request");
-        expect(loggerStub.firstCall.args[1]).to.equal(error);
-      }
-      sinon.restore();
-    });
   });
 
   describe("createImpersonationRequestService", () => {
@@ -141,6 +35,7 @@ describe("Tests Impersonation Requests Service", () => {
 
     afterEach(async () => {
       await cleanDb();
+      sinon.restore();
     });
 
     it("should return NotFound error with USER_NOT_FOUND if userId does not exist", async () => {
@@ -151,7 +46,7 @@ describe("Tests Impersonation Requests Service", () => {
           impersonatedUserId: "randomImpersonatedId",
           reason: "He asked",
         });
-      } catch (err) {
+      } catch (err: any) {
         expect(err.name).to.equal("NotFoundError");
         expect(err.message).to.equal(TASK_REQUEST_MESSAGES.USER_NOT_FOUND);
       }
@@ -170,32 +65,6 @@ describe("Tests Impersonation Requests Service", () => {
       expect(response.id).to.not.be.null;
       expect(response.userId).to.be.equal(requestBody.userId);
       expect(response.impersonatedUserId).to.be.equal(testUserId);
-    });
-
-    it("should throw and log error if validateImpersonationRequestService throws", async () => {
-      const validationError = new Error("Validation exploded");
-      const loggerStub = sinon.stub(logger, "error");
-      const validationStub = sinon.stub(impersonationService, "validateImpersonationRequestService").throws(validationError);
-
-      sinon.stub(userQuery, "fetchUser").resolves({
-        userExists: true,
-        user: userDetail,
-      });
-
-      try {
-        await impersonationService.createImpersonationRequestService({
-          userId: requestBody.userId,
-          createdBy: requestBody.createdBy,
-          impersonatedUserId: testUserId,
-          reason: requestBody.reason,
-        });
-      } catch (err) {
-        expect(err.message).to.equal("Validation exploded");
-        expect(loggerStub.calledOnce).to.be.true;
-        expect(loggerStub.firstCall.args[0]).to.equal("Error while creating request");
-      }
-
-      validationStub.restore();
     });
   });
 });
