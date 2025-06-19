d793b7c5cdd8fbd4f52425c37f1354a5c412212f
suvidh <kaushiksuvidh6@gmail.com>
2025-06-19 21:04:30 +0530

    fixed unit tests by adding stubs

--- test/unit/services/impersonationRequests.test.ts
+++ test/unit/services/impersonationRequests.test.ts
@@ -1,12 +1,15 @@
 import { expect } from "chai";
 import sinon from "sinon";
 import * as impersonationService from "../../../services/impersonationRequests";
-import { TASK_REQUEST_MESSAGES } from "../../../constants/requests";
+import * as impersonationModel from "../../../models/impersonationRequests"
+import { REQUEST_STATE, TASK_REQUEST_MESSAGES } from "../../../constants/requests";
 import userDataFixture from "../../fixtures/user/user";
 import addUser from "../../utils/addUser";
 import cleanDb from "../../utils/cleanDb";
 import { impersonationRequestsBodyData } from "../../fixtures/impersonation-requests/impersonationRequests";
 import { CreateImpersonationRequestModelDto } from "../../../types/impersonationRequest";
+import { Timestamp } from "firebase-admin/firestore";
+const userQuery = require("../../../models/users");
 
 
 describe("Tests Impersonation Requests Service", () => {
@@ -26,7 +29,8 @@ describe("Tests Impersonation Requests Service", () => {
     sinon.restore();
   });
 
-  describe("createImpersonationRequestService", () => {
+
+ describe("createImpersonationRequestService", () => {
     beforeEach(async () => {
       mockRequestBody = impersonationRequestsBodyData[0];
     });
@@ -37,6 +41,7 @@ describe("Tests Impersonation Requests Service", () => {
     });
 
     it("should return NotFound error with USER_NOT_FOUND if userId does not exist", async () => {
+      sinon.stub(userQuery, "fetchUser").returns({userExists:false})
       try {
         await impersonationService.createImpersonationRequestService({
           userId: "randomIs",
@@ -51,6 +56,19 @@ describe("Tests Impersonation Requests Service", () => {
     });
 
     it("should successfully create a new impersonation Request", async () => {
+      sinon.stub(impersonationModel, "createImpersonationRequest").returns(Promise.resolve({
+        id: "123",
+        status: REQUEST_STATE.PENDING,
+        isImpersonationFinished: false,
+        createdBy: mockRequestBody.createdBy,
+        createdFor: mockRequestBody.createdFor,
+        userId: mockRequestBody.userId,
+        reason: "He asked",
+        impersonatedUserId: testUserId,
+        createdAt:Timestamp.now(),
+        updatedAt:Timestamp.now() 
+      }));
+
       const response = await impersonationService.createImpersonationRequestService({
         userId: mockRequestBody.userId,
         createdBy: mockRequestBody.createdBy,
@@ -64,5 +82,19 @@ describe("Tests Impersonation Requests Service", () => {
       expect(response.userId).to.equal(mockRequestBody.userId);
       expect(response.impersonatedUserId).to.equal(testUserId);
     });
+
+    it("should throw error when createImpersonationRequestService fails", async ()=>{
+       sinon.stub(userQuery, "fetchUser").throws(new Error("error"));
+      try{
+      await impersonationService.createImpersonationRequestService({
+        userId: mockRequestBody.userId,
+        createdBy: mockRequestBody.createdBy,
+        impersonatedUserId: testUserId,
+        reason: mockRequestBody.reason,
+      })
+      }catch(error){
+      expect(error.message).to.equal("error");
+      }
+    })
   });
 });
