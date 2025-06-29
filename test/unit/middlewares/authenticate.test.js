a03a2c1391dde9fb7f5f30860ba43457ea809645
suvidh <kaushiksuvidh6@gmail.com>
2025-06-29 11:39:53 +0530

    fixed naming mistake in test

--- test/unit/middlewares/authenticate.test.js
+++ test/unit/middlewares/authenticate.test.js
@@ -146,7 +146,7 @@ describe("Authentication Middleware", function () {
       expect(res.boom.forbidden.notCalled).to.equal(true);
     });
 
-    it("should retirct all write/update type requests during impersonation", async function () {
+    it("should restrict all write/update type requests during impersonation", async function () {
       req.method = "POST";
       req.baseUrl = "/impersonation";
       req.path = "/abc123";
