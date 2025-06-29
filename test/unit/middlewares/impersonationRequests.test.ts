cfcf6b686bc8d936a71749855426218f6282cddb
Suvidh-kaushik <127885995+Suvidh-kaushik@users.noreply.github.com>
2025-06-30 01:21:27 +0530

    feat: adds unit tests for impersonation session feature (#2452)
    * added unit tests for impersonation session route
    
    * fixed test identation and spacing
    
    * fixed unecessary imports and variable in tests
    
    * fixed comments on tests
    
    * fixed naming mistake in test
    
    * fixed failing test

--- test/unit/middlewares/impersonationRequests.test.ts
+++ test/unit/middlewares/impersonationRequests.test.ts
@@ -4,19 +4,20 @@ import {
   createImpersonationRequestValidator,
   getImpersonationRequestByIdValidator,
   getImpersonationRequestsValidator,
-  updateImpersonationRequestValidator
+  updateImpersonationRequestValidator,
+  impersonationSessionValidator
 } from "../../../middlewares/validators/impersonationRequests";
 import {
   CreateImpersonationRequest,
   CreateImpersonationRequestBody,
   ImpersonationRequestResponse,
   GetImpersonationControllerRequest,
+  ImpersonationSessionRequest,
   GetImpersonationRequestByIdRequest,
   UpdateImpersonationRequest,
   UpdateImpersonationRequestStatusBody,
 } from "../../../types/impersonationRequest";
 import { Request, Response } from "express";
-import { getImpersonationRequestById } from "../../../models/impersonationRequests";
 
 const { expect } = chai;
 
@@ -232,4 +233,32 @@ describe("Impersonation Request Validators", function () {
       expect(nextSpy.called).to.be.false;
     });
   });
+
+    describe("impersonationSessionValidator", function(){
+     it("should validate for a valid impersonation session request", async function () {
+      req = {
+        query: {action:"START", dev:"true"}
+      };
+      await impersonationSessionValidator(req as ImpersonationSessionRequest,res as ImpersonationRequestResponse, nextSpy);
+      expect(nextSpy.calledOnce).to.be.true;
+    });
+
+    it("should not validate for an invalid session request on missing action", async function () {
+      req = {
+        query:{action:""}
+      };
+      await impersonationSessionValidator(req as  ImpersonationSessionRequest,res as ImpersonationRequestResponse, nextSpy);
+      expect(res.boom.badRequest.calledOnce).to.be.true;
+      expect(nextSpy.called).to.be.false;
+    });
+
+    it("should invalidate if action field is not of correct type", async function () {
+      req = {
+        query:{action:"ACTIVE",dev:"true"}
+      };
+      await impersonationSessionValidator(req as ImpersonationSessionRequest,res as ImpersonationRequestResponse, nextSpy);
+      expect(res.boom.badRequest.calledOnce).to.be.true;
+      expect(nextSpy.called).to.be.false;
+    });
+  })
 });
