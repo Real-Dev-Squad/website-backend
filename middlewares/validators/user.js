92eec1c137746da555a35b6a54c973897114bf26
Suvidh-kaushik <127885995+Suvidh-kaushik@users.noreply.github.com>
2025-11-15 17:17:50 +0530

    feat: Add ROLE based Login  (#2500)
    * final code changes without tests
    
    * feature commit along with tests
    
    * fixed failing tests
    
    * resolved bot comments
    
    * resolved comments in pr
    
    * resolved comments
    
    * soc for the controller
    
    * resolved comments

--- middlewares/validators/user.js
+++ middlewares/validators/user.js
@@ -5,6 +5,7 @@ const {
   USER_STATUS,
   USERS_PATCH_HANDLER_ACTIONS,
   USERS_PATCH_HANDLER_ERROR_MESSAGES,
+  ALL_USER_ROLES,
 } = require("../../constants/users");
 const ROLES = require("../../constants/roles");
 const { IMAGE_VERIFICATION_TYPES } = require("../../constants/imageVerificationTypes");
@@ -52,11 +53,10 @@ const updateUser = async (req, res, next) => {
         .optional(),
       discordId: joi.string().optional(),
       disabledRoles: joi.array().items(joi.string().valid("super_user", "member")).optional(),
-      roles: joi.object().keys({
-        designer: joi.boolean().optional(),
-        maven: joi.boolean().optional(),
-        product_manager: joi.boolean().optional(),
-      }),
+      role: joi
+        .string()
+        .valid(...Object.values(ALL_USER_ROLES))
+        .optional(),
     });
 
   try {
