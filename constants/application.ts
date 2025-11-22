89abab8eee6f383b8e4e7ba2336242e1494ca139
Hariom Vashista <rishivashista4@gmail.com>
2025-11-22 21:04:08 +0530

    feat(application-feedback): added semicolon in constant file to maintain consistency

--- constants/application.ts
+++ constants/application.ts
@@ -1,5 +1,5 @@
 const APPLICATION_STATUS_TYPES = ["accepted", "rejected", "pending"];
-const NEW_APPLICATION_STATUS_TYPES = ["not_submitted", "pending", "accepted", "rejected", "requested_changes"]
+const NEW_APPLICATION_STATUS_TYPES = ["not_submitted", "pending", "accepted", "rejected", "requested_changes"];
 
 const API_RESPONSE_MESSAGES = {
   APPLICATION_RETURN_SUCCESS: "Applications returned successfully",
