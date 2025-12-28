777f32fa0ae23a7fe6def04a7f1da5a173021e7f
Dhirenderchoudhary <dhirenderchoudhary0001@gmailcom>
2025-12-29 02:36:39 +0530

    Revert "feature flag removed for OOO creation"
    This reverts commit 6685c0c80f1f26596f27f8d147a540e80077fad3.

--- utils/requests.ts
+++ utils/requests.ts
@@ -78,11 +78,6 @@ export const transformRequestResponse = (allRequests: (OooStatusRequest | oldOoo
     if (dev && 'status' in request) {
       return oldOOOSchema(request as OooStatusRequest);
     }
-    
-    if (!dev && 'state' in request) {
-      return newOOOSchema(request as oldOooStatusRequest);
-    }
-
     return request;
   });
 };
